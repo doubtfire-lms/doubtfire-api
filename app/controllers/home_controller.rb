8f09bfae06896a864039f2391a7175a8fe35faab
Rohan Liston <rliston@swin.edu.au>
2012-07-16 16:32:36 +1000

    Adjusted root route to go to the sign-in page if not logged in, and appropriate user page if logged in.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,4 @@
 class HomeController < ApplicationController
-  def index
-  end
+	def login
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 Doubtfire::Application.routes.draw do
+
 	devise_for :users
 	
-  resources :users, :only => ["index", "show", "edit", "update"]
-  resources :home, :only => :index
-  resources :projects
-  resources :tasks
-  resources :task_statuses
-  resources :project_statuses
-  resources :teams
+  	resources :users, :only => ["index", "show", "edit", "update"]
+  	resources :home, :only => :index
+  	resources :projects
+  	resources :tasks
+  	resources :task_statuses
+  	resources :project_statuses
+  	resources :teams
 
-  root :to => "home#index"
+  	# If the user is logged in, go to the appropriate page based on their system role (eg. developer dashboard, user listing, etc.)
+  	authenticate :user do
+  		root :to => "users#index"
+  	end
+
+  	# Otherwise, go to the home page
+  	root :to => "home#index"
 end
