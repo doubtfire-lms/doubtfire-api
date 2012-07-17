b98403221ce891884c72290525db10998bd06ca4
Allan Jones <ajones@swin.edu.au>
2012-07-17 15:15:08 +1000

    Updated devise migration to comply with 2.1 standards

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120716013213) do
+ActiveRecord::Schema.define(:version => 20120713033807) do
 
   create_table "project_memberships", :force => true do |t|
     t.integer  "project_status_id"
@@ -45,7 +45,6 @@ ActiveRecord::Schema.define(:version => 20120716013213) do
     t.integer  "task_id"
     t.integer  "project_membership_id"
     t.integer  "task_status_id"
-    t.string   "task_status"
     t.boolean  "awaiting_signoff"
     t.datetime "created_at",            :null => false
     t.datetime "updated_at",            :null => false
@@ -100,24 +99,20 @@ ActiveRecord::Schema.define(:version => 20120716013213) do
   add_index "teams", ["user_id"], :name => "index_teams_on_user_id"
 
   create_table "users", :force => true do |t|
-    t.string   "email",                                 :default => "", :null => false
-    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                         :default => 0
+    t.integer  "sign_in_count",          :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                                            :null => false
-    t.datetime "updated_at",                                            :null => false
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "username"
   end
-
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
