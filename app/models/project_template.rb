453034072f4e3ef5c979bd939c50b966d6c5a16c
Rohan Liston <rliston@swin.edu.au>
2012-07-24 16:23:25 +1000

    Renamed 'convenor_id' accessor to 'convenors' in ProjectTemplate - makes more sense given the relationship.

--- app/models/project_template.rb
+++ app/models/project_template.rb
@@ -1,8 +1,9 @@
 class ProjectTemplate < ActiveRecord::Base
   attr_accessible :description, :end_date, :name, :start_date
-  attr_accessor :convenor_id
-
   validates_presence_of :name, :description, :start_date, :end_date
+
+  # Accessor to allow setting of convenors via the new/edit form
+  attr_accessor :convenors  
 
   # Model associations
   has_many :task_templates
