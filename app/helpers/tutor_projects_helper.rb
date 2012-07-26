39773620747462cd30bcb9ba4dc2571596eb91fd
Allan Jones <ajones@swin.edu.au>
2012-07-27 09:45:39 +1000

    Added content to tutor project widgets on index page

--- app/helpers/tutor_projects_helper.rb
+++ app/helpers/tutor_projects_helper.rb
@@ -1,2 +1,6 @@
 module TutorProjectsHelper
+  def unmarked_project_tasks(tutor_project_template)
+    projects = Project.where(:project_template_id => tutor_project_template.id)
+    projects.map{|project| project.tasks }.flatten.select{|task| task.awaiting_signoff? }
+  end
 end
