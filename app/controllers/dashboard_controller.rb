7cd8ed6a9af8b004fdfef380dc0f2cafc9301364
Allan Jones <allanpauljones@gmail.com>
2012-07-30 23:58:07 +1000

    Cut down number of queries in DashboardController

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -10,8 +10,8 @@ class DashboardController < ApplicationController
       redirect_to convenor_index_path
     end
     
-    @student_projects         = @user.team_memberships.map{|tm| tm.project } || []
-    @tutor_project_templates  = Team.where(:user_id => @user.id).map{|team| team.project_template }.uniq
+    @student_projects         = Project.includes(:tasks).find(@user.team_memberships.map{|membership| membership.project_id })
+    @tutor_project_templates  = ProjectTemplate.find(Team.where(:user_id => @user.id).map{|team| team.project_template_id }).uniq
   end
 
   private
--- app/helpers/tutor_projects_helper.rb
+++ app/helpers/tutor_projects_helper.rb
@@ -1,7 +1,10 @@
 module TutorProjectsHelper
   def unmarked_project_tasks(tutor_project_template, tutor)
     tutors_teams    = Team.where(:project_template_id => tutor_project_template.id, :user_id => tutor.id)
-    tutors_projects = TeamMembership.where(:team_id => [tutors_teams.map{|team| team.id}]).map{|tm| tm.project }
+    tutors_projects = Project.includes(:tasks).find(
+    	TeamMembership.where(:team_id => [tutors_teams.map{|team| team.id}])
+    	.map{|membership| membership.project_id }
+    )
 
     tutors_projects.map{|project| project.tasks }.flatten.select{|task| task.awaiting_signoff? }
   end
