06867f5afcbd58f3e794be02595d6b0c61e075aa
Allan Jones <allanpauljones@gmail.com>
2012-07-31 00:19:34 +1000

    Improved query performance for TutorProjectsController

--- app/controllers/tutor_projects_controller.rb
+++ app/controllers/tutor_projects_controller.rb
@@ -5,15 +5,15 @@ class TutorProjectsController < ApplicationController
   before_filter :load_current_user
 
   def show
-    @student_projects         = @user.team_memberships.map{|tm| tm.project }
+    @student_projects         = Project.find(@user.team_memberships.map{|membership| membership.project_id })
     @tutor_project_templates  = Team.where(:user_id => @user.id).map{|team| team.project_template }.uniq
 
-    @tutor_teams              = Team.where(:user_id => @user.id, :project_template_id => params[:id])
+    @tutor_teams              = Team.includes(:team_memberships => [{:project => [{:tasks => [:task_template]}]}]).where(:user_id => @user.id, :project_template_id => params[:id])
     @tutor_team_projects      = @tutor_teams.map{|team| team.team_memberships }.flatten.map{|team_membership| team_membership.project }
 
     @project_template         = ProjectTemplate.find(params[:id])
 
-    @unmarked_tasks           = unmarked_project_tasks(@project_template, @user)
+    @unmarked_tasks           = unmarked_project_tasks(@tutor_team_projects, @user)
   end
 
   def load_current_user
--- app/helpers/tutor_projects_helper.rb
+++ app/helpers/tutor_projects_helper.rb
@@ -2,10 +2,14 @@ module TutorProjectsHelper
   def unmarked_project_tasks(tutor_project_template, tutor)
     tutors_teams    = Team.where(:project_template_id => tutor_project_template.id, :user_id => tutor.id)
     tutors_projects = Project.includes(:tasks).find(
-    	TeamMembership.where(:team_id => [tutors_teams.map{|team| team.id}])
-    	.map{|membership| membership.project_id }
+      TeamMembership.where(:team_id => [tutors_teams.map{|team| team.id}])
+      .map{|membership| membership.project_id }
     )
 
+    tutors_projects.map{|project| project.tasks }.flatten.select{|task| task.awaiting_signoff? }
+  end
+
+  def unmarked_tasks(tutors_projects)
     tutors_projects.map{|project| project.tasks }.flatten.select{|task| task.awaiting_signoff? }
   end
 end
