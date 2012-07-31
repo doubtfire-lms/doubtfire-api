2ed90cd344d4bbdfab85948bd1f830411ad4a288
Allan Jones <allanpauljones@gmail.com>
2012-07-31 19:05:29 +1000

    Added flash messages to application.html, along with helpers to determine the appropriate bootstrap alert class to use according to the notification type

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 module ApplicationHelper
+  def flash_class(level)
+    case level
+    when :notice  then "info"
+    when :error   then "error"
+    when :alert   then "warning"
+    end
+  end
+
   def javascript(*files)
     content_for(:javascript) { javascript_include_tag(*files) }
   end
