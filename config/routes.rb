e45b878788fd9dbaa28f4d66da805147ef68f996
Allan Jones <allanpauljones@gmail.com>
2012-08-05 22:34:50 +1000

    Shortened routes for devise functions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Doubtfire::Application.routes.draw do
   devise_for :users, :skip => [:registrations]
+  devise_for :users, :skip => [:sessions]
+  as :user do
+    get 'login' => 'devise/sessions#new', :as => :new_user_session
+    post 'login' => 'devise/sessions#create', :as => :user_session
+    delete 'logout' => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
   
   resources :users # custom :path separates CRUD interface from Devise
   resources :home, :only => :index
