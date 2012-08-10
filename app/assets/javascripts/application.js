61f987f8c5df871fd226cc4fd783549216870da7
Rohan Liston <rliston@swin.edu.au>
2012-08-10 17:49:04 +1000

    Added progress bands to convenor dashboard widgets Corrected colouring of graph and progress bands Stats are not drawn if a project has no members, and the project button goes to the project admin page instead of convenor_project Added a 'total' to the statuses hash

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,3 +22,29 @@
 $(document).on("focus", "[data-behaviour~='datepicker']", function(e){
     $(this).datepicker({"format": "yyyy-mm-dd", "weekStart": 1, "autoclose": true});
 });
+
+function colourForProjectProgress(progress) 
+{
+  var colour = null;
+
+  switch (progress)
+  {
+    case "ahead":
+      colour = "#48842c"; // Twitter Bootstrap @green
+      break;
+    case "on_track":
+      colour = "#049cdb"; // Twitter Bootstrap @blue
+      break;
+    case "behind":
+      colour = "#f89406"; // Twitter Bootstrap @orange
+      break;
+    case "danger":
+      colour = "#9d261d"; // Twitter Bootstrap @red
+      break;
+    case "doomed":
+      colour = "#000000";
+      break;
+  }
+
+  return colour;
+}
--- app/assets/javascripts/status_distribution.js
+++ app/assets/javascripts/status_distribution.js
@@ -9,56 +9,66 @@ function constructStatusDistributionChart(statusDistributionContainer)
   var projectJSONURL = $(statusDistributionContainer).attr("data-url");
 
   d3.json(projectJSONURL, function(projectJSON) {
-  
-  statuses = [ 
-  {
-    key: "Status",
-    values: [
-      { 
-        "label" : "Ahead" ,
-        "value" : projectJSON.status_distribution.ahead
-      } , 
-      { 
-        "label" : "On track" , 
-        "value" : projectJSON.status_distribution.on_track
-      } , 
-      { 
-        "label" : "Behind" , 
-        "value" : projectJSON.status_distribution.behind
-      } , 
-      { 
-        "label" : "In danger" , 
-        "value" : projectJSON.status_distribution.in_danger
-      } , 
-      { 
-        "label" : "Doomed" ,
-        "value" : projectJSON.status_distribution.doomed
-      }
+    if(projectJSON.status_distribution.total == 0) return;
+
+    var statuses = [ 
+    {
+      key: "Status",
+      values: [
+        { 
+          "label" : "Ahead" ,
+          "value" : projectJSON.status_distribution.ahead
+        } , 
+        { 
+          "label" : "On track" , 
+          "value" : projectJSON.status_distribution.on_track
+        } , 
+        { 
+          "label" : "Behind" , 
+          "value" : projectJSON.status_distribution.behind
+        } , 
+        { 
+          "label" : "In danger" , 
+          "value" : projectJSON.status_distribution.in_danger
+        } , 
+        { 
+          "label" : "Doomed" ,
+          "value" : projectJSON.status_distribution.doomed
+        }
+      ]
+    } 
+    ];
+
+    colours = [
+      colourForProjectProgress("ahead"), 
+      colourForProjectProgress("on_track"), 
+      colourForProjectProgress("behind"), 
+      colourForProjectProgress("danger"), 
+      colourForProjectProgress("doomed")
     ]
-  } 
-  ];
-    nv.addGraph(function() {  
+      
+      nv.addGraph(function() {  
 
-      var width = 150,
-          height = 150;
+        var width = 150,
+            height = 150;
 
-      var chart = nv.models.pieChart()
-          .x(function(d) { return d.label })
-          .y(function(d) { return d.value })
-          .color(d3.scale.category10().range())
-          .showLabels(false)
-          .showLegend(false)
-          .width(width)
-          .height(height);
+        var chart = nv.models.pieChart()
+            .x(function(d) { return d.label })
+            .y(function(d) { return d.value })
+            .color(colours)
+            .showLabels(false)
+            .showLegend(false)
+            .width(width)
+            .height(height);
 
-      d3.select($(statusDistributionContainer).children("svg")[0])
-          .datum(statuses)
-          .transition().duration(1200)
-          .attr('width', width)
-          .attr('height', height)
-          .call(chart);
+        d3.select($(statusDistributionContainer).children("svg")[0])
+            .datum(statuses)
+            .transition().duration(1200)
+            .attr('width', width)
+            .attr('height', height)
+            .call(chart);
 
-      return chart;
+        return chart;
+      });
     });
-  });
 }
