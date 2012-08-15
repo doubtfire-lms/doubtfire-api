0dd2a4f5bcb5d96776f31ff9b8fad0856512292f
Allan Jones <ajones@swin.edu.au>
2012-08-15 16:09:38 +1000

    Removed health % from team project list; made tasks progress bar items link to student's project page (rather than the individual task)

--- app/helpers/tutor_projects_helper.rb
+++ app/helpers/tutor_projects_helper.rb
@@ -26,7 +26,7 @@ module TutorProjectsHelper
             task.awaiting_signoff? ? "awaiting-signoff" : "incomplete-task"
           end
 
-          href        = "/tutor/projects/#{project.id}/students/#{student.id}/tasks/#{task.id}"
+          href        = "/tutor/projects/#{project.id}/students/#{student.id}"
 
           "<a class=\"task-progress-item progress-#{task_class}\" href=\"#{href}\">#{i + 1}</a>"
         }.join("\n")
