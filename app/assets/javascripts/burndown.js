820c816136dace4743ad587ee898dc4730f7fa8f
Allan Jones <allanpauljones@gmail.com>
2012-08-16 21:45:41 +1000

    Renamed recommended_completion_date to target_date in burndown.js

--- app/assets/javascripts/burndown.js
+++ app/assets/javascripts/burndown.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+ $(document).ready(function() {
   $.each($(".burndownchart"), function(i, burndownChartContainer) {
     constructBurndownChart(burndownChartContainer);
   });
