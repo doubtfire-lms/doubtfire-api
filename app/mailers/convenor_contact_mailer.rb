2777e8281306b2fbed6570b7c25e71159b4debfb
Rohan Liston <rliston@swin.edu.au>
2012-08-21 16:27:24 +1000

    Project membership request emails will come to Allan/Rohan instead of the subject convenor whilst we implement a better solution

--- app/mailers/convenor_contact_mailer.rb
+++ app/mailers/convenor_contact_mailer.rb
@@ -3,10 +3,10 @@ class ConvenorContactMailer < ActionMailer::Base
   def request_project_membership(user, convenor, project_template, role)
     user_email = "#{user.username}@swin.edu.au"
 
-    mail to: "#{convenor.email}", 
+    mail to: "ajones@swin.edu.au, rliston@swin.edu.au", 
          from: user_email, 
-         subject: "Please add #{user.username} to #{project_template.name} as a #{role}", 
-         body: "Dear #{convenor.first_name},\n\nThe following user wishes to be added to #{project_template.name} on Doubtfire:\n\nUsername: #{user.username}\nEmail: #{user_email}\nRole: #{role}\n\nYou will also need to ensure their name, surname, and email are present in the system."
+         subject: "[Doubtfire] Please add #{user.username} to #{project_template.name} as a #{role}", 
+         body: "The following user wishes to be added to #{project_template.name} on Doubtfire:\n\nUsername: #{user.username}\nEmail: #{user_email}\nRole: #{role}\n\nYou will also need to ensure their name, surname, and email are present in the system."
   end
 
 end
