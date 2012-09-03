fa9afdf450281caba454e5a9023db3115e51d718
Allan Jones <ajones@swin.edu.au>
2012-09-03 15:18:26 +1000

    Added teams tab to convenor_projects#show

--- app/controllers/convenor_projects_controller.rb
+++ app/controllers/convenor_projects_controller.rb
@@ -3,9 +3,11 @@ class ConvenorProjectsController < ApplicationController
 		@project_template = ProjectTemplate.find(params[:id])
     
 		@project_users = User.joins(:team_memberships => {:project => :project_template})
-							           .where(:projects => {:project_template_id => params[:id]})
-                         .order(:first_name)
-                         .page(params[:page])
-                         .per(20)
+                      .where(:projects => {:project_template_id => params[:id]})
+                      .order(:first_name)
+                      .page(params[:page])
+                      .per(20)
+
+    @project_teams = @project_template.teams
 	end
 end
