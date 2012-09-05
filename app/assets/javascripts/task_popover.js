5a66d53ab34518d037b7328abcb68394b9a6072d
Allan Jones <ajones@swin.edu.au>
2012-09-05 15:05:31 +1000

    Changed popover behaviour so that it will disappear using the mouseleave event rather than requiring the selected task be manually clicked

--- app/assets/javascripts/task_popover.js
+++ app/assets/javascripts/task_popover.js
@@ -2,11 +2,12 @@ $(document).ready(function(){
 	$(".task-progress-item").popover({
 		trigger: 'manual',
         placement: 'top',
-        html: true
+        html: true,
+        template: '<div class="popover" onmouseover="$(this).mouseleave(function() {$(this).hide(); });"><div class="arrow"></div><div class="popover-inner"><h3 class="popover-title"></h3><div class="popover-content"><p></p></div></div></div>'
     })
     .click(function(e) {
-    	$(".popover").hide();
     	e.preventDefault();
-    	$(this).popover('toggle');
+        $(".popover").hide();
+        $(this).popover('show');
     });
 });
