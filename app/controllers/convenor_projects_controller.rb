66507c7f3aea4e9cdcfedc7e6699939b2882cc12
Allan Jones <ajones@swin.edu.au>
2012-09-05 16:28:27 +1000

    Removed pagination from convenor_projects student_list

--- app/controllers/convenor_projects_controller.rb
+++ app/controllers/convenor_projects_controller.rb
@@ -5,8 +5,6 @@ class ConvenorProjectsController < ApplicationController
 		@project_users = User.joins(:team_memberships => {:project => :project_template})
                       .where(:projects => {:project_template_id => params[:id]})
                       .order(:first_name)
-                      .page(params[:page])
-                      .per(20)
 
     @project_teams = @project_template.teams
 	end
