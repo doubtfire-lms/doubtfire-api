26fcdc404f439f75ecb2b82e08813ed84e08390f
Allan Jones <allanpauljones@gmail.com>
2012-09-12 22:16:45 +1000

    Changed schedule.rb so that it actually runs the project status/progress update task; changed path to cron log

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,5 @@
-set :output, "/path/to/my/cron_log.log"
+set :output, "#{path}/log/cron.log"
 
-every 1.minutes do
-  command "echo hi >> /Users/ajones/test.txt"
+every 1.day, :at => '3:00 am' do
+  rake "db:update_temporal"
 end
