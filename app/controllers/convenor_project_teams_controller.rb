8bc3bd25520bf1600746b5efbfa8c5e009d7a449
Allan Jones <ajones@swin.edu.au>
2012-09-18 13:22:25 +1000

    Added convenor_project_teams#show for show a full list of students and task progress

--- app/controllers/convenor_project_teams_controller.rb
+++ app/controllers/convenor_project_teams_controller.rb
@@ -1,4 +1,21 @@
 class ConvenorProjectTeamsController < ApplicationController
+  def index
+    @project_template = ProjectTemplate.includes(:task_templates).find(params[:id])
+    
+    @projects = Project.includes({
+                  team_membership: [:user, :team],
+                  tasks: [:task_template]
+                  }, :project_template
+                )
+                .where(project_template_id: params[:id])
+
+    @projects.sort!{|a,b| a.team_membership.user.name <=> b.team_membership.user.name }
+
+    @project_teams = @projects.map {|project|
+      project.team_membership.team
+    }.uniq
+  end
+
   def show
     @project_template = ProjectTemplate.find(params[:project_template_id])
     @team             = Team.find(params[:team_id])
