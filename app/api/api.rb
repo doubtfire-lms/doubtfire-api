66a0b125e6e87b071c891a44f930bdba6c514c0a
Angus Morton <angus.m.morton@gmail.com>
2014-07-09 15:43:47 +1000

    Remove user_roles endpoint.

--- app/api/api.rb
+++ app/api/api.rb
@@ -50,7 +50,6 @@ module Api
     mount Api::Tasks
     mount Api::Users
     mount Api::UnitRoles
-    mount Api::UserRoles
 
     AuthHelpers.add_auth_to Api::Units
     AuthHelpers.add_auth_to Api::Projects
@@ -58,7 +57,6 @@ module Api
     AuthHelpers.add_auth_to Api::Tasks
     AuthHelpers.add_auth_to Api::Users
     AuthHelpers.add_auth_to Api::UnitRoles
-    AuthHelpers.add_auth_to Api::UserRoles
 
     add_swagger_documentation base_path: "",
                             # api_version: 'api',
