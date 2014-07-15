64e51c1e633190c92bccc1d97ba70d4071b9be96
Alex Cummaudo <alexcu@me.com>
2014-07-15 12:27:51 +1000

    fix role to system_role bug

--- app/api/users.rb
+++ app/api/users.rb
@@ -40,7 +40,7 @@ module Api
         optional :email         , type: String,   desc: 'New email address for user'
         optional :username      , type: String,   desc: 'New username for user'
         optional :nickname      , type: String,   desc: 'New nickname for user'
-        optional :role          , type: String,   desc: 'New role for user [Admin, Convenor, Tutor, Student]'
+        optional :system_role   , type: String,   desc: 'New role for user [Admin, Convenor, Tutor, Student]'
       end
     end
     put '/users/:id' do
@@ -58,24 +58,24 @@ module Api
                                               :email,
                                               :username,
                                               :nickname,
-                                              :role
+                                              :system_role
                                             )
         #
         # Only allow change of role if current user has permissions to demote/promote the user to the new role
         #
-        if user_parameters[:role]
+        if user_parameters[:system_role]
           # work out if promoting or demoting
-          new_role = Role.with_name(user_parameters[:role])
+          new_role = Role.with_name(user_parameters[:system_role])
           if new_role.nil?
-            error!({"error" => "No such role name #{user_parameters[:role]}"}, 403)
+            error!({"error" => "No such role name #{user_parameters[:system_role]}"}, 403)
           end
           action = new_role.id > user.role.id ? :promote : :demote
           # current user not authorised to peform action with new role?
           if not authorise? current_user, current_user, action, new_role
             error!({"error" => "Not authorised to #{action} user with id=#{params[:id]} to #{new_role.name}" }, 403)
           end
-          # update :role to actual Role object rather than String type
-          user_parameters[:role] = new_role
+          # update :system_role to actual Role object rather than String type
+          user_parameters[:system_role] = new_role
         end
         
         # Update changes made to user
@@ -96,7 +96,7 @@ module Api
         requires :email         , type: String,   desc: 'New email address for user'
         requires :username      , type: String,   desc: 'New username for user'
         requires :nickname      , type: String,   desc: 'New nickname for user'
-        requires :role          , type: String,   desc: 'New system role for user [Admin, Convenor, Tutor, Student]'
+        requires :system_role   , type: String,   desc: 'New system role for user [Admin, Convenor, Tutor, Student]'
       end
     end
     post '/users' do
@@ -117,20 +117,18 @@ module Api
                                             :username,
                                             :nickname,
                                             :password,
-                                            :role
+                                            :system_role
                                           )
 
       #
       # Give new user their new role
       #
-      if user_parameters[:role]      
-        new_role = Role.with_name(user_parameters[:role])
-        if new_role.nil?
-          error!({"error" => "No such role name #{val}"}, 403)
-        end
-        # update :role to actual Role object rather than String type
-        user_parameters[:role] = new_role
+      new_role = Role.with_name(user_parameters[:system_role])
+      if new_role.nil?
+        error!({"error" => "No such role name #{val}"}, 403)
       end
+      # update :system_role to actual Role object rather than String type
+      user_parameters[:system_role] = new_role
       
       user = User.create!(user_parameters)
       user
