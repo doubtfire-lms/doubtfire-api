9bfe082cba98aa0631a53ae96e8102202c66618b
hellola <ewoudt.kellerman@gmail.com>
2014-07-24 14:36:00 +1000

    add update role api method and force tutorial serializer to provide datetime

--- app/serializers/tutorial_serializer.rb
+++ app/serializers/tutorial_serializer.rb
@@ -3,5 +3,9 @@ require 'user_serializer'
 class TutorialSerializer < ActiveModel::Serializer
   attributes :id, :unit_id, :code, :meeting_day, :meeting_time, :meeting_location, :abbreviation
 
+  def meeting_time 
+    DateTime.parse("01/01/2014 #{object.meeting_time}")
+  end 
+
   has_one :tutor, serializer: ShallowUserSerializer
 end
