601785a995fb24bcb950f572598e252653f42b51
Andrew Cain <acain@swin.edu.au>
2014-07-29 17:14:02 +1000

    delete controllers - not used in grape api

--- app/controllers/resources_controller.rb
+++
@@ -1,5 +0,0 @@
-class ResourcesController < ApplicationController
-  def download_task_import_template
-    send_file "public/resources/task-import-template.csv", type:  "application/csv"
-  end
-end
