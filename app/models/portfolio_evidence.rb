4b44a2cc3afc5965210a66ca52b2eb819527d736
Alex Cummaudo <alexcu@me.com>
2014-08-01 14:56:20 +1000

    add generation methods to models/portfolio_evidence.rb

--- app/models/portfolio_evidence.rb
+++ app/models/portfolio_evidence.rb
@@ -30,7 +30,6 @@ class PortfolioEvidence
   # once the method is finished.
   #
   def produce_student_work(files, student, task)
-    
     #
     # Ensure that each file in files has the following attributes:
     # id, name, filename, type, tempfile  
@@ -39,10 +38,10 @@ class PortfolioEvidence
       error!({"error" => "Missing file data for '#{file.name}'"}, 403) if file.id.nil? || file.name.nil? || file.filename.nil? || file.type.nil? || file.tempfile.nil?
     end
    
-    # file.key      = "file0"
-    # file.name     = front end name for file
-    # file.tempfile.path = actual file
-    # file.filename = their name for the file
+    # file.key            = "file0"
+    # file.name           = front end name for file
+    # file.tempfile.path  = actual file dir
+    # file.filename       = their name for the file
 
     #
     # Confirm subtype categories using filemagic (exception handling
@@ -73,9 +72,9 @@ class PortfolioEvidence
     end
     
     #
-    # Create student submission folder (<tmpdir>/doubtfire/<id>)
+    # Create student submission folder (<tmpdir>/doubtfire/new/<id>)
     #
-    tmp_dir = File.join( Dir.tmpdir, 'doubtfire',  task.id )
+    tmp_dir = File.join( Dir.tmpdir, 'doubtfire', 'new', task.id )
     logger.debug('creating output at #{tmp_dir}, #{new_dir}')
     # ensure the dir exists
     Dir.mkdir_p(tmp_dir)
@@ -102,16 +101,16 @@ class PortfolioEvidence
       logger.debug "generating cover page #{file.key}.cover.html"
       
       #
-      # Create cover page for the submitted file
+      # Create cover page for the submitted file (<taskid>/file0.cover.html etc.)
       #
       coverp_file = File.new([tmp_dir, "#{file.key}.cover", ".html"], model="w")
       coverp_file.write(coverpage_body)
       coverp_file.close
 
       #
-      # Now copy the actual data for the submitted file (<taskid>/file0.png etc.)
+      # Now copy the actual data for the submitted file (<taskid>/file0.image.png etc.)
       #
-      output_filename = File.join(tmp_dir, "#{file.key}", File.extname(file.tempfile.path))
+      output_filename = File.join(tmp_dir, "#{file.key}.#{file.type}", File.extname(file.tempfile.path))
       FileUtils.cp file.tempfile.path, output_filename
     end
     
@@ -125,22 +124,166 @@ class PortfolioEvidence
     FileUtils.rmdir tmp_dir
   end  
 
-  
-  # 
-  # Alex
-  #
-  
   #
   # Process enqueued pdfs in each folder of the :new directory
   # into PDF files
   #
-  def self.process_pdfs
-    # For each folder in new
-    #Dir.entries(
+  def self.process_new_to_pdf
+    # For each folder in new (i.e., queued folders to process)
+    new_root_dir = Dir.entries(student_work_dir(:new)).reject { | f | f == "." || f == ".." }
+    new_root_dir.each do | folder_id |
+      process_task_to_pdf(folder_id)
+    end
+  end
+
+  def self.process_task_to_pdf(id)
+    #
+    # Get access to the task
+    #
+    task = Task.find(id)
+    
+    #
+    # Move folder over from new -> in_provess
+    #
+    new_task_dir = student_work_dir(:new, task)
+    in_process_root_dir = student_work_dir(:in_process)
+    FileUtils.mv new_task_dir, in_process_root_dir
+    
+    #
+    # Get access to all files to process
+    #
+    in_process_dir = student_work_dir(:in_process, task)
+    in_process_files = Dir.entries(in_process_dir).reject { | f | f == "." || f == ".." }
+
+    #
+    # Map each process file to have extra info i.e.:
+    #
+    # file.key            = "file0"
+    # file.path           = actual file dir sitting in in_process directory
+    # file.ext            = file extension
+    # file.type           = image/code/document
+    # file.actualfile     = actual file variable that can be used - File.open(path)
+    #
+    files = []
+    in_process_files.each do | file |
+      # file0.code.png
+      key = file.split('.').first
+      type = file.split('.').second
+      path = "#{in_process_dir}#{file}"
+      ext = File.extname(path)
+      actualfile = File.open(path)
+      files << { :key => key, :type => type, :path => path, :ext => ext, :actualfile => actualfile }
+    end
+    
+    #
+    # Create student submission folder (<tmpdir>/doubtfire/pdf/<id>)
+    # This is the output directory of all pdfs once compiled from src->pdf
+    #
+    tmp_dir = File.join( Dir.tmpdir, 'doubtfire', 'pdf', task.id.to_s )
+    # ensure the dir exists
+    FileUtils.mkdir_p(tmp_dir)
+    
+    #
+    # Begin processing... 
+    #   @andrew, need to sort it so that cover files will come first... any ideas? ran out of time :(
+    # 
+    pdf_paths = []
+    pdf_paths_mutex = Mutex.new
+    files.each_with_index do | file, idx |
+      Thread.new do
+        outdir = "#{tmp_dir}/#{file.key}.#{file.type}.pdf"
+        
+        convert_to_pdf(file, outdir)
+        
+        pdf_paths_mutex.synchronize do
+          pdf_paths[idx] = outdir
+        end
+      end
+    end.each { | thread | thread.join }
+    
+    pdf_paths = pdf_paths.flatten
+    
+    final_pdf_path = "#{student_work_dir(:pdf, task)}#{task.abbrev}-#{task.id}.pdf"
+    
+    #
+    # Aggregate each of the output PDFs
+    #
+    didCompile = system "pdftk #{pdf_paths.join ' '} cat output #{final_pdf_path}"
+    if !didCompile 
+      #
+      # @andrew What should happen on an error?
+      #
+    end
+    
+    # Cleanup
+    pdf_paths.each { | path | if File::exist?(path) then FileUtils::rm path end } 
+  end
+
+  #
+  # Converts the given file to a pdf
+  #  
+  def self.convert_to_pdf(file, outdir)
+    case file.type
+    when 'image'
+      img_to_pdf(file, outdir)
+    when 'code'
+      code_to_pdf(file, outdir)
+    when 'document', 'cover'
+      doc_to_pdf(file, outdir)
+    end
   end
   
+  #
+  # Converts the code provided to a pdf
+  #
+  def self.code_to_pdf(file, outdir)
+    # decide language syntax highlighting
+    case file.ext
+    when '.cpp', '.cs'
+      lang = :cplusplus
+    when '.c', '.h'
+      lang = :c
+    when '.java'
+      lang = :java
+    when '.pas'
+      lang = :delphi
+    else
+      # should follow basic C syntax (if, else etc...)
+      lang = :c
+    end
+    # code -> HTML
+    html_body = CodeRay.scan_file(file.actualfile, lang).html(:wrap => :div, :tab_width => 2, :css => :class, :line_numbers => :table, :line_number_anchors => false)
+
+    # HTML -> PDF
+    kit = PDFKit.new(html_body, :page_size => 'A4', :header_right => "[page]/[toPage]", :margin_top => "10mm", :margin_right => "5mm", :margin_bottom => "5mm", :margin_left => "5mm")
+    kit.stylesheets << "vendor/assets/stylesheets/coderay.css"
+    kit.to_file(outdir)
+  end
   
-  
-  
-  
+  #
+  # Converts the image provided to a pdf
+  #
+  def self.img_to_pdf(file, outdir)
+    img = Magick::Image.read(file.path).first
+    # resize the image if its too big (e.g., taken with a digital camera)
+    if img.columns > 1000 || img.rows > 500
+      # resize such that it's 600px in width
+      scale = 1000.0 / img.columns
+      img = img.resize(scale)
+    end
+    img.write("pdf:#{outdir}") { self.quality = 75 }
+  end
+
+  #
+  # Converts the document provided to a pdf
+  #
+  def self.doc_to_pdf(file, outdir)
+    # if uploaded a PDF, then directly pass in
+    if file.ext == '.pdf'
+      # copy the file over (note we need to copy it into
+      # output_file as file will be removed at the end of this block)
+      FileUtils.cp file.path, outdir
+    end
+    # TODO msword doc...
+  end
 end
