562a82c0fc58d1715ff735df50f6e077e30af662
Andrew Cain <acain@swin.edu.au>
2015-11-19 10:11:21 +1100

    ENHANCE: adjust task feedback to include tasks to discuss as well as those needing help and ready to mark

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1074,7 +1074,7 @@ class Unit < ActiveRecord::Base
   # Return the tasks that are waiting for feedback
   #
   def tasks_awaiting_feedback
-    tasks.where('task_status_id IN (:ids)', ids: [ TaskStatus.ready_to_mark, TaskStatus.need_help ]).order('tasks.project_id').map { |t| 
+    tasks.where('task_status_id IN (:ids)', ids: [ TaskStatus.ready_to_mark, TaskStatus.need_help, TaskStatus.discuss ]).order('tasks.project_id').map { |t| 
         { 
           project_id: t.project_id, 
           id: t.id, 
