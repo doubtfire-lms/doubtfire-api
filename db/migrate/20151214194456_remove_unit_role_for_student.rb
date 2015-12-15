714a9c7428c7441394c7100544cc2499bfd3389f
Andrew Cain <acain@swin.edu.au>
2015-12-15 20:36:35 +1100

    FIX: add missing migration for unit_role change

---
+++ db/migrate/20151214194456_remove_unit_role_for_student.rb
@@ -0,0 +1,22 @@
+class RemoveUnitRoleForStudent < ActiveRecord::Migration
+  def change
+  	add_column :projects, :tutorial_id, :integer
+  	add_column :projects, :user_id, :integer
+  	add_index :projects, [:tutorial_id], name: "index_projects_on_tutorial_id", using: :btree
+  	add_index :projects, [:user_id], name: "index_projects_on_user_id", using: :btree
+
+  	if Role.count > 0
+  		puts "-- Migrating tutorial from student unit role to project"
+	  	UnitRole.where("role_id = :student", student: Role.student.id).each do |ur|
+	  		proj = ur.project
+	  		proj.user_id = ur.user_id
+	  		proj.tutorial_id = ur.tutorial_id
+	  		proj.save
+
+	  		ur.delete
+	  	end
+	end
+
+  	remove_column :projects, :unit_role_id, :integer
+  end
+end
