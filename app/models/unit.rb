fff536d34754d3f0b96fc08d05282a68c805bbac
Andrew Cain <acain@swin.edu.au>
2016-01-29 16:27:54 +1100

    FIX: change task sequence to use start date

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -32,7 +32,7 @@ class Unit < ActiveRecord::Base
 
   # Model associations.
   # When a Unit is destroyed, any TaskDefinitions, Tutorials, and ProjectConvenor instances will also be destroyed.
-  has_many :task_definitions, -> { order "target_date ASC, abbreviation ASC" }, dependent: :destroy
+  has_many :task_definitions, -> { order "start_date ASC, abbreviation ASC" }, dependent: :destroy
   has_many :projects, dependent: :destroy
   has_many :tutorials, dependent: :destroy
   has_many :unit_roles, dependent: :destroy
