5003ebc0d3b7baa196984f31a4d7a9f0519912c0
Andrew Cain <acain@swin.edu.au>
2016-02-17 14:19:26 +1100

    FIX: extend the size of the description for an ILO

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160203083806) do
+ActiveRecord::Schema.define(version: 20160217031823) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,7 +92,7 @@ ActiveRecord::Schema.define(version: 20160203083806) do
     t.integer "unit_id"
     t.integer "ilo_number"
     t.string  "name"
-    t.string  "description"
+    t.string  "description",  limit: 2048
     t.string  "abbreviation"
   end
 
