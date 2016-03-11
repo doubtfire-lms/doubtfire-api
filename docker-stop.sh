fc1ba1fe3bdc8d9bbf252aef779e822fd4dc6bab
Alex Cummaudo <alexcu@me.com>
2016-03-12 01:18:56 +1100

    ENHANCE: Improvements to handling errors and more logging
    The start script will try to restore first, then create if the
    docker-compose service does not exist

--- docker-stop.sh
+++ docker-stop.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 DOUBTFIRE_DOCKER_MACHINE_IP=$(docker-machine ip doubtfire)
 
-echo Stopping doubtfire...
+echo "Stopping doubtfire..."
 DOUBTFIRE_DOCKER_MACHINE_IP=$DOUBTFIRE_DOCKER_MACHINE_IP docker-compose -p doubtfire down
+
+if [ $? -ne 0 ]; then
+    echo "Failed to stop Doubtfire! Either it isn't running or something went wrong."
+    exit 3
+fi
+
+echo "Done!"
+echo
+echo "Doubtfire is no longer running"
+echo "To start Doubtfire, run docker-start.sh"
