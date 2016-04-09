c8441a3d2a441433956779513ff2f76eacce783e
Reuben Wilson <reuben@bitnak.com>
2016-04-09 18:40:25 +1000

    QUALITY: Add auth_token member to auth tests

--- test/api/auth_test.rb
+++ test/api/auth_test.rb
@@ -7,9 +7,24 @@ class AuthTest < MiniTest::Test
     Rails.application
   end
 
+  def setup
+    @auth_token = JSON.parse((post '/api/auth.json', '{"username":"acain", "password":"password"}', "CONTENT_TYPE" => 'application/json').body)['auth_token']
+  end
+
+  # --------------------------------------------------------------------------- #
+  # --- Endpoint testing for:
+  # ------- /api/auth.json
+  # ------- POST PUT DELETE
+
+  # POST test
   def test_auth_post
     # Get response back for logging in with username 'acain' password 'password'
-    post '/api/auth.json', '{"username":"acain", "password":"password"}', "CONTENT_TYPE" => 'application/json'
+    post '/api/auth.json',
+                          '{'                       +
+                            '"username":"acain",'   +
+                            '"password":"password"' +
+                          '}', "CONTENT_TYPE" => 'application/json'
+
     # Check to see if the username matches what was expected
     assert JSON.parse(last_response.body)['user']['username'], 'acain'
     # Check to see if the first name matches what was expected
@@ -18,18 +33,21 @@ class AuthTest < MiniTest::Test
     assert JSON.parse(last_response.body)['user']['last_name'], 'cain'
   end
 
+  # PUT test
   def test_auth_put
-    # Get the auth token needed for put test
-    auth_token = JSON.parse((post '/api/auth.json', '{"username":"acain", "password":"password"}', "CONTENT_TYPE" => 'application/json').body)['auth_token']
-    put "/api/auth/#{auth_token}.json", '{"username":"acain"}', "CONTENT_TYPE" => 'application/json'
+    put "/api/auth/#{@auth_token}.json",
+                                        '{'                     +
+                                          '"username":"acain"'  +
+                                        '}', "CONTENT_TYPE" => 'application/json'
+
     # Check to see if the response auth token matches the auth token that was sent through in put
-    assert JSON.parse(last_response.body)['auth_token'], auth_token
+    assert JSON.parse(last_response.body)['auth_token'], @auth_token
   end
 
+  # DELETE test
   def test_auth_delete
     # Get the auth token needed for delete test
-    auth_token = JSON.parse((post '/api/auth.json', '{"username":"acain", "password":"password"}', "CONTENT_TYPE" => 'application/json').body)['auth_token']
-    delete "/api/auth/#{auth_token}.json", "CONTENT_TYPE" => 'application/json'
+    delete "/api/auth/#{@auth_token}.json", "CONTENT_TYPE" => 'application/json'
     # 200 response code means success!
     assert_equal(last_response.status, 200)
   end
