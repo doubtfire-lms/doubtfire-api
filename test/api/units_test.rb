de2c4858e813ca98c2cda94923781389f54d678d
Jake Renzella <j@kerenzella.com>
2016-04-10 17:07:10 +1000

    TEST: Add test for units PUT
    PUT Test will POST a unit, test it, PUT an update and test the updated values.

--- test/api/units_test.rb
+++ test/api/units_test.rb
@@ -70,9 +70,51 @@ class UnitsTest < MiniTest::Test
     # Check code of second unit created
     assert_equal JSON.parse(last_response.body)[1]['code'], 'COS30243'
     # Check start date
-    assert (Time.zone.now - 6.weeks).to_date, JSON.parse(last_response.body)[1]['start_date'].to_date
+    assert (Time.zone.now - 6.weeks).to_date == JSON.parse(last_response.body)[1]['start_date'].to_date
     # Check end date
-    assert (13.weeks.since(Time.zone.now - 6.weeks)).to_date, JSON.parse(last_response.body)[1]['end_date'].to_date
+    assert (13.weeks.since(Time.zone.now - 6.weeks)).to_date == JSON.parse(last_response.body)[1]['end_date'].to_date
+  end
+
+  def test_units_put
+
+    # First post a new unit and test it
+    post  '/api/units.json',
+          '{"unit":'                                    +
+            '{'                                         +
+            '"name":"Intro to Ethnic Studies",'         +
+            '"code":"ETS1011",'                         +
+            '"start_date":"2016-05-14T00:00:00.000Z",'  +
+            '"end_date":"2017-05-14T00:00:00.000Z"'     +
+            '},'                                        +
+          '"auth_token":' + '"' + @auth_token + '"'     +
+          '}', "CONTENT_TYPE" => 'application/json'
+
+    unit_id = JSON.parse(last_response.body)['id']
+
+    assert_equal JSON.parse(last_response.body)['name'], 'Intro to Ethnic Studies'
+    assert_equal JSON.parse(last_response.body)['code'], 'ETS1011'
+    assert JSON.parse(last_response.body)['start_date'].to_date == '2016-05-14T00:00:00.000Z'.to_date
+    assert JSON.parse(last_response.body)['end_date'].to_date == '2017-05-14T00:00:00.000Z'.to_date
+
+    # put  "/api/units/1.json?auth_token=#{@auth_token}"
+
+    put  "/api/units/#{unit_id}.json",
+          '{"unit":'                                    +
+            '{'                                         +
+            '"name":"Intro to Pizza Crafting",'         +
+            '"code":"PZA1011",'                         +
+            '"start_date":"2017-05-14T00:00:00.000Z",'  +
+            '"end_date":"2018-05-14T00:00:00.000Z"'     +
+            '},'                                        +
+          '"auth_token":' + '"' + @auth_token + '"'     +
+          '}', "CONTENT_TYPE" => 'application/json'
+    # Check second unit in Units
+    # Check Name of second unit created
+    assert_equal JSON.parse(last_response.body)['name'], 'Intro to Pizza Crafting'
+    assert_equal JSON.parse(last_response.body)['code'], 'PZA1011'
+    assert JSON.parse(last_response.body)['start_date'].to_date == '2017-05-14T00:00:00.000Z'.to_date
+    assert JSON.parse(last_response.body)['end_date'].to_date == '2018-05-14T00:00:00.000Z'.to_date
+
   end
 
   # Test GET for getting a specific unit by id
