ac2e2c1fb27f071e598d6395fa798123cfd5fca7
Reuben Wilson <reuben@bitnak.com>
2016-04-11 14:38:48 +1000

    TEST: Add support for auth_helper auth_helper manages authentication for all tests that require authentication

--- test/api/units_test.rb
+++ test/api/units_test.rb
@@ -1,15 +1,16 @@
 require 'test_helper'
-require "date"
+require 'date'
 
 class UnitsTest < MiniTest::Test
   include Rack::Test::Methods
+  include AuthHelper
 
   def app
     Rails.application
   end
 
   def setup
-    @auth_token = JSON.parse((post '/api/auth.json', '{"username":"acain", "password":"password"}', "CONTENT_TYPE" => 'application/json').body)['auth_token']
+    @auth_token = get_auth_token()
   end
 
   # --------------------------------------------------------------------------- #
@@ -22,25 +23,59 @@ class UnitsTest < MiniTest::Test
 
   # Test POST for creating new unit
   def test_units_post
-    # Get response back from posting new unit
-    post  '/api/units.json',
-          '{"unit":'                                    +
-            '{'                                         +
-            '"name":"Intro to Social Skills",'          +
-            '"code":"JRRW40003",'                       +
-            '"start_date":"2016-05-14T00:00:00.000Z",'  +
-            '"end_date":"2017-05-14T00:00:00.000Z"'     +
-            '},'                                        +
-          '"auth_token":' + '"' + @auth_token + '"'     +
-          '}', "CONTENT_TYPE" => 'application/json'
+    data_to_post = add_auth_token({
+      unit: {
+        name: "Intro to Social Skills",
+        code: "JRRW40003",
+        start_date: "2016-05-14T00:00:00.000Z",
+        end_date: "2017-05-14T00:00:00.000Z"
+      },
+    })
+
+    unit_count = Unit.all.length
+
+    post '/api/units.json', data_to_post.to_json, "CONTENT_TYPE" => 'application/json'
+
     # Check to see if the unit's name matches what was expected
-    assert_equal JSON.parse(last_response.body)['name'], 'Intro to Social Skills'
+    response = JSON.parse(last_response.body)
+
+    assert_equal response['name'], data_to_post[:unit][:name]
     # Check to see if the unit's code matches what was expected
     assert_equal JSON.parse(last_response.body)['code'], 'JRRW40003'
     # Check to see if the unit's stat date matches what was expected
     assert_equal JSON.parse(last_response.body)['start_date'], '2016-05-14T00:00:00.000Z'
     # Check to see if the unit's end date matches what was expected
     assert_equal JSON.parse(last_response.body)['end_date'], '2017-05-14T00:00:00.000Z'
+
+    assert_equal Unit.all.count, unit_count + 1
+    assert_equal Unit.last.name, data_to_post[:unit][:name]
+  end
+
+  # Test POST for adding a tutorial to a unit
+  def test_units_post_tutorial
+    # Let us add a tutorial to the first unit, which is Introduction to Programming (id 1)
+    post  '/api/units/1/tutorials.json',
+          '{"tutorial":'                                    +
+            '{'                                             +
+            '"day":"Monday",'                               +
+            '"time":"2016-05-14T10:00:00+10:00",'           +
+            '"location":"The Moon",'                        +
+            '"abbrev":"Boss Mode Unlocked",'                +
+            '"tutor_username":"rwilson"'                    +
+            '},'                                            +
+          '"auth_token":' + '"' + @auth_token + '"'         +
+          '}', "CONTENT_TYPE" => 'application/json'
+
+    puts JSON.parse(last_response.body)
+
+    # # Check to see if the unit's name matches what was expected
+    # assert_equal JSON.parse(last_response.body)['name'], 'Intro to Social Skills'
+    # # Check to see if the unit's code matches what was expected
+    # assert_equal JSON.parse(last_response.body)['code'], 'JRRW40003'
+    # # Check to see if the unit's stat date matches what was expected
+    # assert_equal JSON.parse(last_response.body)['start_date'], '2016-05-14T00:00:00.000Z'
+    # # Check to see if the unit's end date matches what was expected
+    # assert_equal JSON.parse(last_response.body)['end_date'], '2017-05-14T00:00:00.000Z'
   end
   # End POST tests
   # --------------------------------------------------------------------------- #
@@ -75,6 +110,39 @@ class UnitsTest < MiniTest::Test
     assert (13.weeks.since(Time.zone.now - 6.weeks)).to_date == JSON.parse(last_response.body)[1]['end_date'].to_date
   end
 
+  # Test GET for getting a specific unit by id
+  def test_units_get_by_id
+    # Get response back from getting a unit by id
+
+    # Test getting the first unit with id of 1
+    get  "/api/units/1.json?auth_token=#{@auth_token}"
+
+    # Check to see if the first unit's name matches
+    assert_equal JSON.parse(last_response.body)['name'], 'Introduction to Programming'
+    # Check to see if the first unit's code matches
+    assert_equal JSON.parse(last_response.body)['code'], 'COS10001'
+    # Test time zones, need to use operator for date conversion.
+    # Check tart date
+    assert (Time.zone.now - 6.weeks).to_date, JSON.parse(last_response.body)['start_date'].to_date
+    # Check end date
+    assert (13.weeks.since(Time.zone.now - 6.weeks)).to_date, JSON.parse(last_response.body)['end_date'].to_date
+
+    # Get response back from getting a unit by id
+    # Test getting the first unit with id of 2
+    get  "/api/units/2.json?auth_token=#{@auth_token}"
+
+    # Check to see if the first unit's name matches
+    assert_equal JSON.parse(last_response.body)['name'], 'Game Programming'
+    # Check to see if the first unit's code matches
+    assert_equal JSON.parse(last_response.body)['code'], 'COS30243'
+    # Test time zones, need to use operator for date conversion.
+    # Check tart date
+    assert (Time.zone.now - 6.weeks).to_date, JSON.parse(last_response.body)['start_date'].to_date
+    # Check end date
+    assert (13.weeks.since(Time.zone.now - 6.weeks)).to_date, JSON.parse(last_response.body)['end_date'].to_date
+  end
+  # End GET tests
+  # --------------------------------------------------------------------------- #
 
   # --------------------------------------------------------------------------- #
   # PUT tests
@@ -118,39 +186,7 @@ class UnitsTest < MiniTest::Test
     assert JSON.parse(last_response.body)['start_date'].to_date == '2017-05-14T00:00:00.000Z'.to_date
     assert JSON.parse(last_response.body)['end_date'].to_date == '2018-05-14T00:00:00.000Z'.to_date
   end
-
-  # Test GET for getting a specific unit by id
-  def test_units_get_by_id
-    # Get response back from getting a unit by id
-
-    # Test getting the first unit with id of 1
-    get  "/api/units/1.json?auth_token=#{@auth_token}"
-
-    # Check to see if the first unit's name matches
-    assert_equal JSON.parse(last_response.body)['name'], 'Introduction to Programming'
-    # Check to see if the first unit's code matches
-    assert_equal JSON.parse(last_response.body)['code'], 'COS10001'
-    # Test time zones, need to use operator for date conversion.
-    # Check tart date
-    assert (Time.zone.now - 6.weeks).to_date, JSON.parse(last_response.body)['start_date'].to_date
-    # Check end date
-    assert (13.weeks.since(Time.zone.now - 6.weeks)).to_date, JSON.parse(last_response.body)['end_date'].to_date
-
-    # Get response back from getting a unit by id
-    # Test getting the first unit with id of 2
-    get  "/api/units/2.json?auth_token=#{@auth_token}"
-
-    # Check to see if the first unit's name matches
-    assert_equal JSON.parse(last_response.body)['name'], 'Game Programming'
-    # Check to see if the first unit's code matches
-    assert_equal JSON.parse(last_response.body)['code'], 'COS30243'
-    # Test time zones, need to use operator for date conversion.
-    # Check tart date
-    assert (Time.zone.now - 6.weeks).to_date, JSON.parse(last_response.body)['start_date'].to_date
-    # Check end date
-    assert (13.weeks.since(Time.zone.now - 6.weeks)).to_date, JSON.parse(last_response.body)['end_date'].to_date
-  end
-  # End GET tests
+  # End PUT tests
   # --------------------------------------------------------------------------- #
 
 end
