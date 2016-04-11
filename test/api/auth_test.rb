9bed4018fed3a2caa00677ce62a2782931ff565c
Reuben Wilson <reuben@bitnak.com>
2016-04-11 15:19:58 +1000

    FIX: Use auth_helper methods in api endpoint tests for auth

--- test/api/auth_test.rb
+++ test/api/auth_test.rb
@@ -2,13 +2,10 @@ require 'test_helper'
 
 class AuthTest < MiniTest::Test
   include Rack::Test::Methods
+  include AuthHelper
 
   def app
     Rails.application
-  end
-
-  def setup
-    @auth_token = JSON.parse((post '/api/auth.json', '{"username":"acain", "password":"password"}', "CONTENT_TYPE" => 'application/json').body)['auth_token']
   end
 
   # --------------------------------------------------------------------------- #
@@ -21,24 +18,21 @@ class AuthTest < MiniTest::Test
 
   # Test POST for new authentication token
   def test_auth_post
+    data_to_post = add_auth_token({
+        username: "acain",
+        password: "password"
+    })
     # Get response back for logging in with username 'acain' password 'password'
-    post  '/api/auth.json',
-          '{'                       +
-            '"username":"acain",'   +
-            '"password":"password"' +
-          '}',
-          "CONTENT_TYPE" => 'application/json'
+    post  '/api/auth.json', data_to_post.to_json, "CONTENT_TYPE" => 'application/json'
+    actual_auth = JSON.parse(last_response.body)
+    expected_auth = User.first
 
     # Check to see if the username matches what was expected
-    assert_equal 'acain', JSON.parse(last_response.body)['user']['username']
+    assert_equal actual_auth['user']['username'], User.first.username
     # Check to see if the first name matches what was expected
-    assert_equal 'Andrew', JSON.parse(last_response.body)['user']['first_name']
-    # Check to see if first name is written in Pascal Case
-    refute_equal 'andrew', JSON.parse(last_response.body)['user']['first_name']
+    assert_equal actual_auth['user']['first_name'], User.first.first_name
     # Check to see if the last name matches what was expected
-    assert_equal 'Cain', JSON.parse(last_response.body)['user']['last_name']
-    # Check to see if last name is written in Pascal Case
-    refute_equal 'cain', JSON.parse(last_response.body)['user']['last_name']
+    assert_equal actual_auth['user']['last_name'], User.first.last_name
   end
   # End POST tests
   # --------------------------------------------------------------------------- #
@@ -48,14 +42,17 @@ class AuthTest < MiniTest::Test
 
   # Test put for authentication token
   def test_auth_put
-    put "/api/auth/#{@auth_token}.json",
-        '{'                     +
-          '"username":"acain"'  +
-        '}',
-        "CONTENT_TYPE" => 'application/json'
+    auth_token = get_auth_token
+    data_to_put = {
+        username: "acain",
+        password: "password"
+    }
+    put "/api/auth/#{auth_token}.json", data_to_put.to_json, "CONTENT_TYPE" => 'application/json'
+    actual_auth = JSON.parse(last_response.body)['auth_token']
+    expected_auth = User.first.auth_token
 
     # Check to see if the response auth token matches the auth token that was sent through in put
-    assert_equal @auth_token, JSON.parse(last_response.body)['auth_token']
+    assert_equal actual_auth, expected_auth
   end
   # End PUT tests
   # --------------------------------------------------------------------------- #
@@ -65,8 +62,9 @@ class AuthTest < MiniTest::Test
 
   # Test for deleting authentication token
   def test_auth_delete
+    auth_token = get_auth_token
     # Get the auth token needed for delete test
-    delete "/api/auth/#{@auth_token}.json", "CONTENT_TYPE" => 'application/json'
+    delete "/api/auth/#{auth_token}.json", "CONTENT_TYPE" => 'application/json'
     # 200 response code means success!
     assert_equal last_response.status, 200
   end
