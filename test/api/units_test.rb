683bc5eef2306ed9390d6c4d3cb3d4d78f64d623
Jake Renzella <j@kerenzella.com>
2016-04-11 18:04:54 +1000

    QUALITY: Refactor tests to use new format

--- test/api/units_test.rb
+++ test/api/units_test.rb
@@ -41,11 +41,11 @@ class UnitsTest < MiniTest::Test
 
     assert_equal response['name'], data_to_post[:unit][:name]
     # Check to see if the unit's code matches what was expected
-    assert_equal JSON.parse(last_response.body)['code'], 'JRRW40003'
+    assert_equal response['code'], data_to_post[:unit][:code]
     # Check to see if the unit's stat date matches what was expected
-    assert_equal JSON.parse(last_response.body)['start_date'], '2016-05-14T00:00:00.000Z'
+    assert_equal response['start_date'], data_to_post[:unit][:start_date]
     # Check to see if the unit's end date matches what was expected
-    assert_equal JSON.parse(last_response.body)['end_date'], '2017-05-14T00:00:00.000Z'
+    assert_equal response['end_date'], data_to_post[:unit][:end_date]
 
     assert_equal Unit.all.count, unit_count + 1
     assert_equal Unit.last.name, data_to_post[:unit][:name]
@@ -53,18 +53,32 @@ class UnitsTest < MiniTest::Test
 
   # Test POST for adding a tutorial to a unit
   def test_units_post_tutorial
+
+  data_to_post = add_auth_token({
+    tutorial: {
+      day: "Monday",
+      time: "2016-05-14T10:00:00+10:00",
+      location: "The Moon",
+      abbrev: "Boss Mode Unlocked",
+      tutor_username: "rwilson"
+      },
+    })
+    post  '/api/units/1/tutorials.json', data_to_post.to_json, "CONTENT_TYPE" => 'application/json'
+
+    # tutor_count = Unit.all.length
+
     # Let us add a tutorial to the first unit, which is Introduction to Programming (id 1)
-    post  '/api/units/1/tutorials.json',
-          '{"tutorial":'                                    +
-            '{'                                             +
-            '"day":"Monday",'                               +
-            '"time":"2016-05-14T10:00:00+10:00",'           +
-            '"location":"The Moon",'                        +
-            '"abbrev":"Boss Mode Unlocked",'                +
-            '"tutor_username":"rwilson"'                    +
-            '},'                                            +
-          '"auth_token":' + '"' + @auth_token + '"'         +
-          '}', "CONTENT_TYPE" => 'application/json'
+    # post  '/api/units/1/tutorials.json',
+    #       '{"tutorial":'                                    +
+    #         '{'                                             +
+    #         '"day":"Monday",'                               +
+    #         '"time":"2016-05-14T10:00:00+10:00",'           +
+    #         '"location":"The Moon",'                        +
+    #         '"abbrev":"Boss Mode Unlocked",'                +
+    #         '"tutor_username":"rwilson"'                    +
+    #         '},'                                            +
+    #       '"auth_token":' + '"' + @auth_token + '"'         +
+    #       '}', "CONTENT_TYPE" => 'application/json'
 
     puts JSON.parse(last_response.body)
 
@@ -89,25 +103,21 @@ class UnitsTest < MiniTest::Test
     # The GET we are testing
     get  "/api/units.json?auth_token=#{@auth_token}"
 
-    # Check to see if the first unit's name matches
-    assert_equal JSON.parse(last_response.body)[0]['name'], 'Introduction to Programming'
-    # Check to see if the first unit's code matches
-    assert_equal JSON.parse(last_response.body)[0]['code'], 'COS10001'
-    # Test time zones, need to use operator for date conversion.
-    # Check tart date
-    assert (Time.zone.now - 6.weeks).to_date, JSON.parse(last_response.body)[0]['start_date'].to_date
-    # Check end date
-    assert (13.weeks.since(Time.zone.now - 6.weeks)).to_date, JSON.parse(last_response.body)[0]['end_date'].to_date
+    actual_unit = JSON.parse(last_response.body)[0]
+    expected_unit = Unit.first
+    assert_equal actual_unit['name'], expected_unit.name
+    assert_equal actual_unit['code'], expected_unit.code
+    assert_equal actual_unit['start_date'].to_date, expected_unit.start_date.to_date
+    assert_equal actual_unit['end_date'].to_date.to_date, expected_unit.end_date.to_date
 
-    # Check second unit in Units
-    # Check Name of second unit created
-    assert_equal JSON.parse(last_response.body)[1]['name'], 'Game Programming'
-    # Check code of second unit created
-    assert_equal JSON.parse(last_response.body)[1]['code'], 'COS30243'
-    # Check start date
-    assert (Time.zone.now - 6.weeks).to_date == JSON.parse(last_response.body)[1]['start_date'].to_date
-    # Check end date
-    assert (13.weeks.since(Time.zone.now - 6.weeks)).to_date == JSON.parse(last_response.body)[1]['end_date'].to_date
+    # Check last unit in Units (created in seed.db)
+    actual_unit = JSON.parse(last_response.body)[1]
+    expected_unit = Unit.find(2)
+
+    assert_equal actual_unit['name'], expected_unit.name
+    assert_equal actual_unit['code'], expected_unit.code
+    assert_equal actual_unit['start_date'].to_date, expected_unit.start_date.to_date
+    assert_equal actual_unit['end_date'].to_date.to_date, expected_unit.end_date.to_date
   end
 
   # Test GET for getting a specific unit by id
@@ -117,29 +127,27 @@ class UnitsTest < MiniTest::Test
     # Test getting the first unit with id of 1
     get  "/api/units/1.json?auth_token=#{@auth_token}"
 
-    # Check to see if the first unit's name matches
-    assert_equal JSON.parse(last_response.body)['name'], 'Introduction to Programming'
-    # Check to see if the first unit's code matches
-    assert_equal JSON.parse(last_response.body)['code'], 'COS10001'
-    # Test time zones, need to use operator for date conversion.
-    # Check tart date
-    assert (Time.zone.now - 6.weeks).to_date, JSON.parse(last_response.body)['start_date'].to_date
-    # Check end date
-    assert (13.weeks.since(Time.zone.now - 6.weeks)).to_date, JSON.parse(last_response.body)['end_date'].to_date
+    actual_unit = JSON.parse(last_response.body)
+    expected_unit = Unit.find(1)
+
+    # Check to see if the first unit's match
+    assert_equal actual_unit['name'], expected_unit.name
+    assert_equal actual_unit['code'], expected_unit.code
+    assert_equal actual_unit['start_date'].to_date, expected_unit.start_date.to_date
+    assert_equal actual_unit['end_date'].to_date, expected_unit.end_date.to_date
 
     # Get response back from getting a unit by id
     # Test getting the first unit with id of 2
     get  "/api/units/2.json?auth_token=#{@auth_token}"
 
-    # Check to see if the first unit's name matches
-    assert_equal JSON.parse(last_response.body)['name'], 'Game Programming'
-    # Check to see if the first unit's code matches
-    assert_equal JSON.parse(last_response.body)['code'], 'COS30243'
-    # Test time zones, need to use operator for date conversion.
-    # Check tart date
-    assert (Time.zone.now - 6.weeks).to_date, JSON.parse(last_response.body)['start_date'].to_date
-    # Check end date
-    assert (13.weeks.since(Time.zone.now - 6.weeks)).to_date, JSON.parse(last_response.body)['end_date'].to_date
+    actual_unit = JSON.parse(last_response.body)
+    expected_unit = Unit.find(2)
+
+    # Check to see if the first unit's match
+    assert_equal actual_unit['name'], expected_unit.name
+    assert_equal actual_unit['code'], expected_unit.code
+    assert_equal actual_unit['start_date'].to_date, expected_unit.start_date.to_date
+    assert_equal actual_unit['end_date'].to_date, expected_unit.end_date.to_date
   end
   # End GET tests
   # --------------------------------------------------------------------------- #
@@ -149,42 +157,44 @@ class UnitsTest < MiniTest::Test
 
   def test_units_put
 
-    # First post a new unit and test it
-    post  '/api/units.json',
-          '{"unit":'                                    +
-            '{'                                         +
-            '"name":"Intro to Ethnic Studies",'         +
-            '"code":"ETS1011",'                         +
-            '"start_date":"2016-05-14T00:00:00.000Z",'  +
-            '"end_date":"2017-05-14T00:00:00.000Z"'     +
-            '},'                                        +
-          '"auth_token":' + '"' + @auth_token + '"'     +
-          '}', "CONTENT_TYPE" => 'application/json'
+    unit_to_update = Unit.new({
+      name: "Intro to Ethnic Studies",
+      code: "ETS1011",
+      start_date: "2016-05-14T00:00:00.000Z",
+      end_date: "2017-05-14T00:00:00.000Z",
+      description: "Woglyf"
+    })
+    unit_to_update.save!
 
-    unit_id = JSON.parse(last_response.body)['id']
+    actual_unit = unit_to_update
+    expected_unit = Unit.last
+    unit_id = unit_to_update.id
 
-    # Standard POST tests to ensure POST was complete.
-    assert_equal JSON.parse(last_response.body)['name'], 'Intro to Ethnic Studies'
-    assert_equal JSON.parse(last_response.body)['code'], 'ETS1011'
-    assert JSON.parse(last_response.body)['start_date'].to_date == '2016-05-14T00:00:00.000Z'.to_date
-    assert JSON.parse(last_response.body)['end_date'].to_date == '2017-05-14T00:00:00.000Z'.to_date
+    assert_equal expected_unit.name, actual_unit['name']
+    assert_equal expected_unit.code, actual_unit['code']
+    assert_equal expected_unit.start_date.to_date, actual_unit['start_date'].to_date
+    assert_equal expected_unit.end_date.to_date, actual_unit['end_date'].to_date
 
-    # Test the PUT of unit with unit_id and test updated values.
-    put  "/api/units/#{unit_id}.json",
-          '{"unit":'                                    +
-            '{'                                         +
-            '"name":"Intro to Pizza Crafting",'         +
-            '"code":"PZA1011",'                         +
-            '"start_date":"2017-05-14T00:00:00.000Z",'  +
-            '"end_date":"2018-05-14T00:00:00.000Z"'     +
-            '},'                                        +
-          '"auth_token":' + '"' + @auth_token + '"'     +
-          '}', "CONTENT_TYPE" => 'application/json'
-    # Check Name of second unit created
-    assert_equal JSON.parse(last_response.body)['name'], 'Intro to Pizza Crafting'
-    assert_equal JSON.parse(last_response.body)['code'], 'PZA1011'
-    assert JSON.parse(last_response.body)['start_date'].to_date == '2017-05-14T00:00:00.000Z'.to_date
-    assert JSON.parse(last_response.body)['end_date'].to_date == '2018-05-14T00:00:00.000Z'.to_date
+    data_to_put = add_auth_token({
+      unit: {
+        name: "Intro to Pizza Crafting",
+        code: "PZA1011",
+        start_date: "2017-05-14T00:00:00.000Z",
+        end_date: "2018-05-14T00:00:00.000Z",
+        description: "pizza lyf"
+      },
+    })
+    put "/api/units/#{unit_id}.json", data_to_put.to_json, "CONTENT_TYPE" => 'application/json'
+
+    actual_unit = JSON.parse(last_response.body)
+    expected_unit = data_to_put
+
+    puts actual_unit
+
+    assert_equal expected_unit.name, actual_unit['name']
+    assert_equal expected_unit.code, actual_unit['code']
+    assert_equal expected_unit['start_date'].to_date, actual_unit['start_date'].to_date
+    assert_equal expected_unit['end_date'].to_date, actual_unit['end_date'].to_date
   end
   # End PUT tests
   # --------------------------------------------------------------------------- #
