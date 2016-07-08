db5ec6a7ac4b0c48775f06677c732efb8c43380a
Alex Cummaudo <alexcu@me.com>
2016-07-08 15:31:31 +1000

    CONFIG: Update Swagger configuration to support information

--- app/api/api.rb
+++ app/api/api.rb
@@ -52,8 +52,15 @@ module Api
     AuthHelpers.add_auth_to Api::Submission::PortfolioEvidenceApi
     AuthHelpers.add_auth_to Api::Submission::BatchTask
 
-    add_swagger_documentation base_path: "",
-                          # api_version: 'api',
-                  hide_documentation_path: true
+    add_swagger_documentation \
+      base_path: "http://localhost:3000/",
+      add_version: false,
+      hide_documentation_path: true,
+      info: {
+        title: "Doubtfire API Documentaion",
+        description: "Doubtfire is a modern, lightweight learning management system.",
+        license: "AGPL v3.0",
+        license_url: "https://github.com/doubtfire-lms/doubtfire-api/blob/master/LICENSE"
+      }
   end
 end
