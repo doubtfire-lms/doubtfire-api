52738a28bd4c65bde714cd3e85a59b52b9e663d9
Jake Renzella <j@kerenzella.com>
2016-07-25 13:45:29 +1000

    QUALITY: Ensure last_response_body helper is used for units_test

--- test/api/units_test.rb
+++ test/api/units_test.rb
@@ -36,7 +36,7 @@ class UnitsTest < ActiveSupport::TestCase
     post_json '/api/units.json', data_to_post
 
     # Check to see if the unit's name matches what was expected
-    actual_unit = JSON.parse(last_response.body)
+    actual_unit = last_response_body
 
     assert_equal expected_unit[:name], actual_unit['name']
     assert_equal expected_unit[:code], actual_unit['code']
@@ -76,16 +76,16 @@ class UnitsTest < ActiveSupport::TestCase
     #       '"auth_token":' + '"' + @auth_token + '"'         +
     #       '}', "CONTENT_TYPE" => 'application/json'
 
-    # puts JSON.parse(last_response.body)
+    # puts last_response_body
 
     # # Check to see if the unit's name matches what was expected
-    # assert_equal JSON.parse(last_response.body)['name'], 'Intro to Social Skills'
+    # assert_equal last_response_body['name'], 'Intro to Social Skills'
     # # Check to see if the unit's code matches what was expected
-    # assert_equal JSON.parse(last_response.body)['code'], 'JRRW40003'
+    # assert_equal last_response_body['code'], 'JRRW40003'
     # # Check to see if the unit's stat date matches what was expected
-    # assert_equal JSON.parse(last_response.body)['start_date'], '2016-05-14T00:00:00.000Z'
+    # assert_equal last_response_body['start_date'], '2016-05-14T00:00:00.000Z'
     # # Check to see if the unit's end date matches what was expected
-    # assert_equal JSON.parse(last_response.body)['end_date'], '2017-05-14T00:00:00.000Z'
+    # assert_equal last_response_body['end_date'], '2017-05-14T00:00:00.000Z'
   end
   # End POST tests
   # --------------------------------------------------------------------------- #
@@ -98,7 +98,7 @@ class UnitsTest < ActiveSupport::TestCase
     # The GET we are testing
     get with_auth_token "/api/units.json"
 
-    actual_unit = JSON.parse(last_response.body)[0]
+    actual_unit = last_response_body[0]
     expected_unit = Unit.first
     assert_equal expected_unit.name, actual_unit['name']
     assert_equal expected_unit.code, actual_unit['code']
@@ -106,7 +106,7 @@ class UnitsTest < ActiveSupport::TestCase
     assert_equal expected_unit.end_date.to_date, actual_unit['end_date'].to_date.to_date
 
     # Check last unit in Units (created in seed.db)
-    actual_unit = JSON.parse(last_response.body)[1]
+    actual_unit = last_response_body[1]
     expected_unit = Unit.find(2)
 
     assert_equal expected_unit.name, actual_unit['name']
@@ -122,7 +122,7 @@ class UnitsTest < ActiveSupport::TestCase
     # Test getting the first unit with id of 1
     get with_auth_token "/api/units/1.json"
 
-    actual_unit = JSON.parse(last_response.body)
+    actual_unit = last_response_body
     expected_unit = Unit.find(1)
 
     # Check to see if the first unit's match
@@ -135,7 +135,7 @@ class UnitsTest < ActiveSupport::TestCase
     # Test getting the first unit with id of 2
     get with_auth_token "/api/units/2.json"
 
-    actual_unit = JSON.parse(last_response.body)
+    actual_unit = last_response_body
     expected_unit = Unit.find(2)
 
     # Check to see if the first unit's match
@@ -213,7 +213,7 @@ class UnitsTest < ActiveSupport::TestCase
     # })
     # put "/api/units/#{unit_id}.json", data_to_put.to_json, "CONTENT_TYPE" => 'application/json'
     #
-    # actual_unit = JSON.parse(last_response.body)
+    # actual_unit = last_response_body
     # expected_unit = data_to_put
     #
     # puts actual_unit
