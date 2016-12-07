579f81b5f6bfe9af02ad85d441ca807b48411fdb
Jake Renzella <j@kerenzella.com>
2016-12-07 16:02:44 +1100

    TEST: (WIP) Add post tests for resource uploads

--- test/api/units/task_definitions_test.rb
+++ test/api/units/task_definitions_test.rb
@@ -10,8 +10,6 @@ class TaskDefinitionTest < ActiveSupport::TestCase
   end
 
   def test_post_invalid_file_tasksheet
-    # Get a random unit id to test
-    num_of_tasksheets = TaskDefinition.all.length
     test_unit = Unit.order('RANDOM()').first
     test_task_definition_id = test_unit.task_definitions.order('RANDOM()').first.id
 
@@ -22,20 +20,35 @@ class TaskDefinitionTest < ActiveSupport::TestCase
     post_json "/api/units/#{test_unit.id}/task_definitions/#{test_task_definition_id}/task_sheet", data_to_post
 
     assert last_response_body.key?('error')
-    assert_equal num_of_tasksheets, TaskDefinition.all.length
   end
 
   def test_post_tasksheet
-    num_of_tasksheets = TaskDefinition.all.length
-    test_unit = Unit.order('RANDOM()').first
+
+    test_unit = Unit.where(:administrators == User.first).order('RANDOM()').first
     test_task_definition_id = test_unit.task_definitions.order('RANDOM()').first.id
 
     data_to_post = {
       file: Rack::Test::UploadedFile.new('test_files/submissions/00_question.pdf', 'application/pdf')
     }
+
     post "/api/units/#{test_unit.id}/task_definitions/#{test_task_definition_id}/task_sheet", with_auth_token(data_to_post)
 
     assert_equal 201, last_response.status
-    # assert_equal num_of_tasksheets, TaskDefinition.all.length + 1
+  end
+
+  def test_post_task_resources
+    test_unit = Unit.where(:administrators == User.first).order('RANDOM()').first
+    tdid = test_unit.task_definitions.order('RANDOM()').first.id
+
+    data_to_post = {
+      file: Rack::Test::UploadedFile.new('test_files/2015-08-06-COS10001-acain.zip', 'application/zip')
+    }
+    post "/api/units/#{test_unit.id}/task_definitions/#{tdid}/task_resources", with_auth_token(data_to_post)
+
+    if last_response.status == 403
+      puts last_response_body
+    end
+
+    assert_equal 201, last_response.status
   end
 end
