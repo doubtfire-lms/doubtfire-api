7deb4fc7620aa81cc552389410b0246d57281783
Jake Renzella <j@kerenzella.com>
2016-12-07 17:48:02 +1100

    TEST: Add tests for tasks awaiting feedback

---
+++ test/api/units/feedback_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class FeedbackTest < ActiveSupport::TestCase
+  include Rack::Test::Methods
+  include TestHelpers::AuthHelper
+  include TestHelpers::JsonHelper
+
+  def app
+    Rails.application
+  end
+
+  def test_get_awaiting_feedback
+    random_unitrole = UnitRole.tutors.sample
+    unit = random_unitrole.unit
+    user = random_unitrole.user
+
+    expected_response = unit.tasks_awaiting_feedback
+
+    get with_auth_token "/api/units/#{unit.id}/feedback", user
+
+    assert_equal 200, last_response.status
+
+    # check each is the same
+    last_response_body.zip(expected_response).each do |response, expected|
+      assert_json_matches_model response, expected, ['id']
+    end
+  end
+end
