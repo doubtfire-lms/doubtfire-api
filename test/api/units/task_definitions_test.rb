cf80783f565b13513fd2b9ae9834a9f6c555a60c
Jake Renzella <j@kerenzella.com>
2016-12-09 11:07:04 +1100

    TEST: Add additional assertions for task_definitions

--- test/api/units/task_definitions_test.rb
+++ test/api/units/task_definitions_test.rb
@@ -23,27 +23,29 @@ class TaskDefinitionTest < ActiveSupport::TestCase
   end
 
   def test_post_tasksheet
-
-    test_unit = Unit.where(:administrators == User.first).order('RANDOM()').first
-    test_task_definition_id = test_unit.task_definitions.order('RANDOM()').first.id
+    test_unit = Unit.first
+    test_task_definition = TaskDefinition.first
 
     data_to_post = {
       file: Rack::Test::UploadedFile.new('test_files/submissions/00_question.pdf', 'application/pdf')
     }
 
-    post "/api/units/#{test_unit.id}/task_definitions/#{test_task_definition_id}/task_sheet", with_auth_token(data_to_post)
+    post "/api/units/#{test_unit.id}/task_definitions/#{test_task_definition.id}/task_sheet", with_auth_token(data_to_post)
 
     assert_equal 201, last_response.status
+    assert test_task_definition.task_sheet
+
+    assert_equal File.size('test_files/submissions/00_question.pdf'), File.size(TaskDefinition.first.task_sheet)
   end
 
   def test_post_task_resources
-    test_unit = Unit.where(:administrators == User.first).order('RANDOM()').first
-    tdid = test_unit.task_definitions.order('RANDOM()').first.id
+    test_unit_id = Unit.first.id
+    test_task_definition_id = TaskDefinition.first.id
 
     data_to_post = {
       file: Rack::Test::UploadedFile.new('test_files/2015-08-06-COS10001-acain.zip', 'application/zip')
     }
-    post "/api/units/#{test_unit.id}/task_definitions/#{tdid}/task_resources", with_auth_token(data_to_post)
+    post "/api/units/#{test_unit_id}/task_definitions/#{test_task_definition_id}/task_resources", with_auth_token(data_to_post)
 
     if last_response.status == 403
       puts last_response_body
