4d92357aeb61e011b3b85b416077f4fe8786de00
Jake Renzella <j@kerenzella.com>
2017-01-05 10:41:35 +1100

    FIX: Use correct values in methods

--- app/models/task_comment.rb
+++ app/models/task_comment.rb
@@ -4,5 +4,18 @@ class TaskComment < ActiveRecord::Base
 
   validates :task, presence: true
   validates :user, presence: true
+  validates :recipient, presence: true
+  validates :is_new, presence: true
   validates :comment, length: { minimum: 1, maximum: 4095, allow_blank: false }
+
+  def new_for?(user)
+    if !:is_new then return false
+    if (user.role_id != Role.student.id || user.role_id != Role.tutor.id) then return true
+    return user == :recipient
+  end
+
+  def recipient
+    return :task.project.student if :user == :task.project.main_tutor
+    return :task.project.main_tutor
+  end
 end
