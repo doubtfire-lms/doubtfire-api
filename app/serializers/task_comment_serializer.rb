0d591a951e2d139099204c103c9e1c8fbb8acfbe
Jake Renzella <j@kerenzella.com>
2017-01-06 10:39:24 +1100

    QUALITY: Remove recipient_id from comment serializer

--- app/serializers/task_comment_serializer.rb
+++ app/serializers/task_comment_serializer.rb
@@ -1,5 +1,5 @@
 class TaskCommentSerializer < ActiveModel::Serializer
-  attributes :id, :comment, :created_at, :comment_by, :recipient_id, :is_new
+  attributes :id, :comment, :created_at, :comment_by, :is_new
 
   def comment_by
     object.user.name
