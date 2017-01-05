a8f149e00c41e5787e93bcb2e6fb29930faf644d
Jake Renzella <j@kerenzella.com>
2017-01-06 10:21:01 +1100

    FIX: Add correct table names to migration

--- db/migrate/20170105032555_add_recipient_to_task_comment.rb
+++ db/migrate/20170105032555_add_recipient_to_task_comment.rb
@@ -1,9 +1,9 @@
 class AddRecipientToTaskComment < ActiveRecord::Migration
   def change
-    add_column :task_comment, :is_new, :boolean, default: true
+    add_column :task_comments, :is_new, :boolean, default: true
 
-    add_reference :task_comment, :recipient, references: :user
-    add_foreign_key :task_comment, :user, column: :recipient_id
+    add_reference :task_comments, :recipient, references: :users
+    add_foreign_key :task_comments, :users, column: :recipient_id
 
     TaskComment.update_all(is_new: false)
   end
