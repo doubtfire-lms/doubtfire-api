95e4e416eb1440ffdda22ef5547f8cf324ac822a
Jake Renzella <j@kerenzella.com>
2017-01-05 14:30:06 +1100

    FIX: Remove symbols to use values correctly

--- app/models/task_comment.rb
+++ app/models/task_comment.rb
@@ -9,13 +9,8 @@ class TaskComment < ActiveRecord::Base
   validates :comment, length: { minimum: 1, maximum: 4095, allow_blank: false }
 
   def new_for?(user)
-    if !:is_new then return false
-    if (user.role_id != Role.student.id || user.role_id != Role.tutor.id) then return true
-    return user == :recipient
-  end
-
-  def recipient
-    return :task.project.student if :user == :task.project.main_tutor
-    return :task.project.main_tutor
+    return false unless is_new
+    return true if user.role != Role.student || user.role != Role.tutor
+    user == recipient
   end
 end
