51329602438e9a85597f21147b44adec71d11174
Alex Cummaudo <alexcu@me.com>
2017-01-05 16:18:33 +1100

    CONFIG: Add staging to database and app config

---
+++ config/environments/staging.rb
@@ -0,0 +1,7 @@
+# Settings specified here will take precedence over those in config/application.rb
+require_relative 'production'
+Doubtfire::Application.configure do
+  # Staging uses production configuration, with minor changes to logging
+  # levels for extra information
+  config.log_level = :info
+end
