c7eed5b85c187ccc6c790083ad36b6eeab1414ed
Jake Renzella <j@kerenzella.com>
2017-01-06 10:01:45 +1100

    QUALITY: Add foreign key to migration

--- db/migrate/20170105032555_add_recipient_to_task_comment.rb
+++ db/migrate/20170105032555_add_recipient_to_task_comment.rb
@@ -2,9 +2,8 @@ class AddRecipientToTaskComment < ActiveRecord::Migration
   def change
     add_column :task_comment, :is_new, :boolean, default: true
 
-    # this is the same as add_reference but allows you to specify name of field. (recipient)
-    add_column :task_comment, :recipient, :integer
-    add_index :task_comment, :recipient
+    add_reference :task_comment, :recipient, references: :user
+    add_foreign_key :task_comment, :user, column: :recipient_id
 
     TaskComment.all.each do |task_comment|
       task_comment.is_new = false
