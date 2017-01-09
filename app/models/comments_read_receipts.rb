d5f996f8066679e316ec799497e32801e9b7b9ee
Jake Renzella <j@kerenzella.com>
2017-01-10 09:21:09 +1100

    QUALITY: Remove validation for created_at from model

--- app/models/comments_read_receipts.rb
+++ app/models/comments_read_receipts.rb
@@ -1,7 +1,6 @@
 class CommentsReadReceipts < ActiveRecord::Base
   validates :user, presence: true
   validates :task_comment, presence: true
-  validates :created_at, presence: true
 
   belongs_to :task_comment
   belongs_to :user
