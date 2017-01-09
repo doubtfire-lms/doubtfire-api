ef41a0deefee569cec8124d1b2aab3a655843ade
Alex Cummaudo <alexcu@me.com>
2017-01-09 15:13:49 +1100

    CONFIG: Set binding to 0.0.0.0 port 3000 on dev start
    To allow connections to all developmental devices, including
    mobile devices that want to connect.

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,17 @@
 require 'rubygems'
+require 'rails/commands/server'
+
+# Set default binding to 0.0.0.0 to allow connections from everyone if
+# under development
+if Rails.env.development?
+  module Rails
+    class Server
+      def default_options
+        super.merge(Host: '0.0.0.0', Port: 3000)
+      end
+    end
+  end
+end
 
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
