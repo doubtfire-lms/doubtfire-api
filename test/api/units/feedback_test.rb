110aeadb70a055efb4d91eb9576ddc6032f996a9
Jake Renzella <j@kerenzella.com>
2017-01-16 16:38:39 +1100

    TEST: Add new test for inbox endpoint

--- test/api/units/feedback_test.rb
+++ test/api/units/feedback_test.rb
@@ -10,13 +10,28 @@ class FeedbackTest < ActiveSupport::TestCase
   end
 
   def test_get_awaiting_feedback
-    random_unitrole = UnitRole.tutors.sample
     user = User.first
     unit = Unit.first
 
     expected_response = unit.tasks_awaiting_feedback(user)
 
     get with_auth_token "/api/units/#{unit.id}/feedback", user
+
+    assert_equal 200, last_response.status
+
+    # check each is the same
+    last_response_body.zip(expected_response).each do |response, expected|
+      assert_json_matches_model response, expected, ['id']
+    end
+  end
+
+  def test_tasks_for_task_inbox
+    user = User.first
+    unit = Unit.first
+
+    expected_response = unit.tasks_for_task_inbox(user)
+
+    get with_auth_token "/api/units/#{unit.id}/tasks/inbox", user
 
     assert_equal 200, last_response.status
 
