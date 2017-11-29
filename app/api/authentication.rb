ccd504c404e455d91050dbeed6a34301499c56f6
Andrew Cain <macite@gmail.com>
2017-11-29 16:50:41 +1100

    ENHANCE: Add ability to find user by username in aaf login

--- app/api/authentication.rb
+++ app/api/authentication.rb
@@ -129,6 +129,7 @@ module Api
         # Lookup using email otherwise and set login_id
         # Otherwise create new
         user = User.find_by(login_id: login_id) ||
+               User.find_by_username(email[/(.*)@/,1]) ||
                User.find_by(email: email) ||
                User.find_or_create_by(login_id: login_id) do |new_user|
                  role = Role.aaf_affiliation_to_role_id(attrs[:edupersonscopedaffiliation])
