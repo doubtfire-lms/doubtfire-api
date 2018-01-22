a538d5af3e25c2641829626f33e2594e71d7fee5
Akshat Bajaj <akshat.bajaj@outlook.com>
2018-01-22 15:14:10 +1100

    NEW: Comment Attachment Type

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180119124755) do
+ActiveRecord::Schema.define(version: 20180122022727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -227,6 +227,7 @@ ActiveRecord::Schema.define(version: 20180119124755) do
     t.string   "attachment_content_type"
     t.integer  "attachment_file_size"
     t.datetime "attachment_updated_at"
+    t.string   "content_type"
   end
 
   add_index "task_comments", ["task_id"], name: "index_task_comments_on_task_id", using: :btree
