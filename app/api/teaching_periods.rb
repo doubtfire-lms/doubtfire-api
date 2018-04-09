03ab9c473e53272d62d7fd420fa51570f950df74
Akshat Bajaj <akshat.bajaj@outlook.com>
2018-04-09 13:06:13 +1000

    NEW: Get all Teaching Periods

---
+++ app/api/teaching_periods.rb
@@ -0,0 +1,22 @@
+require 'grape'
+
+module Api
+    class TeachingPeriods < Grape::API
+        
+        
+        desc 'Get all the Teaching Periods'
+        get '/teaching_period_id' do
+            teaching_periods = teaching_period.all_teaching_periods
+            result = teaching_periods.map do |c|
+                {
+                    id: c.id,
+                    period: c.period,
+                    start_date: c.start_date,
+                    end_date: c.end_date,                    
+                }
+            end
+            result        
+        end
+
+    end    
+end
