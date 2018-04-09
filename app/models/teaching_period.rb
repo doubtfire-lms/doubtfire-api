5b49d1dfed0cc0fa03c4bf1b346462fe63719904
Akshat Bajaj <akshat.bajaj@outlook.com>
2018-04-09 12:13:17 +1000

    NEW: Add add_teaching_period method

--- app/models/teaching_period.rb
+++ app/models/teaching_period.rb
@@ -1,3 +1,11 @@
 class TeachingPeriod < ActiveRecord::Base
     has_many :units
+
+    def add_teaching_period (start_date, end_date)
+        period = self
+        period.period = self
+        period.start_date = start_date
+        period.end_date = end_date
+        period.save!
+    end
 end
