b5c96edf4a5a2ec247b923dd263d3e651c29134f
Akshat Bajaj <akshat.bajaj@outlook.com>
2018-04-29 18:44:15 +1000

    FIX: Teaching Period Indentation

--- app/models/teaching_period.rb
+++ app/models/teaching_period.rb
@@ -1,7 +1,7 @@
 class TeachingPeriod < ActiveRecord::Base
-    has_many :units
-    
-    validates :period, length: { minimum: 1, maximum: 10, allow_blank: false }  
-    validates :start_date, presence: true
-    validates :end_date, presence: true
+  has_many :units
+  
+  validates :period, length: { minimum: 1, maximum: 10, allow_blank: false }  
+  validates :start_date, presence: true
+  validates :end_date, presence: true
 end
