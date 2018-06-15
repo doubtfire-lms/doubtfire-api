50c66722938cfd8acc3a18081ab6ac3879fc7cbf
Andrew Cain <macite@gmail.com>
2018-06-15 11:11:21 +1000

    ENHANCE: Improve reliability of project task stats

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -194,12 +194,6 @@ class Unit < ActiveRecord::Base
     q = q.where('projects.enrolled = TRUE') if limit_to_enrolled
 
     q.map do |t|
-      red_pct = ((t.fail_count + t.do_not_resubmit_count + t.time_exceeded_count) / task_count[3]).signif(2)
-      orange_pct = ((t.redo_count + t.need_help_count + t.fix_and_resubmit_count) / task_count[3]).signif(2)
-      green_pct = ((t.discuss_count + t.demonstrate_count + t.complete_count) / task_count[3]).signif(2)
-      blue_pct = (t.ready_to_mark_count / task_count[3]).signif(2)
-      grey_pct = (1 - red_pct - orange_pct - green_pct - blue_pct).signif(2)
-
       {
         project_id: t.project_id,
         enrolled: t.enrolled,
@@ -215,7 +209,7 @@ class Unit < ActiveRecord::Base
         grade_rationale: t.grade_rationale,
         max_pct_copy: t.plagiarism_match_links_max_pct,
         has_portfolio: !t.portfolio_production_date.nil?,
-        stats: "#{red_pct}|#{grey_pct}|#{orange_pct}|#{blue_pct}|#{green_pct}"
+        stats: Project.create_task_stats_from(task_count, t, t.target_grade)
       }
     end
   end
