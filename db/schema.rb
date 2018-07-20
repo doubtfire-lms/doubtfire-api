d078b9b09b8ef08c04318bae1d34fb5154699562
Akshat Bajaj <akshat.bajaj@outlook.com>
2018-07-20 15:19:15 +1000

    NEW: Add year to teaching_period

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180405141539) do
+ActiveRecord::Schema.define(version: 20180720051403) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -315,6 +315,7 @@ ActiveRecord::Schema.define(version: 20180405141539) do
     t.string   "period",     null: false
     t.datetime "start_date", null: false
     t.datetime "end_date",   null: false
+    t.integer  "year",       null: false
   end
 
   create_table "teams", force: :cascade do |t|
