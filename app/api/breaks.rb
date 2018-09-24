ece5928d98c875d61ef34f32f473058860a6bb3d
Akshat Bajaj <akshat.bajaj@outlook.com>
2018-09-24 10:51:31 +1000

    NEW: Add break to a teaching period

--- app/api/breaks.rb
+++ app/api/breaks.rb
@@ -8,5 +8,24 @@ module Api
     before do
       authenticated?
     end
+
+    desc 'Add a new break to the teaching period'
+    params do
+      requires :start_date, type: Date, desc: 'The start date of the break'
+      requires :number_of_weeks, type: Integer, desc: 'Break duration'
+    end
+    post '/teaching_periods/:teaching_period_id/breaks' do
+      unless authorise? current_user, User, :handle_teaching_period
+        error!({ error: 'Not authorised to create a teaching period' }, 403)
+      end
+
+      # Find the Teaching Period to add break
+      teaching_period = TeachingPeriod.find(params[:teaching_period_id])
+
+      start_date = params[:start_date]
+      number_of_weeks = params[:number_of_weeks]
+
+      teaching_period.add_break(start_date, number_of_weeks)
+    end
   end
 end
