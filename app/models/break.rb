ca44a2263253f3c8c716185f6bd33ffd9ceb8b61
Akshat Bajaj <akshat.bajaj@outlook.com>
2018-09-24 11:16:17 +1000

    ENHANCE: Ensure break start date is within teaching period

--- app/models/break.rb
+++ app/models/break.rb
@@ -4,4 +4,12 @@ class Break < ActiveRecord::Base
   validates :start_date, presence: true
   validates :number_of_weeks, presence: true
   validates :teaching_period_id, presence: true
+
+  validate :ensure_start_date_is_within_teaching_period
+
+  def ensure_start_date_is_within_teaching_period
+    if start_date < teaching_period.start_date
+      errors.add(:start_date, "should be after the Teaching Period start date")
+    end
+  end
 end
