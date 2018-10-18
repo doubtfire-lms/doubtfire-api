202b9a67eee42eaa2d93352e9a61d057b448a012
Andrew Cain <macite@gmail.com>
2018-10-19 08:52:28 +1100

    TEST: Add test for unit ilo progress

---
+++ test/models/unit_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class UnitTest < ActiveSupport::TestCase
+  test 'ensure valid response from unit ilo data' do
+    unit = Unit.first
+    details = unit.ilo_progress_class_details
+
+    assert details.key?('all'), 'contains all key'
+
+    unit.tutorials.each do |tute|
+      assert details.key?(tute.id), 'contains tutorial keys'
+    end
+
+  end
+end
