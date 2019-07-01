44f611dfe96332fdfa581a5e67057170166b8871
Jake Renzella <j@kerenzella.com>
2019-07-01 20:37:00 +1000

    QUALITY: Refactor how discussion comments are served

--- app/serializers/discussion_comment_serializer.rb
+++ app/serializers/discussion_comment_serializer.rb
@@ -3,27 +3,4 @@ class DiscussionCommentSerializer < ActiveModel::Serializer
   def status
     object.status
   end
-
-    # created_at: "2019-06-28T08:41:24.471Z"
-    # id: 1
-    # task_comment_id: 33
-    # time_completed: null
-    # time_started: "2019-06-30T13:07:05.424Z"
-    # updated_at: "2019-06-30T13:07:05.425Z"
-
-  def author
-    {
-      id: object.user.id,
-      name: object.user.name,
-      email: object.user.email
-    }
-  end
-
-  def recipient
-    {
-      id: object.recipient.id,
-      name: object.recipient.name,
-      email: object.user.email
-    }
-  end
 end
