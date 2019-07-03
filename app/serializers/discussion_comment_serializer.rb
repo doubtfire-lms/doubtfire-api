efdb8035c6b87bb9ce0332ad9883e3535da0f345
Jake Renzella <j@kerenzella.com>
2019-07-03 13:58:07 +1000

    NEW: Mark AMS as deprecated

--- app/serializers/discussion_comment_serializer.rb
+++
@@ -1,6 +0,0 @@
-class DiscussionCommentSerializer < TaskCommentSerializer
-  attributes :id, :status, :time_discussion_completed, :time_discussion_started, :number_of_prompts
-  def status
-    object.status
-  end
-end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,5 @@
+# Doubtfire will deprecate ActiveModelSerializer in the future.
+# Instead, write a serialize method on the model.
 
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :student_id, :email, :name, :first_name, :last_name, :username, :nickname, :system_role, :receive_task_notifications, :receive_portfolio_notifications, :receive_feedback_notifications, :opt_in_to_research, :has_run_first_time_setup
