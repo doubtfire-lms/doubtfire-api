26b873af78b24f0bcbcae86ba8e03442d823f7e2
Sue Do <dmthu92@gmail.com>
2019-09-24 23:58:12 +1000

    TEST: add check for privacy statement

--- test/api/settings_test.rb
+++ test/api/settings_test.rb
@@ -11,7 +11,13 @@ class SettingsTest < ActiveSupport::TestCase
 
   def test_get_privacy_policy_details
     get 'api/settings/privacy'
+
+    expected_privacy = Doubtfire::Application.config.institution[:privacy]
+    expected_plagiarism = Doubtfire::Application.config.institution[:plagiarism]
+
     assert_equal 200, last_response.status
+    assert_equal expected_privacy, last_response_body['privacy']
+    assert_equal expected_plagiarism, last_response_body['plagiarism']
   end
 
   def test_get_config_details
