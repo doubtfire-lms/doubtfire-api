7c8ecf7a25186a52c87b69fa119436e842d1b7e1
jalqahtani <jalqahtani@deakin.edu.au>
2019-09-24 19:24:12 +1000

    TEST: Change unit tests for Break and Task Status

--- test/models/break_test.rb
+++ test/models/break_test.rb
@@ -18,4 +18,51 @@ class BreakTest < ActiveSupport::TestCase
     assert b1.valid?, "b1 not valid"
     assert_equal 1, tp.breaks.count
   end
+
+  def test_start_date_is_within_teaching_period
+    data = {
+      year: 2023,
+      period: 'T1',
+      start_date: Date.parse('2023-01-05'),
+      end_date: Date.parse('2023-02-05'),
+      active_until: Date.parse('2023-03-05')
+    }
+
+    tp = TeachingPeriod.create(data)
+
+    assert_raises ActiveRecord::RecordInvalid do
+        b1 = tp.add_break('2023-01-02', 1)
+    end 
+  end
+  
+ def test_end_date_is_within_teaching_period
+    data = {
+      year: 2023,
+      period: 'T1',
+      start_date: Date.parse('2023-01-01'),
+      end_date: Date.parse('2023-02-01'),
+      active_until: Date.parse('2023-03-01')
+    }
+
+    tp = TeachingPeriod.create(data)
+    assert_raises ActiveRecord::RecordInvalid do
+    b1 = tp.add_break('2023-01-02', 5)
+    end 
+ end
+ def test_monday_after_break
+     data = {
+      year: 2019,
+      period: 'T1',
+      start_date: Date.parse('2019-01-01'),
+      end_date: Date.parse('2019-02-01'),
+      active_until: Date.parse('2019-03-01')
+    }
+
+    tp = TeachingPeriod.create(data)
+    tp.add_break(tp.date_for_week(3), 1)
+    br = tp.breaks.first 
+
+    noWeeks = br.number_of_weeks.weeks
+    assert_equal br.monday_after_break, '2019-01-28'
+  end 
 end
--- test/models/task_status_test.rb
+++ test/models/task_status_test.rb
@@ -1,9 +1,157 @@
 require 'test_helper'
 
 class TaskStatusTest < ActiveSupport::TestCase
+  include Rack::Test::Methods
+  include TestHelpers::TestFileHelper
+  include TestHelpers::AuthHelper
+
+  def app
+    Rails.application
+  end
+  
   test 'ensure status matches id' do
     TaskStatus.all.each do |ts|
       assert_equal TaskStatus.id_to_key(ts.id), ts.status_key 
-    end
   end
+  end
+   
+  def test_status_chanaged_with_extenssion     
+  unit = Unit.first
+    td = TaskDefinition.new({
+        unit_id: unit.id,
+        name: 'Task past due - for revert',
+        description: 'Task past due',
+        weighting: 4,
+        target_grade: 0,
+        start_date: Time.zone.now - 2.weeks,
+        target_date: Time.zone.now - 1.week,
+        due_date: Time.zone.now + 1.week,
+        abbreviation: 'TaskPastDueForRevert',
+        restrict_status_updates: false,
+        upload_requirements: [ ],
+        plagiarism_warn_pct: 0.8,
+        is_graded: false,
+        max_quality_pts: 0
+      })
+    td.save! 
+  
+  # Get the first student - who now has this task
+    project = unit.active_projects.first
+  
+  data_to_post = {
+      trigger: 'ready_to_mark'
+    }
+  
+    # Make a submission for this student
+    post "/api/projects/#{project.id}/task_def_id/#{td.id}/submission", with_auth_token(data_to_post)
+
+    # Get the task... check it is now time exceeded
+    task = project.task_for_task_definition(td)
+    assert_equal TaskStatus.time_exceeded, task.task_status
+end
+def test_status_for_name    
+    assert_equal TaskStatus.status_for_name('complete').name,TaskStatus.complete.name
+    assert_equal TaskStatus.status_for_name('fix_and_resubmit').name,TaskStatus.fix_and_resubmit.name
+    assert_equal TaskStatus.status_for_name('fix and resubmit').name,TaskStatus.fix_and_resubmit.name
+    assert_equal TaskStatus.status_for_name('fix').name,TaskStatus.fix_and_resubmit.name
+    assert_raise NoMethodError do
+      assert_equal TaskStatus.status_for_name('f').name,TaskStatus.fix.name
+    end
+    
+    assert_equal TaskStatus.status_for_name('do_not_resubmit').name,TaskStatus.do_not_resubmit.name
+    assert_equal TaskStatus.status_for_name('do not resubmit').name,TaskStatus.do_not_resubmit.name
+    assert_equal TaskStatus.status_for_name('redo').name,TaskStatus.redo.name
+
+    assert_equal TaskStatus.status_for_name('need_help').name,TaskStatus.need_help.name
+    assert_equal TaskStatus.status_for_name('need help').name,TaskStatus.need_help.name
+    assert_equal TaskStatus.status_for_name('working_on_it').name,TaskStatus.working_on_it.name
+    assert_equal TaskStatus.status_for_name('working on it').name,TaskStatus.working_on_it.name
+    assert_equal TaskStatus.status_for_name('discuss').name,TaskStatus.discuss.name
+    assert_equal TaskStatus.status_for_name('d').name,TaskStatus.discuss.name
+        
+    assert_equal TaskStatus.status_for_name('demonstrate').name,TaskStatus.demonstrate.name
+    assert_equal TaskStatus.status_for_name('demo').name,TaskStatus.demonstrate.name
+    assert_equal TaskStatus.status_for_name('ready to mark').name,TaskStatus.ready_to_mark.name
+    assert_equal TaskStatus.status_for_name('ready_to_mark').name,TaskStatus.ready_to_mark.name 
+    assert_equal TaskStatus.status_for_name('rtm').name,TaskStatus.ready_to_mark.name
+
+    assert_equal TaskStatus.status_for_name('fail').name,TaskStatus.fail.name
+    assert_equal TaskStatus.status_for_name('not_started').name,TaskStatus.not_started.name
+    assert_equal TaskStatus.status_for_name('not started').name,TaskStatus.not_started.name 
+    assert_equal TaskStatus.status_for_name('ns').name,TaskStatus.not_started.name 
+    assert_equal TaskStatus.status_for_name('time exceeded').name,TaskStatus.time_exceeded.name
+    assert_equal TaskStatus.status_for_name('time_exceeded').name,TaskStatus.time_exceeded.name
+    assert_equal TaskStatus.status_for_name(''), nil       
+end 
+
+def test_staff_assigned_statuses
+  unit = Unit.first
+  td = TaskDefinition.new({
+        unit_id: unit.id,
+        name: 'Task past due - for revert',
+        description: 'Task past due',
+        weighting: 4,
+        target_grade: 0,
+        start_date: Time.zone.now - 2.weeks,
+        target_date: Time.zone.now - 1.week,
+        due_date: Time.zone.now + 1.week,
+        abbreviation: 'TaskPastDueForRevert',
+        restrict_status_updates: false,
+        upload_requirements: [ ],
+        plagiarism_warn_pct: 0.8,
+        is_graded: false,
+        max_quality_pts: 0
+      })
+    td.save! 
+  
+  # Get the first student - who now has this task
+  project = unit.active_projects.first
+  
+  data_to_post = {
+      trigger: 'ready_to_mark'
+    }
+  
+    # Make a submission for this student
+    post "/api/projects/#{project.id}/task_def_id/#{td.id}/submission", with_auth_token(data_to_post)
+
+    # Get the task... check it is now time exceeded
+  task = project.task_for_task_definition(td) 
+  assert_equal TaskStatus.find(task.task_status.id), task.task_status 
+end  
+
+def test_staff_assigned_statuses 
+  assert_equal TaskStatus.staff_assigned_statuses.count,8 # number of staff tasks 
+end  
+
+def test_status_key
+  unit = Unit.first
+  td = TaskDefinition.new({
+        unit_id: unit.id,
+        name: 'Task past due - for revert',
+        description: 'Task past due',
+        weighting: 4,
+        target_grade: 0,
+        start_date: Time.zone.now + 2.weeks,
+        target_date: Time.zone.now + 3.week,
+        due_date: Time.zone.now + 1.week,
+        abbreviation: 'UpcomingTask',
+        restrict_status_updates: false,
+        upload_requirements: [ ],
+        plagiarism_warn_pct: 0.8,
+        is_graded: false,
+        max_quality_pts: 0
+      })
+    td.save! 
+  
+  # Get the first student - who now has this task
+  project = unit.active_projects.first
+  task = project.task_for_task_definition(td) 
+  status = task.task_status
+
+  assert_equal status.status_key, :not_started
+end 
+
+def test_id_to_key_not_started
+  assert_equal TaskStatus.id_to_key(13), :not_started
+end 
 end
