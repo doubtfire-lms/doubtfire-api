27f822c63840d5935bc9f597e2735b2ca4890c47
AkshatBajaj <akshat.bajaj@outlook.com>
2019-10-09 09:08:47 +1100

    ENHANCE: Change allowed values in campus mode

--- app/api/campuses_authenticated_api.rb
+++ app/api/campuses_authenticated_api.rb
@@ -38,7 +38,7 @@ module Api
     params do
       requires :campus, type: Hash do
         optional :name, type: String, desc: 'The name of the campus'
-        optional :mode, type: String, values: ['physical', 'online'], desc: 'This will determine the campus mode'
+        optional :mode, type: String, values: ['timetable', 'automatic', 'manual'], desc: 'This will determine the campus mode'
       end
     end
     put '/campuses/:id' do
