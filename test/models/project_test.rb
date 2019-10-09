17c1892f67a39c1ef9873483ee1d6af5a07ead02
AkshatBajaj <akshat.bajaj@outlook.com>
2019-10-09 18:25:06 +1100

    TEST: Check campus is consistent

---
+++ test/models/project_test.rb
@@ -0,0 +1,39 @@
+require "test_helper"
+
+class ProjectTest < ActiveSupport::TestCase
+  def get_campus
+    data = {
+      name: 'Burwood',
+      mode: 'automatic',
+      abbreviation: 'B'
+    }
+
+    Campus.create!(data)
+  end
+
+  def test_campus_inconsistency_raises_error
+    campus = get_campus
+
+    project = Project.create!
+    project.tutorial = Tutorial.first
+    project.campus = campus
+    project.save
+
+    assert project.invalid?
+  end
+
+  def test_consistent_campus_is_allowed
+    campus = get_campus
+    tutorial = Tutorial.first
+
+    tutorial.campus = campus
+    tutorial.save!
+
+    project = Project.create!
+    project.tutorial = Tutorial.first
+    project.campus = campus
+    project.save!
+
+    assert project.valid?
+  end
+end
