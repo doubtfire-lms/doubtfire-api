0848a97c9f1c884e91ae32bf606b7e31892335eb
Andrew Cain <macite@gmail.com>
2019-10-10 21:21:37 +1100

    FIX: Fix name and ensure file helper test uses temp dir

--- test/models/file_helper_test.rb
+++ test/models/file_helper_test.rb
@@ -1,15 +1,14 @@
 require "test_helper"
 
 class FileHelperTest < ActiveSupport::TestCase
-  def test_convert_use
+  def test_convert_use_with_gif
     in_file = 'test_files/submissions/unbelievable.gif'
 
     Dir.mktmpdir do |dir|
-      dir = 'test_files/submissions/'
       dest_file = "#{dir}#{File.basename(in_file, ".*")}.jpg"
-      FileHelper.compress_image_to_dest in_file, dest_file, true
-      puts dest_file
+      assert FileHelper.compress_image_to_dest(in_file, dest_file, true)
       assert File.exists? dest_file
     end
+    puts "here"
   end
 end
