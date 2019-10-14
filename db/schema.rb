425a39973559e1afaea4014549f153ae76f1acfd
AkshatBajaj <akshat.bajaj@outlook.com>
2019-10-14 11:16:36 +1100

    ENHANCE: Remove activity type from unit sets

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191013232038) do
+ActiveRecord::Schema.define(version: 20191014001155) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -417,9 +417,8 @@ ActiveRecord::Schema.define(version: 20191013232038) do
   add_index "tutorials", ["unit_role_id"], name: "index_tutorials_on_unit_role_id", using: :btree
 
   create_table "unit_activity_sets", force: :cascade do |t|
-    t.string   "activity_type", null: false
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer  "unit_id"
   end
 
