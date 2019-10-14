8fe64db1349991680c3e88e4a43439fde1d88832
AkshatBajaj <akshat.bajaj@outlook.com>
2019-10-14 14:02:26 +1100

    ENHANCE: Validate the presence of campus and unit activity set

--- app/models/campus_activity_set.rb
+++ app/models/campus_activity_set.rb
@@ -1,2 +1,7 @@
 class CampusActivitySet < ActiveRecord::Base
+  belongs_to :campus
+  belongs_to :unit_activity_set
+
+  validates :campus,            presence: true
+  validates :unit_activity_set, presence: true
 end
