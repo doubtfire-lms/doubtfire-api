ab12a30075ddd199539ddbe6a671568497b06757
AkshatBajaj <akshat.bajaj@outlook.com>
2019-10-14 14:13:32 +1100

    ENHANCE: Check whether Activity type can be destroyed

--- app/models/activity_type.rb
+++ app/models/activity_type.rb
@@ -2,6 +2,14 @@ class ActivityType < ActiveRecord::Base
   # Relationships
   has_many :unit_activity_sets
 
+  before_destroy :can_destroy?
+
   validates :name,         presence: true
   validates :abbreviation, presence: true
+
+  def can_destroy?
+    return true if unit_activity_sets.count == 0
+    errors.add :base, "Cannot delete ActivityType with unit_activity_sets"
+    false
+  end
 end
