1fb04dab39f41e3bd5c54fe6795f27a190f48ae7
AkshatBajaj <akshat.bajaj@outlook.com>
2019-10-16 11:45:42 +1100

    ENHANCE: Use factory girl for project test

--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -1,18 +1,8 @@
 require "test_helper"
 
 class ProjectTest < ActiveSupport::TestCase
-  def get_campus
-    data = {
-      name: 'Melbourne',
-      mode: 'automatic',
-      abbreviation: 'Melb'
-    }
-
-    Campus.create!(data)
-  end
-
   def test_campus_inconsistency_raises_error
-    campus = get_campus
+    campus = FactoryGirl.create(:campus)
 
     project = Project.create!
     project.tutorial = Tutorial.first
@@ -23,7 +13,7 @@ class ProjectTest < ActiveSupport::TestCase
   end
 
   def test_consistent_campus_is_allowed
-    campus = get_campus
+    campus = FactoryGirl.create(:campus)
     tutorial = Tutorial.first
 
     tutorial.campus = campus
