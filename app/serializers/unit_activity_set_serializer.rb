4c805085cd04e1ff7a62b58dc3e62d27ecf5dce0
AkshatBajaj <akshat.bajaj@outlook.com>
2019-10-22 12:31:16 +1100

    ENHANCE: Return tutorials with unit activity sets

--- app/serializers/unit_activity_set_serializer.rb
+++ app/serializers/unit_activity_set_serializer.rb
@@ -1,4 +1,4 @@
 class UnitActivitySetSerializer < ActiveModel::Serializer
   attributes :id, :unit_id, :activity_type_id
-  has_many :campus_activity_sets
+  has_many :campus_activity_sets, :tutorials
 end
