c599f37a3b4a352a8226d0914a42a732ad55762e
AkshatBajaj <akshat.bajaj@outlook.com>
2019-10-25 15:29:16 +1100

    ENHANCE: Add validations to tutorial enrolment

---
+++ app/models/tutorial_enrolment.rb
@@ -0,0 +1,10 @@
+class TutorialEnrolment < ActiveRecord::Base
+  belongs_to :tutorial
+  belongs_to :project
+
+  validates :tutorial, presence: true
+  validates :project,  presence: true
+
+  # Always add a unique index to the DB to prevent new records from passing the validations when checked at the same time before being written
+  validates_uniqueness_of :tutorial, :scope => :project, message: 'already exists for the selected student'
+end
