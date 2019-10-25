ff95ead1c2531ff723bc982e15ac2f55b50ef657
AkshatBajaj <akshat.bajaj@outlook.com>
2019-10-25 13:45:22 +1100

    TEST: Check put activity type endpoint

--- test/api/activity_types_test.rb
+++ test/api/activity_types_test.rb
@@ -35,4 +35,19 @@ class ActivityTypesTest < ActiveSupport::TestCase
     activity_type = ActivityType.find(last_response_body['id'])
     assert_json_matches_model(last_response_body, activity_type, response_keys)
   end
+
+  def test_put_activity_types
+    data_to_put = {
+      activity_type: FactoryGirl.build(:activity_type),
+      auth_token: auth_token
+    }
+
+    # Update activity_type with id = 1
+    put_json '/api/activity_types/1', data_to_put
+    assert_equal 200, last_response.status
+
+    response_keys = %w(name abbreviation)
+    first_activity_type = ActivityType.first
+    assert_json_matches_model(last_response_body, first_activity_type, response_keys)
+  end
 end
