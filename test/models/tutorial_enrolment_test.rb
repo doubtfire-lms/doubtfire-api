0fad675e102e6ed21ec996922ab1277acb38c818
AkshatBajaj <akshat.bajaj@outlook.com>
2019-10-28 14:01:13 +1100

    NEW: Add tutorial enrolment model test

---
+++ test/models/tutorial_enrolment_test.rb
@@ -0,0 +1,17 @@
+require "test_helper"
+
+class TutorialEnrolmentTest < ActiveSupport::TestCase
+  def test_default_create
+    tutorial_enrolment = FactoryGirl.create(:tutorial_enrolment)
+    assert tutorial_enrolment.valid?
+  end
+
+  def test_specific_create
+    project = FactoryGirl.create(:project)
+    tutorial = FactoryGirl.create(:tutorial)
+    tutorial_enrolment = FactoryGirl.create(:tutorial_enrolment, project: project, tutorial: tutorial)
+    assert_equal tutorial_enrolment.project, project
+    assert_equal tutorial_enrolment.tutorial, tutorial
+    assert tutorial_enrolment.valid?
+  end
+end
