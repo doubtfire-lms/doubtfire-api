88120574342c029ea60920dd9e58041b102f4ae7
AkshatBajaj <akshat.bajaj@outlook.com>
2019-10-28 12:19:25 +1100

    NEW: Create tutorial and project factory

---
+++ test/factories/tutorials_factory.rb
@@ -0,0 +1,12 @@
+require 'faker'
+
+FactoryGirl.define do
+  factory :tutorial do
+    meeting_day               "Monday"
+    meeting_time              "17:30"
+    meeting_location          "ATC101"
+    sequence(:abbreviation)   { |n| "T#{n}" }
+    unit
+    campus
+  end
+end
