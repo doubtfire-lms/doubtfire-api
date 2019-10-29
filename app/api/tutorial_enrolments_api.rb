1660d2a071145b882dea502da90c871e06522fe8
AkshatBajaj <akshat.bajaj@outlook.com>
2019-10-29 11:29:52 +1100

    QUALITY: Remove unused tutorial enrolment endpoints

--- app/api/tutorial_enrolments_api.rb
+++ app/api/tutorial_enrolments_api.rb
@@ -28,26 +28,6 @@ module Api
       end
     end
 
-    desc 'Get all the enrolments in the tutorial'
-    get '/tutorials/:tutorial_id/enrolments' do
-      tutorial = Tutorial.find(params[:tutorial_id])
-      unless authorise? current_user, tutorial.unit, :get_students
-        error!({ error: 'Not authorised to get enrolments for the selected tutorial' }, 403)
-      end
-
-      tutorial.tutorial_enrolments
-    end
-
-    desc 'Get specific enrolment in the tutorial'
-    get '/tutorials/:tutorial_id/enrolments/:id' do
-      tutorial = Tutorial.find(params[:tutorial_id])
-      unless authorise? current_user, tutorial.unit, :get_students
-        error!({ error: 'Not authorised to get enrolments for the selected tutorial' }, 403)
-      end
-
-      tutorial.tutorial_enrolments.find(params[:id])
-    end
-
     desc 'Delete an enrolment in the tutorial'
     delete '/tutorials/:tutorial_id/enrolments/:id' do
       tutorial = Tutorial.find(params[:tutorial_id])
