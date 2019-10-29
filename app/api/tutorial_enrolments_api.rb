0088cf80c63be3d4304e86d71ff6ad243db1eafe
AkshatBajaj <akshat.bajaj@outlook.com>
2019-10-29 11:59:26 +1100

    FIX: Use unit for enroling project in a tutorial

--- app/api/tutorial_enrolments_api.rb
+++ app/api/tutorial_enrolments_api.rb
@@ -10,16 +10,17 @@ module Api
     end
 
     desc 'Enrol project in a tutorial'
-    params do
-      requires :project_id,  type: Integer,  desc: 'The id of the project to enrol'
-    end
-    post '/tutorials/:tutorial_id/enrolments' do
-      tutorial = Tutorial.find(params[:tutorial_id])
-      unless authorise? current_user, tutorial.unit, :enrol_student
+    post '/units/:unit_id/tutorials/:tutorial_abbr/enrolments/:project_id' do
+      unit = Unit.find(params[:unit_id])
+      unless authorise? current_user, unit, :enrol_student
         error!({ error: 'Not authorised to enrol student' }, 403)
       end
 
+      tutorial = unit.tutorials.find_by(abbreviation: params[:tutorial_abbr])
+      error!({ error: "No tutorial with abbreviation #{params[:tutorial_abbr]} exists for the unit" }, 403) unless tutorial.present?
+
       project = Project.find(params[:project_id])
+      result = project.enrol_in(tutorial)
 
       if result.nil?
         error!({ error: 'No enrolment added' }, 403)
