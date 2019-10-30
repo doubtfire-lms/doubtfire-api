a3329b73761797835673f14bbe622f2d679e3393
AkshatBajaj <akshat.bajaj@outlook.com>
2019-10-30 13:37:29 +1100

    TEST: Check add tutorial stream

--- test/models/tutorial_stream_model_test.rb
+++ test/models/tutorial_stream_model_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class TutorialStreamTest < ActiveSupport::TestCase
+class TutorialStreamModelTest < ActiveSupport::TestCase
   def test_default_create
     tutorial_stream = FactoryGirl.create(:tutorial_stream)
     last_tutorial_stream = TutorialStream.last
@@ -14,6 +14,16 @@ class TutorialStreamTest < ActiveSupport::TestCase
     assert_equal(tutorial_stream.name, 'Seminar-01')
     assert_equal tutorial_stream.abbreviation, 'sem-01'
     assert tutorial_stream.valid?
+    assert_equal tutorial_stream, last_tutorial_stream
+  end
+
+  def test_add_tutorial_stream
+    unit = FactoryGirl.create(:unit)
+    activity_type = FactoryGirl.create(:activity_type)
+    tutorial_stream = unit.add_tutorial_stream('Practical-01', 'prac-01', activity_type)
+    last_tutorial_stream = unit.tutorial_streams.last
+    assert tutorial_stream.valid?
+    assert_equal last_tutorial_stream, TutorialStream.last
     assert_equal tutorial_stream, last_tutorial_stream
   end
 end
