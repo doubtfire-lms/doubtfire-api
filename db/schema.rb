f1b0214336b0bb34479b85d66adccebce1200184
AkshatBajaj <akshat.bajaj@outlook.com>
2019-11-01 10:57:31 +1100

    ENHANCE: Add unique index to activity types

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191031233511) do
+ActiveRecord::Schema.define(version: 20191031235109) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,9 @@ ActiveRecord::Schema.define(version: 20191031233511) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
   end
+
+  add_index "activity_types", ["abbreviation"], name: "index_activity_types_on_abbreviation", unique: true, using: :btree
+  add_index "activity_types", ["name"], name: "index_activity_types_on_name", unique: true, using: :btree
 
   create_table "badges", force: :cascade do |t|
     t.string   "name",                   limit: 255
