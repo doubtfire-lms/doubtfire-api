e2a137af36fdd41293689ce02311956e648c5c75
AkshatBajaj <akshat.bajaj@outlook.com>
2019-11-05 11:44:11 +1100

    ENHANCE: Check whether tutorial stream can be destroyed

--- app/models/tutorial_stream.rb
+++ app/models/tutorial_stream.rb
@@ -2,11 +2,36 @@ class TutorialStream < ActiveRecord::Base
   belongs_to :activity_type
   belongs_to :unit
 
-  has_many :task_definitions, -> { order 'start_date ASC, abbreviation ASC' }, dependent: :destroy
+  # Callbacks - methods called are private
+  before_destroy :can_destroy?
+
+  has_many :task_definitions, -> { order 'start_date ASC, abbreviation ASC' }
 
   # Always add a unique index with uniqueness constraint
   # This is to prevent new records from passing the validations when checked at the same time before being written
   validates :name,         presence: true, uniqueness: { scope: :unit, message: "%{value} already exists in this unit"}
   validates :abbreviation, presence: true, uniqueness: { scope: :unit, message: "%{value} already exists in this unit"}
   validates_inclusion_of :combine_all_tasks, :in => [true, false]
+
+  private
+  def can_destroy?
+    return true if task_definitions.empty?
+    if unit.tutorial_streams.count > 2
+      errors.add :base, "cannot be deleted as it has task definitions associated with it, and it is not the last (or second last) tutorial stream"
+      false
+    elsif unit.tutorial_streams.count.eql? 2
+      other_tutorial_stream = (self.eql? unit.tutorial_streams.first) ? unit.tutorial_streams.second : unit.tutorial_streams.first
+      task_definitions.each do |task_definition|
+        task_definition.tutorial_stream = other_tutorial_stream
+        task_definition.save!
+      end
+      true
+    elsif unit.tutorial_streams.count.eql? 1
+      task_definitions.each do |task_definition|
+        task_definition.tutorial_stream = nil
+        task_definition.save!
+      end
+      true
+    end
+  end
 end
--- test/models/tutorial_stream_model_test.rb
+++ test/models/tutorial_stream_model_test.rb
@@ -26,4 +26,34 @@ class TutorialStreamModelTest < ActiveSupport::TestCase
     assert_equal last_tutorial_stream, TutorialStream.last
     assert_equal tutorial_stream, last_tutorial_stream
   end
+
+  def test_delete_when_two_tutorial_stream
+    unit = FactoryGirl.create(:unit)
+    activity_type = FactoryGirl.create(:activity_type)
+    tutorial_stream_first = unit.add_tutorial_stream('Practical-01', 'prac-01', activity_type)
+    tutorial_stream_second = unit.add_tutorial_stream('Practical-02', 'prac-02', activity_type)
+
+    # Add task definition to first tutorial stream
+    task_def_first = unit.task_definitions.first
+    task_def_first.tutorial_stream = tutorial_stream_first
+    task_def_first.save!
+
+    # Add task definition to second tutorial stream
+    task_def_second = unit.task_definitions.second
+    task_def_second.tutorial_stream = tutorial_stream_second
+    task_def_second.save!
+
+    assert_equal task_def_first, tutorial_stream_first.task_definitions.first
+    assert_equal 1, tutorial_stream_first.task_definitions.count
+    assert_equal task_def_second, tutorial_stream_second.task_definitions.first
+    assert_equal 1, tutorial_stream_second.task_definitions.count
+
+    tutorial_stream_first.destroy
+    assert tutorial_stream_first.destroyed?
+
+    assert_equal 1, unit.tutorial_streams.count
+    assert_equal 2, unit.tutorial_streams.first.task_definitions.count
+    assert_equal task_def_first, unit.tutorial_streams.first.task_definitions.first
+    assert_equal task_def_second, unit.tutorial_streams.first.task_definitions.second
+  end
 end
