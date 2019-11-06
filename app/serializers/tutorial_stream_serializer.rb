3ddd08f957a2d285baa27e3db65be073b3bd85a9
AkshatBajaj <akshat.bajaj@outlook.com>
2019-11-07 10:53:47 +1100

    NEW: Tutorial Stream Serializer

---
+++ app/serializers/tutorial_stream_serializer.rb
@@ -0,0 +1,11 @@
+# Doubtfire will deprecate ActiveModelSerializer in the future.
+# Instead, write a serialize method on the model.
+
+class TutorialStreamSerializer < ActiveModel::Serializer
+  attributes :id, :name, :abbreviation, :unit_id,
+             :activity_type
+
+  def activity_type
+    object.activity_type.abbreviation
+  end
+end
