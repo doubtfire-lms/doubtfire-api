042df7e93b1b42f4a5796a921ef0681398fb176d
Akshat Bajaj <akshat.bajaj@outlook.com>
2019-11-17 12:01:56 +1100

    FIX: Get the campus in the tutorial enrolment factory

--- test/factories/tutorial_enrolments_factory.rb
+++ test/factories/tutorial_enrolments_factory.rb
@@ -1,6 +1,9 @@
 FactoryGirl.define do
   factory :tutorial_enrolment do
-    tutorial
-    project
+    # Get the campus to make sure it is same
+    campus = FactoryGirl.build(:campus)
+
+    association :tutorial, factory: :tutorial, campus: campus, strategy: :build
+    association :project, factory: :project, campus: campus, strategy: :build
   end
 end
