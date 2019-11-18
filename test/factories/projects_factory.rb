b69d304f05ebcb6b5299246e838a98e50bd7b835
AkshatBajaj <akshat.bajaj@outlook.com>
2019-11-19 10:49:57 +1100

    FIX: Max one validation when tutorial stream is null

--- test/factories/projects_factory.rb
+++ test/factories/projects_factory.rb
@@ -3,10 +3,5 @@ FactoryGirl.define do
     unit
     campus
     user
-
-    after(:create) do |project|
-      tutorial = FactoryGirl.create(:tutorial, campus: project.campus)
-      create_list(:tutorial_enrolment, 1, project: project, tutorial: tutorial)
-    end
   end
 end
