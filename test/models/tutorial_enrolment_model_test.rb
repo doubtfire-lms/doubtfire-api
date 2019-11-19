d8c96362ad0ce1bfbb14c8bdadfd1337b0507c95
AkshatBajaj <akshat.bajaj@outlook.com>
2019-11-19 11:29:54 +1100

    TEST: Changing from stream to no stream

--- test/models/tutorial_enrolment_model_test.rb
+++ test/models/tutorial_enrolment_model_test.rb
@@ -137,6 +137,35 @@ class TutorialEnrolmentModelTest < ActiveSupport::TestCase
     assert_equal tutorial_enrolment_first.id, tutorial_enrolment_second.id
   end
 
+  def test_changing_from_stream_to_no_stream
+    campus = FactoryGirl.create(:campus)
+    project = FactoryGirl.create(:project, campus: campus)
+
+    # Make sure there are no enrolments for the project
+    assert_empty project.tutorial_enrolments
+
+    # Create tutorial with tutorial stream
+    tutorial_stream = FactoryGirl.create(:tutorial_stream)
+    tutorial_first = FactoryGirl.create(:tutorial, tutorial_stream: tutorial_stream, campus: campus)
+    assert_not_nil tutorial_first.tutorial_stream
+
+    # Create tutorial with no tutorial stream
+    tutorial_second = FactoryGirl.create(:tutorial, campus: campus)
+    assert_nil tutorial_second.tutorial_stream
+
+    # Enrol project in tutorial first
+    tutorial_enrolment_first = project.enrol_in(tutorial_first)
+    assert_equal tutorial_first, tutorial_enrolment_first.tutorial
+
+    # Enrol same project in tutorial second
+    tutorial_enrolment_second = FactoryGirl.build(:tutorial_enrolment, project: project, tutorial: tutorial_second)
+    assert tutorial_enrolment_second.invalid?
+    assert_equal 'Project cannot enrol in tutorial with no stream when enrolled in stream', tutorial_enrolment_second.errors.full_messages.last
+
+    exception = assert_raises(Exception) { tutorial_enrolment_second = project.enrol_in(tutorial_second) }
+    assert_equal 'Validation failed: Project cannot enrol in tutorial with no stream when enrolled in stream', exception.message
+  end
+
   def test_cannot_enrol_in_tutorial_stream_twice
     campus = FactoryGirl.create(:campus)
     project = FactoryGirl.create(:project, campus: campus)
