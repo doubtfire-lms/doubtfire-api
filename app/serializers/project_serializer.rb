ef0977d57e584968a84ae8d032e639ae58d66418
AkshatBajaj <akshat.bajaj@outlook.com>
2019-11-21 15:56:04 +1100

    FIX: Remove tutor name from project serialize

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -7,7 +7,7 @@ require 'task_serializer'
 class ShallowProjectSerializer < ActiveModel::Serializer
   attributes :unit_id, :unit_code, :unit_name,
              :project_id, :campus_id, :student_name,
-             :tutor_name, :target_grade,
+             :target_grade,
              :has_portfolio, :start_date, :end_date,
              :teaching_period_id, :active
 
@@ -32,7 +32,6 @@ class ProjectSerializer < ActiveModel::Serializer
              :started,
              :stats,
              :student_name,
-             :tutor_name,
              :burndown_chart_data,
              :enrolled,
              :target_grade,
@@ -55,10 +54,6 @@ class ProjectSerializer < ActiveModel::Serializer
 
   def student_id
     object.student.username
-  end
-
-  def tutor_name
-    object.main_tutor.first_name unless object.main_tutor.nil?
   end
 
   def stats
