5baf617858ad42ac06b0411e087e687d36988603
Akash Agarwal <virusofminds@gmail.com>
2019-11-26 17:10:36 +1100

    ENHANCE: Retrieve only the last 10 timestamps

--- app/api/submission/portfolio_evidence_api.rb
+++ app/api/submission/portfolio_evidence_api.rb
@@ -133,7 +133,7 @@ module Api
           error!({ error: "No submissions found for project: '#{params[:id]}' task: '#{params[:task_def_id]}'" }, 401)
         end
 
-        { result: FileHelper.sorted_timestamp_entries_in_dir(path) }
+        { result: FileHelper.sorted_timestamp_entries_in_dir(path).first(10) }
       end
 
       desc 'Get the result of the submission of a task made at the given timestamp'
