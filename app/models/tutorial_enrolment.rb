d336a42a03e9f352bfc3452c816d295ed9b44755
AkshatBajaj <akshat.bajaj@outlook.com>
2019-11-27 10:45:16 +1100

    ENHANCE: Add tutorial stream to tutorial enrolments

--- app/models/tutorial_enrolment.rb
+++ app/models/tutorial_enrolment.rb
@@ -1,12 +1,14 @@
 class TutorialEnrolment < ActiveRecord::Base
   belongs_to :tutorial
   belongs_to :project
+  belongs_to :tutorial_stream
 
   validates :tutorial, presence: true
   validates :project,  presence: true
 
   # Always add a unique index to the DB to prevent new records from passing the validations when checked at the same time before being written
   validates_uniqueness_of :tutorial, :scope => :project, message: 'already exists for the selected student'
+  validates_uniqueness_of :tutorial_stream, :scope => :project, message: 'already exists for the selected student'
 
   # Ensure that student cannot enrol in tutorial of different units
   validate :unit_must_be_same
