2683980a963f48478960f28ad22733ec1bf9104d
Guoheng Liao <liaogu@deakin.edu.au>
2019-12-20 20:44:50 +1100

    TEST: Add test to get privacy policy details

--- test/api/settings_test.rb
+++ test/api/settings_test.rb
@@ -26,4 +26,24 @@ class SettingTest < ActiveSupport::TestCase
         assert_equal expected_ProductName, returned_mes
     end     
 
+    # Get privacy policy details
+    def test_get_privacy_policy_details
+        expected_privacy = Doubtfire::Application.config.institution[:privacy]
+        expected_plagiarism = Doubtfire::Application.config.institution[:plagiarism]
+        
+        # Perform the GET
+        get '/api/settings/privacy'
+        
+        # Set two returned details
+        returned_privacy = last_response_body['privacy']
+        returned_plagiarism = last_response_body['plagiarism']
+
+        # Check if the call succeeds
+        assert_equal 200, last_response.status
+
+        # Check returned details match as expected
+        assert_equal expected_privacy, returned_privacy
+        assert_equal expected_plagiarism, returned_plagiarism
+    end
+
 end
