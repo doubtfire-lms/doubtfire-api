d1378f9554d2338db7540852a664bc220898ebbf
Andrew Cain <macite@gmail.com>
2019-12-29 20:50:26 +1100

    TEST: Add projects check for inactive units

--- test/api/projects_api_test.rb
+++ test/api/projects_api_test.rb
@@ -36,4 +36,30 @@ class ProjectsApiTest < ActiveSupport::TestCase
       assert_json_matches_model(data, project.unit, %w(teaching_period_id active))
     end
   end
+
+  def test_projects_works_with_inactive_units
+    user = FactoryGirl.create(:user, :student, enrol_in: 2)
+    Unit.last.update(active: false)
+
+    get with_auth_token('/api/projects', user)
+    assert_equal 1, last_response_body.count
+
+    get with_auth_token('/api/projects?include_inactive=false', user)
+    assert_equal 1, last_response_body.count
+
+    get with_auth_token('/api/projects?include_inactive=true', user)
+
+    assert_equal 2, last_response_body.count
+
+    last_response_body.each do |data|
+      project = user.projects.find(data['project_id'])
+      assert project.present?, data.inspect
+
+      assert_json_matches_model(data, project, %w(campus_id has_portfolio target_grade campus_id))
+      assert_equal project.unit.name, data['unit_name'], data.inspect
+      assert_equal project.unit.id, data['unit_id'], data.inspect
+      assert_equal project.unit.code, data['unit_code'], data.inspect
+      assert_json_matches_model(data, project.unit, %w(teaching_period_id active))
+    end
+  end
 end
