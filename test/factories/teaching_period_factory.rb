de59360112ef5c37b8d0d59e4d112ed3486b65d3
Linh Tran <hltr@deakin.edu.au>
2020-01-09 16:10:57 +1100

    QUALITY: Improve teaching period factory

--- test/factories/teaching_period_factory.rb
+++ test/factories/teaching_period_factory.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
   factory :teaching_period do
-    period       "6"
-    year      "2019"
-    start_date  Date.new(2019,3,4)
-    end_date  { start_date + rand(1..2).weeks }
-    active_until { end_date + rand(1..2).weeks }
+    sequence(:period)        { |n| "T#{n}" }
+    start_date               { Faker::Date.between(2.days.ago, Date.today) }
+    year                     { start_date.year }
+    end_date                 { start_date + 14.weeks }
+    active_until             { end_date + rand(1..2).weeks }
   end
 end
