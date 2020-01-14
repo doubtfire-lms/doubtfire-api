ca8e9c12e1c162874bd9293fd3368273407eb295
Samuel Anastassiades <sanastassiades@deakin.edu.au>
2020-01-15 10:35:03 +1100

    QAULITY: refactored code to use factory_girl, and address travis errors

--- test/factories/campuses_factory.rb
+++ test/factories/campuses_factory.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :campus do
+    id                  { rand(4..6)}
     name                { Faker::Educator.unique.campus }
     abbreviation        { name[0...9] }
     mode                { ['timetable', 'automatic', 'manual'].sample }
