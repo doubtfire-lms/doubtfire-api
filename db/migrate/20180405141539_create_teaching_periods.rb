b1753fe6b29e229025d787f28d842b8035e7a8d6
YI LU <fko@deakin.edu.au>
2020-01-15 19:12:49 +1100

    CONFIG: Add integer id in teching_periods

--- db/migrate/20180405141539_create_teaching_periods.rb
+++ db/migrate/20180405141539_create_teaching_periods.rb
@@ -1,6 +1,6 @@
 class CreateTeachingPeriods < ActiveRecord::Migration[4.2]
   def change
-    create_table :teaching_periods do |t|
+    create_table :teaching_periods, id: :integer do |t|
       t.string          :period,          null: false
       t.datetime        :start_date,      null: false
       t.datetime        :end_date,        null: false
