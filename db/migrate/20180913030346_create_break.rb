3b5ee0376a4cf5bb30b82265442868d6cb34982b
YI LU <fko@deakin.edu.au>
2020-01-15 17:52:24 +1100

    CONFIG: Try fix

--- db/migrate/20180913030346_create_break.rb
+++ db/migrate/20180913030346_create_break.rb
@@ -3,10 +3,9 @@ class CreateBreak < ActiveRecord::Migration[4.2]
     # PostgreSQL
     execute('ALTER TABLE breaks ALTER COLUMN teaching_period_id SET DATA TYPE BIGINT')
     # MySQL
-    execute('ALTER TABLE breaks MODIFY COLUMN teaching_period_id BIGINT NOT NULL AUTO_INCREMENT')
+    execute('ALTER TABLE breaks MODIFY COLUMN teaching_period_id BIGINT(20) NOT NULL AUTO_INCREMENT')
 
     create_table :breaks do |t|
-      t.bigint          :teaching_period_id,null: false
       t.datetime        :start_date,        null: false
       t.integer         :number_of_weeks,   null: false
     end
