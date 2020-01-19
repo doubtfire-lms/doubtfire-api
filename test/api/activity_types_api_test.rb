5898ffe7894e6d9cb800d5b5d2918ea52652fd71
Linh Tran <hltr@deakin.edu.au>
2020-01-20 10:23:20 +1100

    QUALITY: Adjust to use factorygirl data in test to create activity type

--- test/api/activity_types_api_test.rb
+++ test/api/activity_types_api_test.rb
@@ -23,17 +23,32 @@ class ActivityTypesApiTest < ActiveSupport::TestCase
     end
   end
 
+  # POST tests
+  # Create a new activity type
   def test_post_activity_types
+    # the number of teaching period before post
+    no_activity_type = ActivityType.count
+
+    # the data that we want to post/create
     data_to_post = {
       activity_type: FactoryGirl.build(:activity_type),
       auth_token: auth_token
     }
+    
+    # perform the POST
     post_json '/api/activity_types', data_to_post
+    
+    # check if the request get through 
     assert_equal 201, last_response.status
 
+    # check if the details posted match as expected
     response_keys = %w(name abbreviation)
     activity_type = ActivityType.find(last_response_body['id'])
     assert_json_matches_model(last_response_body, activity_type, response_keys)
+
+    # check if the details in the newly created match as pre-set data
+    assert_equal data_to_post[:activity_type]['name'], activity_type.name
+    assert_equal data_to_post[:activity_type]['abbreviation'], activity_type.abbreviation
   end
 
   def test_put_activity_types
