f8e6cd8e0c33858df0c2a893296685884f472e07
zhangshuhao <you@example.com>
2020-01-19 18:20:44 +1100

    QUALITY:(activity_types_api_test.rb)

--- test/api/activity_types_api_test.rb
+++ test/api/activity_types_api_test.rb
@@ -9,6 +9,18 @@ class ActivityTypesApiTest < ActiveSupport::TestCase
     Rails.application
   end
 
+  def test_get_an_activity_type_details
+
+    activity_type = FactoryGirl.create(:activity_type)
+    get "/api/activity_types/#{activity_type.id}"
+
+    activity_type_id = activity_type.id
+ 
+    activity_type_id = ActivityType.find(activity_type['id'])
+
+  end
+
+
   def test_get_all_activity_types
     get '/api/activity_types'
     expected_data = ActivityType.all
@@ -22,6 +34,10 @@ class ActivityTypesApiTest < ActiveSupport::TestCase
       assert_json_matches_model(data, activity_type, response_keys)
     end
   end
+
+
+
+
 
   def test_post_activity_types
     data_to_post = {
@@ -49,5 +65,29 @@ class ActivityTypesApiTest < ActiveSupport::TestCase
     response_keys = %w(name abbreviation)
     first_activity_type = ActivityType.first
     assert_json_matches_model(last_response_body, first_activity_type, response_keys)
+  end
+
+  def test_delect_activity_type
+    # Create a activity type
+    activity_type = FactoryGirl.create(:activity_type)
+
+    # number of activity type before delete
+    number_of_ativity_type = ActivityType.count
+    
+    delete_json with_auth_token "/api/activity_types/#{activity_type.id}"
+    assert_equal 200, last_response.status
+
+    # Check delete if success
+    assert_equal ActivityType.count, number_of_ativity_type-1
+  end
+
+  def test_delect_activity_type_not_auth
+    user = FactoryGirl.build(:user, :student)
+
+    activity_type = FactoryGirl.create(:activity_type)
+
+    delete_json with_auth_token("/api/activity_types/#{activity_type.id}", user)
+
+    assert_equal 403, last_response.status
   end
 end
