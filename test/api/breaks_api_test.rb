6692d745a6ccc94ccb4ca2eee93f149dc67d54e6
Areej Ali A Asiri <aasiri@deakin.edu.au>
2020-01-19 23:21:57 +1100

    TEST: Delete unnecessary GET Test Method for breaks_api_test

--- test/api/breaks_api_test.rb
+++ test/api/breaks_api_test.rb
@@ -9,20 +9,4 @@ def app
     Rails.application
 end
 
-# GET TEST 
-# Get All Breaks
-def test_get_all_breaks
-    user = User.admins.first
-    get with_auth_token('/api/breaks',user)
-    expected_data = Break.all
-    assert_equal expected_data.count, last_response_body.count
-    response_keys = %w(start_date number_of_weeks)
-    last_response_body.each do | data |
-        breaks = Break.find(data['id'])
-        
-        assert_json_matches_model(data, breaks, response_keys)
-    end 
-end
-
-
 end
