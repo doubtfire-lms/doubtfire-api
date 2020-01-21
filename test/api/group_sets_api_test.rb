090732bc30d600d4170b8a1b1f99dbb5b1572d4a
Thisara Wijekoon <greencartweb@gmail.com>
2020-01-21 22:31:02 +1100

    TEST: Add unit tests for group_sets_api

---
+++ test/api/group_sets_api_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class GroupSetsApiTest < ActiveSupport::TestCase
+  include Rack::Test::Methods
+  include TestHelpers::AuthHelper
+  include TestHelpers::JsonHelper
+  include TestHelpers::TestFileHelper
+
+  def app
+    Rails.application
+  end
+
+end
