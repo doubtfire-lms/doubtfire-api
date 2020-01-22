7d26915741b89926eb4bce358135f095270ef7f2
Guoheng Liao <liaogu@deakin.edu.au>
2020-01-22 20:48:07 +1100

    TEST: Add unit test for test post activity type cannot authorise

--- test/api/activity_types_api_test.rb
+++ test/api/activity_types_api_test.rb
@@ -50,4 +50,27 @@ class ActivityTypesApiTest < ActiveSupport::TestCase
     first_activity_type = ActivityType.first
     assert_json_matches_model(last_response_body, first_activity_type, response_keys)
   end
+
+  def test_post_activity_types_cannot_auth
+    # Number of Activity type before put new activity type
+    number_of_activity_type = ActivityType.count
+
+    # A user with student role which does not have premision to put a activity type
+    user = FactoryBot.create(:user, :student)
+
+    # Create a dummy activity type
+    data_to_post = {
+      activity_type: FactoryBot.build(:activity_type),
+      auth_token: auth_token
+    }
+
+    # Perform POST, but the student user does not have permissions to put it.
+    post_json '/api/activity_types', with_auth_token(data_to_post, user)
+
+    # Check if the put does not get through
+    assert_equal 403, last_response.status  
+
+    # Check if the number of activity type is the same as initially
+    assert_equal ActivityType.count, number_of_activity_type
+  end
 end
