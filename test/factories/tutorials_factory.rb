a6679f316aa08cb5c2ee833fae89952c4a1e7c3c
YI LU <fko@deakin.edu.au>
2020-01-24 16:14:06 +1100

    CONFIG: Add tutorials and campuses factory

---
+++ test/factories/tutorials_factory.rb
@@ -0,0 +1,14 @@
+require 'faker'
+
+FactoryBot.define do
+  factory :tutorial do
+    meeting_day               { "Monday" }
+    meeting_time              { "17:30" }
+    meeting_location          { "ATC101" }
+    sequence(:abbreviation)   { |n| "T#{n}" }
+    unit
+    campus
+    tutorial_stream           { nil }
+    unit_role                 { unit.staff.sample }
+  end
+end
