f1032180c595f76e69ebe598b36292bf45e890ec
Samuel Anastassiades <sanastassiades@deakin.edu.au>
2020-01-25 15:02:10 +1100

    QUALITY: removed unecessary white space; removed id assignment from campus facotry

--- test/factories/campuses_factory.rb
+++ test/factories/campuses_factory.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :campus do
-    sequence(:id, (4..50).cycle) { |n| n }
     name                { Faker::Educator.unique.campus }
     abbreviation        { name[0...9] }
     mode                { ['timetable', 'automatic', 'manual'].sample }
