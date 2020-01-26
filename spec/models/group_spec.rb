592769bfb23a019e0117352d1bcb740d761fd568
Samuel Anastassiades <sanastassiades@deakin.edu.au>
2020-01-27 09:20:37 +1100

    TEST: Removed already converted spec tests; converted user_spec to minitest

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -2,58 +2,6 @@ require "rails_helper"
 
 RSpec.describe Group do
 
-  it "should have group members" do
-    p = Project.create
-    g1 = FactoryBot.create(:group)
-    expect(g1).to be_valid
-
-    g1.add_member p
-
-    expect(g1.projects).to include(p)
-  end
-
-  it "should not show inactive members" do
-    p = Project.create
-    g1 = FactoryBot.create(:group)
-    expect(g1).to be_valid
-
-    g1.add_member p
-    g1.remove_member p
-
-    expect(g1.projects).not_to include(p)
-  end
-
-  it "should allow students to rejoin groups" do
-    p = Project.create
-    g1 = FactoryBot.create(:group)
-    expect(g1).to be_valid
-
-    g1.add_member p
-    g1.remove_member p
-    g1.add_member p
-
-
-    expect(g1.projects).to include(p)
-    expect(g1.group_memberships.count).to eq(1)
-  end
-
-  it "should know its past members" do
-    p = Project.create
-    p2 = Project.create
-
-    g1 = FactoryBot.create(:group)
-
-    g1.add_member p
-    g1.add_member p2
-    g1.remove_member p
-
-    expect(g1.projects).not_to include(p)
-    expect(g1.past_projects).to include(p)
-    expect(g1.projects).to include(p2)
-    expect(g1.past_projects).not_to include(p2)
-    expect(g1.group_memberships.count).to eq(2)
-  end
-
   it "should exist from unit factory" do
     unit = FactoryBot.create(:unit, group_sets: 1, student_count: 2, :groups => [ { gs: 0, students: 2} ])
     expect(unit).to be_valid
