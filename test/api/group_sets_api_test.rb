f26650dc2df3dee7dc30dcf34c54736050b25ac8
Thisara Wijekoon <greencartweb@gmail.com>
2020-01-27 15:29:21 +1100

    FIX: Change get tests for group_sets_api

--- test/api/group_sets_api_test.rb
+++ test/api/group_sets_api_test.rb
@@ -12,20 +12,32 @@ class GroupSetsApiTest < ActiveSupport::TestCase
   end
 
   def test_get_all_groups_in_unit_with_authorization
-    # Create unit
-    newUnit = FactoryBot.create(:unit)
-  
+    # Create a group
+    newGroup = FactoryBot.create(:group)
+    # Obtain the unit of the group
+    newUnit = newGroup.group_set.unit
     get with_auth_token "/api/units/#{newUnit.id}/groups",newUnit.main_convenor_user
+    
+    #Check response
+    response_keys = %w(id name)
+    last_response_body.each do | data |
+      grp = Group.find(data['id'])
+      assert_json_matches_model(data, grp, response_keys)
+      assert_equal data['name'],grp['name']
+    end
     assert_equal 200, last_response.status
   end  
 
   def test_get_all_groups_in_unit_without_authorization
-    # Create unit
-    newUnit = FactoryBot.create(:unit)
+    # Create a group
+    newGroup = FactoryBot.create(:group)
+    # Obtain the unit of the group
+    newUnit = newGroup.group_set.unit
     # Create student
     studentUser = FactoryBot.create(:user, :student)
 
     get with_auth_token "/api/units/#{newUnit.id}/groups",studentUser
+    # Check error code when an unauthorized user tries to get groups in a unit
     assert_equal 403, last_response.status
   end  
 
