6bca56a7432b0c0e52ce88e81e790cf9cdd138f5
Andrew Cain <macite@gmail.com>
2020-01-29 21:54:22 +1100

    CONFIG: Update rails config for rails 6

--- db/schema.rb
+++ db/schema.rb
@@ -10,45 +10,42 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_05_045015) do
+ActiveRecord::Schema.define(version: 2020_01_07_041946) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "activity_types", force: :cascade do |t|
-    t.string   "name",         null: false
-    t.string   "abbreviation", null: false
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.string "name", null: false
+    t.string "abbreviation", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["abbreviation"], name: "index_activity_types_on_abbreviation", unique: true
+    t.index ["name"], name: "index_activity_types_on_name", unique: true
   end
-
-  add_index "activity_types", ["abbreviation"], name: "index_activity_types_on_abbreviation", unique: true, using: :btree
-  add_index "activity_types", ["name"], name: "index_activity_types_on_name", unique: true, using: :btree
 
   create_table "breaks", force: :cascade do |t|
-    t.datetime "start_date",         null: false
-    t.integer  "number_of_weeks",    null: false
-    t.integer  "teaching_period_id"
+    t.datetime "start_date", null: false
+    t.integer "number_of_weeks", null: false
+    t.integer "teaching_period_id"
+    t.index ["teaching_period_id"], name: "index_breaks_on_teaching_period_id"
   end
-
-  add_index "breaks", ["teaching_period_id"], name: "index_breaks_on_teaching_period_id", using: :btree
 
   create_table "campuses", force: :cascade do |t|
-    t.string  "name",         null: false
-    t.integer "mode",         null: false
-    t.string  "abbreviation", null: false
-    t.boolean "active",       null: false
+    t.string "name", null: false
+    t.integer "mode", null: false
+    t.string "abbreviation", null: false
+    t.boolean "active", null: false
+    t.index ["abbreviation"], name: "index_campuses_on_abbreviation", unique: true
+    t.index ["active"], name: "index_campuses_on_active"
+    t.index ["name"], name: "index_campuses_on_name", unique: true
   end
 
-  add_index "campuses", ["abbreviation"], name: "index_campuses_on_abbreviation", unique: true, using: :btree
-  add_index "campuses", ["active"], name: "index_campuses_on_active", using: :btree
-  add_index "campuses", ["name"], name: "index_campuses_on_name", unique: true, using: :btree
-
   create_table "comments_read_receipts", force: :cascade do |t|
-    t.integer  "task_comment_id", null: false
-    t.integer  "user_id",         null: false
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.integer "task_comment_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "discussion_comments", id: :serial, force: :cascade do |t|
@@ -97,11 +94,11 @@ ActiveRecord::Schema.define(version: 2019_07_05_045015) do
   end
 
   create_table "learning_outcome_task_links", force: :cascade do |t|
-    t.text     "description"
-    t.integer  "rating"
-    t.integer  "task_definition_id"
-    t.integer  "task_id"
-    t.integer  "learning_outcome_id"
+    t.text "description"
+    t.integer "rating"
+    t.integer "task_definition_id"
+    t.integer "task_id"
+    t.integer "learning_outcome_id"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.index ["learning_outcome_id"], name: "learning_outcome_task_links_lo_index"
@@ -138,92 +135,86 @@ ActiveRecord::Schema.define(version: 2019_07_05_045015) do
     t.index ["task_id"], name: "index_plagiarism_match_links_on_task_id"
   end
 
-  add_index "plagiarism_match_links", ["other_task_id"], name: "index_plagiarism_match_links_on_other_task_id", using: :btree
-  add_index "plagiarism_match_links", ["task_id"], name: "index_plagiarism_match_links_on_task_id", using: :btree
-
   create_table "projects", force: :cascade do |t|
-    t.integer  "unit_id"
-    t.string   "project_role",              limit: 255
-    t.datetime "created_at",                                             null: false
-    t.datetime "updated_at",                                             null: false
-    t.boolean  "started"
-    t.string   "progress",                  limit: 255
-    t.string   "status",                    limit: 255
-    t.string   "task_stats",                limit: 255
-    t.boolean  "enrolled",                               default: true
-    t.integer  "target_grade",                           default: 0
-    t.boolean  "compile_portfolio",                      default: false
-    t.date     "portfolio_production_date"
-    t.integer  "max_pct_similar",                        default: 0
-    t.integer  "user_id"
-    t.integer  "grade",                                  default: 0
-    t.string   "grade_rationale",           limit: 4096
-    t.integer  "campus_id"
+    t.integer "unit_id"
+    t.string "project_role", limit: 255
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "started"
+    t.string "progress", limit: 255
+    t.string "status", limit: 255
+    t.string "task_stats", limit: 255
+    t.boolean "enrolled", default: true
+    t.integer "target_grade", default: 0
+    t.boolean "compile_portfolio", default: false
+    t.date "portfolio_production_date"
+    t.integer "max_pct_similar", default: 0
+    t.integer "user_id"
+    t.integer "grade", default: 0
+    t.string "grade_rationale", limit: 4096
+    t.integer "campus_id"
+    t.index ["campus_id"], name: "index_projects_on_campus_id"
+    t.index ["enrolled"], name: "index_projects_on_enrolled"
+    t.index ["unit_id"], name: "index_projects_on_unit_id"
+    t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
-  add_index "projects", ["campus_id"], name: "index_projects_on_campus_id", using: :btree
-  add_index "projects", ["enrolled"], name: "index_projects_on_enrolled", using: :btree
-  add_index "projects", ["unit_id"], name: "index_projects_on_unit_id", using: :btree
-  add_index "projects", ["user_id"], name: "index_projects_on_user_id", using: :btree
-
   create_table "roles", force: :cascade do |t|
-    t.string   "name",        limit: 255
-    t.text     "description"
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
+    t.string "name", limit: 255
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "task_comments", force: :cascade do |t|
-    t.integer  "task_id",                                               null: false
-    t.integer  "user_id",                                               null: false
-    t.string   "comment",                   limit: 4096
-    t.datetime "created_at",                                            null: false
-    t.boolean  "is_new",                                 default: true
-    t.integer  "recipient_id"
-    t.string   "content_type"
-    t.string   "attachment_extension"
-    t.integer  "discussion_comment_id"
-    t.string   "type"
+    t.integer "task_id", null: false
+    t.integer "user_id", null: false
+    t.string "comment", limit: 4096
+    t.datetime "created_at", null: false
+    t.boolean "is_new", default: true
+    t.integer "recipient_id"
+    t.string "content_type"
+    t.string "attachment_extension"
+    t.integer "discussion_comment_id"
+    t.string "type"
     t.datetime "time_discussion_started"
     t.datetime "time_discussion_completed"
     t.integer "number_of_prompts"
     t.datetime "date_extension_assessed"
-    t.boolean  "extension_granted"
-    t.integer  "assessor_id"
-    t.integer  "task_status_id"
-    t.integer  "extension_weeks"
-    t.string   "extension_response"
+    t.boolean "extension_granted"
+    t.integer "assessor_id"
+    t.integer "task_status_id"
+    t.integer "extension_weeks"
+    t.string "extension_response"
+    t.index ["discussion_comment_id"], name: "index_task_comments_on_discussion_comment_id"
+    t.index ["task_id"], name: "index_task_comments_on_task_id"
   end
-
-  add_index "task_comments", ["discussion_comment_id"], name: "index_task_comments_on_discussion_comment_id", using: :btree
-  add_index "task_comments", ["task_id"], name: "index_task_comments_on_task_id", using: :btree
 
   create_table "task_definitions", force: :cascade do |t|
-    t.integer  "unit_id"
-    t.string   "name",                    limit: 255
-    t.string   "description",             limit: 4096
-    t.decimal  "weighting",                            precision: 10
-    t.datetime "target_date",                                                         null: false
-    t.datetime "created_at",                                                          null: false
-    t.datetime "updated_at",                                                          null: false
-    t.string   "abbreviation",            limit: 255
-    t.string   "upload_requirements",     limit: 4096
-    t.integer  "target_grade",                                        default: 0
-    t.boolean  "restrict_status_updates",                             default: false
-    t.string   "plagiarism_checks",       limit: 4096
-    t.string   "plagiarism_report_url",   limit: 255
-    t.boolean  "plagiarism_updated",                                  default: false
-    t.integer  "plagiarism_warn_pct",                                 default: 50
-    t.integer  "group_set_id"
+    t.integer "unit_id"
+    t.string "name", limit: 255
+    t.string "description", limit: 4096
+    t.decimal "weighting", precision: 10
+    t.datetime "target_date", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "abbreviation", limit: 255
+    t.string "upload_requirements", limit: 4096
+    t.integer "target_grade", default: 0
+    t.boolean "restrict_status_updates", default: false
+    t.string "plagiarism_checks", limit: 4096
+    t.string "plagiarism_report_url", limit: 255
+    t.boolean "plagiarism_updated", default: false
+    t.integer "plagiarism_warn_pct", default: 50
+    t.integer "group_set_id"
     t.datetime "due_date"
-    t.datetime "start_date",                                                          null: false
-    t.boolean  "is_graded",                                           default: false
-    t.integer  "max_quality_pts",                                     default: 0
-    t.integer  "tutorial_stream_id"
+    t.datetime "start_date", null: false
+    t.boolean "is_graded", default: false
+    t.integer "max_quality_pts", default: 0
+    t.integer "tutorial_stream_id"
+    t.index ["tutorial_stream_id"], name: "index_task_definitions_on_tutorial_stream_id"
+    t.index ["unit_id"], name: "index_task_definitions_on_unit_id"
   end
-
-  add_index "task_definitions", ["tutorial_stream_id"], name: "index_task_definitions_on_tutorial_stream_id", using: :btree
-  add_index "task_definitions", ["unit_id"], name: "index_task_definitions_on_unit_id", using: :btree
 
   create_table "task_engagements", id: :serial, force: :cascade do |t|
     t.datetime "engagement_time"
@@ -288,63 +279,58 @@ ActiveRecord::Schema.define(version: 2019_07_05_045015) do
     t.index ["period", "year"], name: "index_teaching_periods_on_period_and_year", unique: true
   end
 
-  add_index "teaching_periods", ["period", "year"], name: "index_teaching_periods_on_period_and_year", unique: true, using: :btree
-
   create_table "tutorial_enrolments", force: :cascade do |t|
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
-    t.integer  "project_id",         null: false
-    t.integer  "tutorial_id",        null: false
-    t.integer  "tutorial_stream_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "project_id", null: false
+    t.integer "tutorial_id", null: false
+    t.integer "tutorial_stream_id"
+    t.index ["project_id"], name: "index_tutorial_enrolments_on_project_id"
+    t.index ["tutorial_id", "project_id"], name: "index_tutorial_enrolments_on_tutorial_id_and_project_id", unique: true
+    t.index ["tutorial_id"], name: "index_tutorial_enrolments_on_tutorial_id"
+    t.index ["tutorial_stream_id", "project_id"], name: "index_tutorial_enrolments_on_tutorial_stream_id_and_project_id", unique: true
+    t.index ["tutorial_stream_id"], name: "index_tutorial_enrolments_on_tutorial_stream_id"
   end
-
-  add_index "tutorial_enrolments", ["project_id"], name: "index_tutorial_enrolments_on_project_id", using: :btree
-  add_index "tutorial_enrolments", ["tutorial_id", "project_id"], name: "index_tutorial_enrolments_on_tutorial_id_and_project_id", unique: true, using: :btree
-  add_index "tutorial_enrolments", ["tutorial_id"], name: "index_tutorial_enrolments_on_tutorial_id", using: :btree
-  add_index "tutorial_enrolments", ["tutorial_stream_id", "project_id"], name: "index_tutorial_enrolments_on_tutorial_stream_id_and_project_id", unique: true, using: :btree
-  add_index "tutorial_enrolments", ["tutorial_stream_id"], name: "index_tutorial_enrolments_on_tutorial_stream_id", using: :btree
 
   create_table "tutorial_streams", force: :cascade do |t|
-    t.string   "name",             null: false
-    t.string   "abbreviation",     null: false
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
-    t.integer  "activity_type_id", null: false
-    t.integer  "unit_id",          null: false
+    t.string "name", null: false
+    t.string "abbreviation", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "activity_type_id", null: false
+    t.integer "unit_id", null: false
+    t.index ["abbreviation", "unit_id"], name: "index_tutorial_streams_on_abbreviation_and_unit_id", unique: true
+    t.index ["abbreviation"], name: "index_tutorial_streams_on_abbreviation"
+    t.index ["name", "unit_id"], name: "index_tutorial_streams_on_name_and_unit_id", unique: true
+    t.index ["unit_id"], name: "index_tutorial_streams_on_unit_id"
   end
-
-  add_index "tutorial_streams", ["abbreviation", "unit_id"], name: "index_tutorial_streams_on_abbreviation_and_unit_id", unique: true, using: :btree
-  add_index "tutorial_streams", ["abbreviation"], name: "index_tutorial_streams_on_abbreviation", using: :btree
-  add_index "tutorial_streams", ["name", "unit_id"], name: "index_tutorial_streams_on_name_and_unit_id", unique: true, using: :btree
-  add_index "tutorial_streams", ["unit_id"], name: "index_tutorial_streams_on_unit_id", using: :btree
 
   create_table "tutorials", force: :cascade do |t|
-    t.integer  "unit_id"
-    t.string   "meeting_day",        limit: 255
-    t.string   "meeting_time",       limit: 255
-    t.string   "meeting_location",   limit: 255
-    t.datetime "created_at",                     null: false
-    t.datetime "updated_at",                     null: false
-    t.string   "code",               limit: 255
-    t.integer  "unit_role_id"
-    t.string   "abbreviation",       limit: 255
-    t.integer  "capacity"
-    t.integer  "campus_id"
-    t.integer  "tutorial_stream_id"
+    t.integer "unit_id"
+    t.string "meeting_day", limit: 255
+    t.string "meeting_time", limit: 255
+    t.string "meeting_location", limit: 255
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "code", limit: 255
+    t.integer "unit_role_id"
+    t.string "abbreviation", limit: 255
+    t.integer "capacity"
+    t.integer "campus_id"
+    t.integer "tutorial_stream_id"
+    t.index ["campus_id"], name: "index_tutorials_on_campus_id"
+    t.index ["tutorial_stream_id"], name: "index_tutorials_on_tutorial_stream_id"
+    t.index ["unit_id"], name: "index_tutorials_on_unit_id"
+    t.index ["unit_role_id"], name: "index_tutorials_on_unit_role_id"
   end
 
-  add_index "tutorials", ["campus_id"], name: "index_tutorials_on_campus_id", using: :btree
-  add_index "tutorials", ["tutorial_stream_id"], name: "index_tutorials_on_tutorial_stream_id", using: :btree
-  add_index "tutorials", ["unit_id"], name: "index_tutorials_on_unit_id", using: :btree
-  add_index "tutorials", ["unit_role_id"], name: "index_tutorials_on_unit_role_id", using: :btree
-
   create_table "unit_roles", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "tutorial_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.integer  "role_id"
-    t.integer  "unit_id"
+    t.integer "user_id"
+    t.integer "tutorial_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "role_id"
+    t.integer "unit_id"
   end
 
   create_table "units", id: :serial, force: :cascade do |t|
@@ -357,16 +343,15 @@ ActiveRecord::Schema.define(version: 2019_07_05_045015) do
     t.string "code", limit: 255
     t.boolean "active", default: true
     t.datetime "last_plagarism_scan"
-    t.integer  "teaching_period_id"
-    t.integer  "main_convenor_id"
+    t.integer "teaching_period_id"
+    t.integer "main_convenor_id"
+    t.index ["teaching_period_id"], name: "index_units_on_teaching_period_id"
   end
 
-  add_index "units", ["teaching_period_id"], name: "index_units_on_teaching_period_id", using: :btree
-
   create_table "users", force: :cascade do |t|
-    t.string   "email",                           limit: 255, default: "",    null: false
-    t.string   "encrypted_password",              limit: 255, default: "",    null: false
-    t.string   "reset_password_token",            limit: 255
+    t.string "email", limit: 255, default: "", null: false
+    t.string "encrypted_password", limit: 255, default: "", null: false
+    t.string "reset_password_token", limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.integer "sign_in_count", default: 0
