bee0ba1b2c8c3e1440a9cf02b9d7cf3f85fb0c10
Thisara Wijekoon <greencartweb@gmail.com>
2020-01-29 12:46:11 +1100

    FIX: Change get tests for group_sets_api

--- test/api/group_sets_api_test.rb
+++ test/api/group_sets_api_test.rb
@@ -17,7 +17,7 @@ class GroupSetsApiTest < ActiveSupport::TestCase
     # Obtain the unit of the group
     newUnit = newGroup.group_set.unit
     get with_auth_token "/api/units/#{newUnit.id}/groups",newUnit.main_convenor_user
-    
+
     #Check response
     response_keys = %w(id name)
     last_response_body.each do | data |
@@ -25,7 +25,7 @@ class GroupSetsApiTest < ActiveSupport::TestCase
       assert_json_matches_model(data, grp, response_keys)
     end
     assert_equal 200, last_response.status
-  end  
+  end
 
   def test_get_all_groups_in_unit_without_authorization
     # Create a group
@@ -33,11 +33,11 @@ class GroupSetsApiTest < ActiveSupport::TestCase
     # Obtain the unit of the group
     newUnit = newGroup.group_set.unit
 
-    # Obtain a student object from the unit
+    # Obtain student object from the unit
     studentUser = newUnit.active_projects.first.student
     get with_auth_token "/api/units/#{newUnit.id}/groups",studentUser
     # Check error code when an unauthorized user tries to get groups in a unit
     assert_equal 403, last_response.status
-  end  
+  end
 
 end
