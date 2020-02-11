7f2419bca178fc134ccd29bdfb940853e1968e7c
Andrew Cain <macite@gmail.com>
2020-02-11 20:46:35 +1100

    CONFIG: Add grape swagger rails

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,3 +1,2 @@
-//= link_tree ../images
-//= link_directory ../javascripts .js
-//= link_directory ../stylesheets .css
+//= link grape_swagger_rails/application.css
+//= link grape_swagger_rails/application.js
