d5bc60eec8a4b8c72272ee2ff2bf1917edf7225f
Andrew Cain <macite@gmail.com>
2020-02-20 22:45:36 +1100

    TEST: Group capacity and ranges of gs capacity values

--- test/models/group_test.rb
+++ test/models/group_test.rb
@@ -41,8 +41,8 @@ class GroupModelTest < ActiveSupport::TestCase
 
   def test_knows_past_members
     project1 = FactoryBot.create(:project)
-    project2 = FactoryBot.create(:project)
-    group1 = FactoryBot.create(:group)
+    project2 = FactoryBot.create(:project, unit: project1.unit)
+    group1 = FactoryBot.create(:group, unit: project1.unit)
 
     group1.add_member project1
     group1.add_member project2
@@ -53,5 +53,40 @@ class GroupModelTest < ActiveSupport::TestCase
     assert_includes(group1.projects,project2)
     refute_includes(group1.past_projects,project2)
     assert_equal group1.group_memberships.count, 2
+  end
+
+  def test_capacity_ranges
+    gs = FactoryBot.create :group_set
+
+    gs.capacity = 2
+    assert gs.valid?
+    gs.capacity = 1
+    refute gs.valid?
+    gs.capacity = 0
+    refute gs.valid?
+  end
+
+  def test_at_capacity
+    unit = FactoryBot.create :unit, group_sets: 1, groups: [{gs: 0, students: 2}]
+    gs = unit.group_sets.first
+    group1 = gs.groups.first
+    assert group1.valid?
+
+    refute group1.at_capacity?
+
+    gs.update(capacity: 2)
+
+    assert group1.at_capacity?
+
+    project = FactoryBot.create :project, unit: unit
+    group1.add_member project
+
+    assert group1.at_capacity?
+    gs.update(capacity: 3)
+    assert group1.at_capacity?
+    gs.update(capacity: 4)
+    refute group1.at_capacity?
+    gs.update(capacity: nil)
+    refute group1.at_capacity?
   end
 end
