7affb69d833666dc8fbb4f6905eb4e93aa0a8c9e
Andrew Cain <macite@gmail.com>
2020-02-21 21:46:02 +1100

    FIX: Ensure tutorial change check uses project

--- app/api/tutorial_enrolments_api.rb
+++ app/api/tutorial_enrolments_api.rb
@@ -12,14 +12,14 @@ module Api
     desc 'Enrol project in a tutorial'
     post '/units/:unit_id/tutorials/:tutorial_abbr/enrolments/:project_id' do
       unit = Unit.find(params[:unit_id])
-      unless authorise? current_user, unit, :change_tutorial
+      project = unit.active_projects.find(params[:project_id])
+      unless authorise? current_user, project, :change_tutorial
         error!({ error: 'Not authorised to change tutorial' }, 403)
       end
 
       tutorial = unit.tutorials.find_by(abbreviation: params[:tutorial_abbr])
       error!({ error: "No tutorial with abbreviation #{params[:tutorial_abbr]} exists for the unit" }, 403) unless tutorial.present?
 
-      project = Project.find(params[:project_id])
       result = project.enrol_in(tutorial)
 
       if result.nil?
@@ -37,7 +37,8 @@ module Api
     desc 'Delete an enrolment in the tutorial'
     delete '/units/:unit_id/tutorials/:tutorial_abbr/enrolments/:project_id' do
       unit = Unit.find(params[:unit_id])
-      unless authorise? current_user, unit, :change_tutorial
+      project = unit.active_projects.find(params[:project_id])
+      unless authorise? current_user, project, :change_tutorial
         error!({ error: 'Not authorised to change tutorials' }, 403)
       end
 
