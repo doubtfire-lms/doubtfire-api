3d3c9f719dc38fabfa074ea101061257d6889d4e
Andrew Cain <macite@gmail.com>
2020-02-22 12:59:32 +1100

    FIX: Correct group serialisation of student count

--- app/serializers/group_serializer.rb
+++ app/serializers/group_serializer.rb
@@ -3,6 +3,12 @@
 
 class GroupSerializer < ActiveModel::Serializer
   attributes :id, :name, :tutorial_id, :group_set_id, :number, :student_count
+
+  def student_count
+    return object.student_count if object.has_attribute?(:student_count)
+    return object[:student_count] if object.has_key(:student_count)
+    return 0
+  end
 end
 
 class DeepGroupSerializer < ActiveModel::Serializer
