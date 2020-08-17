3f917700a9061c75b359fbe5a640f307bf72ccbb
Ravindu Liyanapathirana <l.ravindu@hotmail.com>
2020-08-17 13:39:45 +1000

    NEW: Serve webcals with target dates

--- app/api/webcal_api.rb
+++ app/api/webcal_api.rb
@@ -1,4 +1,5 @@
 require 'grape'
+require 'icalendar'
 
 module Api
 
@@ -9,9 +10,10 @@ module Api
       requires :id, type: String, desc: 'The ID of the webcal'
     end
     get '/webcal/:id' do
+
       # Retrieve the specified webcal.
       webcal = Webcal.find(params[:id])
-      task_definitions = []
+      ical = Icalendar::Calendar.new
 
       # Retrieve task definitions and tasks of the user's active units.
       # TODO: Can this be reduced to 1 query instead of 1 + # of projects?
@@ -20,11 +22,21 @@ module Api
         .where(units: { active: true })
         .each do |prj|
           prj.unit.task_definitions.includes(:tasks).where(tasks: { project_id: [prj.id, nil] }).each do |td|
-            task_definitions.push(td)
-          end
-        end
 
-      task_definitions
+            # Add the task to the iCalendar.
+            ical.event do |ev|
+              ev.summary = "#{td.unit.code}: #{td.abbreviation}: #{td.name}"
+              # The start and end dates should be equal because the calendar event is expected to be an "all-day" event.
+              ev.dtstart = Icalendar::Values::Date.new(td.target_date.strftime('%Y%m%d'))
+              ev.dtend = Icalendar::Values::Date.new(td.target_date.strftime('%Y%m%d'))
+            end
+
+          end
+      end
+
+      # Serve the iCalendar with the correct MIME type.
+      content_type 'text/calendar'
+      ical.to_ical
     end
 
   end
