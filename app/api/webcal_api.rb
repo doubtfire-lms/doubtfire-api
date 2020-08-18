024f632bd536ff21afefec5216b27a93fa2ad386
Ravindu Liyanapathirana <l.ravindu@hotmail.com>
2020-08-18 17:47:14 +1000

    NEW: Add endpoints to retrieve and update webcal details

--- app/api/webcal_api.rb
+++ app/api/webcal_api.rb
@@ -4,9 +4,64 @@ require 'icalendar'
 module Api
 
   class WebcalApi < Grape::API
+    helpers AuthenticationHelpers
+
+    # Declare content types
     content_type :txt, 'text/calendar'
 
-    desc 'Serves web calendars ("webcals") that include selected dates of tasks of students\' active units.'
+    desc 'Get webcal details of the authenticated user'
+    params do
+      requires :auth_token, type: String, desc: 'Authentication token'
+    end
+    get '/webcal' do
+      authenticated?
+      current_user.webcal
+    end
+
+    desc 'Update webcal details of the authenticated user'
+    params do
+      requires :webcal, type: Hash do
+        optional :enabled,             type: Boolean, desc: 'Is the webcal enabled?'
+        optional :should_change_id,    type: Boolean, desc: 'Should the ID of the webcal be changed?'
+        optional :include_start_dates, type: Boolean, desc: 'Should events for start dates be included?'
+      end
+      requires :auth_token, type: String, desc: 'Authentication token'
+    end
+    put '/webcal' do
+      authenticated?
+      webcal_params = params[:webcal]
+
+      user = current_user
+      cal = user.webcal
+
+      # Create or destroy the user's webcal, according to the `enabled` parameter.
+      if webcal_params.key?(:enabled)
+        if webcal_params[:enabled] and cal == nil
+          cal = user.create_webcal(id: SecureRandom.uuid)
+        elsif (not webcal_params[:enabled]) and cal != nil
+          cal.destroy
+        end
+      end
+
+      return if cal == nil or cal.destroyed?
+      webcal_update_params = {}
+
+      # Change the ID if requested.
+      if webcal_params.key?(:should_change_id)
+        webcal_update_params[:id] = SecureRandom.uuid
+      end
+
+      # Set any other properties that have to be updated verbatim.
+      webcal_update_params.merge! ActionController::Parameters.new(webcal_params).permit(
+        :include_start_dates
+      )
+
+      # Update and return calendar.
+      cal.update! webcal_update_params
+      cal
+    end
+
+    desc 'Serve webcal with the specified ID'
     params do
       requires :id, type: String, desc: 'The ID of the webcal'
     end
