1526cb897e7b838299f85bfa2944533bc7c93531
Andrew Cain <macite@gmail.com>
2020-08-19 10:03:41 +1000

    FIX: Ensure group membership checks project enrolment on validate

--- app/models/group_membership.rb
+++ app/models/group_membership.rb
@@ -11,11 +11,11 @@ class GroupMembership < ActiveRecord::Base
   validate :must_be_in_same_tutorial, if: :restricted_to_tutorial?
 
   def restricted_to_tutorial?
-    active && group_set.keep_groups_in_same_class
+    project.enrolled && active && group_set.keep_groups_in_same_class
   end
 
   def must_be_in_same_tutorial
-    if active && !in_group_tutorial?(group.tutorial)
+    if project.enrolled && active && !in_group_tutorial?(group.tutorial)
       errors.add(:group, "requires all students to be in the #{group.tutorial.abbreviation} tutorial which is not the case for #{project.student.name}.")
     end
   end
