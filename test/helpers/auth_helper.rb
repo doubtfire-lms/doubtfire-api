ae3e8cfb3fe4987e0a0ecc969855d17361f7c9c1
Arvind Jayasankar <scravuri@deakin.edu.au>
2020-08-26 00:02:03 +1000

    FIX: Updated unit testing for all /api/auth endpoint

--- test/helpers/auth_helper.rb
+++ test/helpers/auth_helper.rb
@@ -8,9 +8,9 @@ module TestHelpers
     #
     def auth_token(user = User.first)
       token = user.valid_auth_tokens().first
-      return token.auth_token unless token.nil?
+      return token.authentication_token unless token.nil?
 
-      return user.generate_authentication_token!().auth_token
+      return user.generate_authentication_token!().authentication_token
     end
 
     #
@@ -28,6 +28,8 @@ module TestHelpers
         # otherwise use ?
         data << (data.include?('?') ? '&' : '?') << "auth_token=#{auth_token user}"
       end
+      header 'username',data[:username]
+      header 'auth_token', data[:auth_token]
       data
     end
 
