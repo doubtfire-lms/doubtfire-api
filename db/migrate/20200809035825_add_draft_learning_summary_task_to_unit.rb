5ab567875990c8ee59b0b90b815a81c508d9ac71
Conor Blencowe <conorblencowe98@gmail.com>
2020-08-25 12:12:30 +1000

    QUALITY: Rename draft task definition reference in unit table

--- db/migrate/20200809035825_add_draft_learning_summary_task_to_unit.rb
+++ db/migrate/20200809035825_add_draft_learning_summary_task_to_unit.rb
@@ -1,5 +1,5 @@
 class AddDraftLearningSummaryTaskToUnit < ActiveRecord::Migration
   def change
-    add_reference :units, :draft_task, references: :task_definitions
+    add_reference :units, :draft_task_definition, references: :task_definitions
   end
 end
