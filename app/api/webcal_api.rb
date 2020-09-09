104db53247ab02d57b75b0997d3d72e23fad2623
Ravindu Liyanapathirana <l.ravindu@hotmail.com>
2020-09-09 10:04:54 +1000

    NEW: Allow excluding units from webcals

--- app/api/webcal_api.rb
+++ app/api/webcal_api.rb
@@ -21,9 +21,10 @@ module Api
     desc 'Update webcal details of the authenticated user'
     params do
       requires :webcal, type: Hash do
-        optional :enabled,             type: Boolean, desc: 'Is the webcal enabled?'
-        optional :should_change_guid,  type: Boolean, desc: 'Should the GUID of the webcal be changed?'
-        optional :include_start_dates, type: Boolean, desc: 'Should events for start dates be included?'
+        optional :enabled,             type: Boolean,        desc: 'Is the webcal enabled?'
+        optional :should_change_guid,  type: Boolean,        desc: 'Should the GUID of the webcal be changed?'
+        optional :include_start_dates, type: Boolean,        desc: 'Should events for start dates be included?'
+        optional :unit_exclusions,     type: Array[Integer], desc: 'IDs of units that must be excluded from the webcal'
       end
       requires :auth_token, type: String, desc: 'Authentication token'
     end
@@ -32,7 +33,12 @@ module Api
       webcal_params = params[:webcal]
 
       user = current_user
-      cal = user.webcal
+
+      cal = Webcal
+        .includes(:webcal_unit_exclusions)
+        .where(user_id: user.id)
+        .load
+        .first
 
       # Create or destroy the user's webcal, according to the `enabled` parameter.
       if webcal_params.key?(:enabled)
@@ -56,8 +62,30 @@ module Api
         :include_start_dates
       )
 
-      # Update and return calendar.
+      # Update calendar.
       cal.update! webcal_update_params
+
+      # Update unit exclusions, if specified.
+      if webcal_params.key?(:unit_exclusions)
+
+        # Delete existing exclusions.
+        cal.webcal_unit_exclusions.delete_all
+
+        # Add exclusions with valid unit IDs.
+        if webcal_params[:unit_exclusions].any?
+          cal.webcal_unit_exclusions.create(
+            Unit
+              .joins(:projects)
+              .where(
+                projects: { user_id: 7 },
+                units: { id: webcal_params[:unit_exclusions], active: true }
+              )
+              .pluck(:id)
+              .map { |i| { unit_id: i } }
+          )
+        end
+      end
+
       cal
     end
 
@@ -68,8 +96,7 @@ module Api
     get '/webcal/:guid' do
 
       # Retrieve the specified webcal.
-      webcal = Webcal.where(guid: params[:guid]).first
-      return error!({ error: 'Unable to find the requested webcal.' }, 404) if webcal.nil?
+      webcal = Webcal.where(guid: params[:guid]).first!
 
       # Generate iCalendar.
       ical = webcal.to_ical_with_task_definitions(
@@ -81,6 +108,9 @@ module Api
           .where(
             projects: { user_id: webcal.user_id },
             units: { active: true }
+          )
+          .where.not(
+            units: { id: WebcalUnitExclusion.where(webcal_id: webcal.id).select(:unit_id) } # exclude :webcal_unit_exclusions
           )
           .where('tasks.project_id is null or tasks.project_id = projects.id')   # eager_load only :tasks of :projects
           .where('? BETWEEN units.start_date AND units.end_date', Time.zone.now) # Current units
