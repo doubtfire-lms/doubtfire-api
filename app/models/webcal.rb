2573a782e25ab1a7edec1b80c1d87f7a42a085b9
Ravindu Liyanapathirana <l.ravindu@hotmail.com>
2020-09-09 19:14:35 +1000

    NEW: Allow specifying webcal reminders

--- app/models/webcal.rb
+++ app/models/webcal.rb
@@ -7,6 +7,21 @@ class Webcal < ActiveRecord::Base
   has_many :webcal_unit_exclusions, dependent: :destroy
 
   #
+  # Array of valid units by which task reminders (alarms) can be set.
+  # Documented at https://tools.ietf.org/html/rfc5545#section-3.3.6
+  #
+  def self.valid_time_units
+    %w(W D H M)
+  end
+
+  #
+  # Represents the presence of `reminder_time` and `reminder_unit`.
+  #
+  def reminder?
+    reminder_time.present? && reminder_unit.present?
+  end
+
+  #
   # Generates a single `Icalendar::Calendar` object from this `Webcal` including calendar events for the specified
   # collection of `TaskDefinition`s.
   #
@@ -31,24 +46,47 @@ class Webcal < ActiveRecord::Base
 
       ev_name = "#{td.unit.code}: #{td.abbreviation}: #{td.name}"
       ev_date_format = '%Y%m%d'
+      ev_reminders = reminder?
+      ev_reminder_trigger = "-PT#{reminder_time}#{reminder_unit}"
 
       # Add event for start date, if the user opted in.
       if include_start_dates
         ical.event do |ev|
+          ev_summary = "Start: #{ev_name}"
+
           ev.uid = "S-#{td.id}"
-          ev.summary = "Start: #{ev_name}"
+          ev.summary = ev_summary
           ev.dtstart = ev.dtend = Icalendar::Values::Date.new(td.start_date.strftime(ev_date_format))
+
+          if ev_reminders
+            ev.alarm do |a|
+              a.action = 'DISPLAY'
+              a.description = ev_summary
+              a.trigger = ev_reminder_trigger
+            end
+          end
         end
       end
 
       # Add event for target/extended date.
       ical.event do |ev|
-        ev.uid = "E-#{td.id}"
-        ev.summary = "#{include_start_dates ? 'End:' : ''}#{ev_name}"
+        ev_summary = "#{include_start_dates ? 'End:' : ''}#{ev_name}"
 
+        ev.uid = "E-#{td.id}"
+        ev.summary = ev_summary
+
+        # Use extended date if available.
         ev_date = td.target_date
         ev_date += (td.tasks.first.extensions * 7).day if td.tasks.present?
         ev.dtstart = ev.dtend = Icalendar::Values::Date.new(ev_date.strftime(ev_date_format))
+
+        if ev_reminders
+          ev.alarm do |a|
+            a.action = 'DISPLAY'
+            a.description = ev_summary
+            a.trigger = ev_reminder_trigger
+          end
+        end
       end
     end
 
--- app/serializers/webcal_serializer.rb
+++ app/serializers/webcal_serializer.rb
@@ -1,5 +1,16 @@
 class WebcalSerializer < ActiveModel::Serializer
-  attributes :id, :guid, :include_start_dates, :unit_exclusions
+  attributes :id, :guid, :include_start_dates, :reminder, :unit_exclusions
+
+  def reminder
+    if object.reminder_time.nil? || object.reminder_unit.nil?
+      nil
+    else
+      {
+        time: object.reminder_time,
+        unit: object.reminder_unit
+      }
+    end
+  end
 
   def unit_exclusions
     object.webcal_unit_exclusions.map(&:unit_id)
