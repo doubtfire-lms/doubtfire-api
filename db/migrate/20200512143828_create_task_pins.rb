c840277bf7894ed83dbe0dfe86523546f9ce885e
Ravindu Liyanapathirana <l.ravindu@hotmail.com>
2020-09-23 12:53:05 +1000

    ENHANCE: Define foreign key between TaskPin and Task & User

--- db/migrate/20200512143828_create_task_pins.rb
+++ db/migrate/20200512143828_create_task_pins.rb
@@ -1,10 +1,13 @@
 class CreateTaskPins < ActiveRecord::Migration
   def change
-    create_table :task_pins do |t|
-      t.integer :task_id
-      t.integer :user_id
 
+    create_table :task_pins do |t|
+      t.references :task, foreign_key: true, null: false
+      t.references :user, foreign_key: true, null: false
       t.timestamps null: false
     end
+
+    add_index :task_pins, [:task_id, :user_id], { unique: true }
+
   end
 end
