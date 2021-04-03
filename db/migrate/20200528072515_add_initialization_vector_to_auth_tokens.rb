7150db4e15d8927f575cfc0c3a2e61a576afa02d
Andrew Cain <macite@gmail.com>
2021-04-03 17:58:08 +1100

    FIX: Update migration to work with new code on migrate without data loss

--- db/migrate/20200528072515_add_initialization_vector_to_auth_tokens.rb
+++
@@ -1,8 +0,0 @@
-class AddInitializationVectorToAuthTokens < ActiveRecord::Migration
-  def up
-  	add_column	:auth_tokens, :encrypted_authentication_token_iv, :string, limit: 255 
-  end
-  def down
-  	remove_column	:auth_tokens, :encrypted_authentication_token_iv
-  end
-end
