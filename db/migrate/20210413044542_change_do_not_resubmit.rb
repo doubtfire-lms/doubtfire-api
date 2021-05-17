05667a9051cccad497766e307bab139207309ff6
Andrew Cain <macite@gmail.com>
2021-05-17 16:11:43 +1000

    CONFIG: Ensure migration of task status works without data

--- db/migrate/20210413044542_change_do_not_resubmit.rb
+++ db/migrate/20210413044542_change_do_not_resubmit.rb
@@ -1,9 +1,11 @@
 class ChangeDoNotResubmit < ActiveRecord::Migration
   def change
-    dnr = TaskStatus.feedback_exceeded
-    TaskStatusComment.where(task_status: dnr).update_all(comment: 'Feedback Exceeded')
-    dnr.name = 'Feedback Exceeded'
-    dnr.save!
-    Rails.cache.delete("task_statuses/#{dnr.id}")
+    if TaskStatus.count > 0
+      dnr = TaskStatus.feedback_exceeded
+      TaskStatusComment.where(task_status: dnr).update_all(comment: 'Feedback Exceeded')
+      dnr.name = 'Feedback Exceeded'
+      dnr.save!
+      Rails.cache.delete("task_statuses/#{dnr.id}")
+    end
   end
 end
