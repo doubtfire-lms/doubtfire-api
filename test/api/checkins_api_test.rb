3ff9766526ebc4e7c73b144198010d665907c9c5
Abood Afifi <a.afifi5299@gmail.com>
2021-07-31 20:13:38 +1000

    Fixed tests

--- test/api/checkins_api_test.rb
+++ test/api/checkins_api_test.rb
@@ -9,64 +9,83 @@ class StudentsApiTest < ActiveSupport::TestCase
     Rails.application
   end
 
-  def test_check_in_student_records_correct_data
+  def test_check_in_student_with_unassigned_card_returns_correct_response
     room = FactoryBot.create(:room)
-    id_card = FactoryBot.create(:id_card)
+    card = FactoryBot.create(:id_card)
 
-    json_data = { card_id: id_card.card_number, room: room.room_number }
+    json_data = { card_id: card.card_number, room_number: room.room_number }
 
-    post_json '/api/iotrack/check-in', json_data
+    post_json '/api/iotrack/check-in-out', json_data
 
-    assert_equal 200, last_response.status
-    assert_equal room.id, last_response_body.room_id
-    assert_equal id_card.id, last_response_body.id_card_id
-    assert_equal last_response_body.checkout_at, nil
-    assert_equal last_response_body.seat, nil
-    # TODO: Test that the timestamp is correct
+    assert_equal 201, last_response.status
+    assert_equal last_response_body['checked_out'], false
+    assert_equal last_response_body['need_user_assignment'], true
   end
 
   def test_check_in_creates_new_id_card_record_if_it_does_not_exist
     room = FactoryBot.create(:room)
 
-    json_data = { card_id: 'non-existent-card', room: room.room_number }
+    json_data = { card_id: 'non-existent-card', room_number: room.room_number }
 
-    post_json '/api/iotrack/check-in', json_data
+    post_json '/api/iotrack/check-in-out', json_data
 
-    assert_equal 200, last_response.status
-    assert_equal room.id, last_response_body.room_id
-    # TODO: Check with Andrew if there's a cleaner way of doing this
-    assert_equal IdCard.find_by(card_number: 'non-existent-card').id, last_response_body.id_card_id
-    assert_equal last_response_body.checkout_at, nil
-    assert_equal last_response_body.seat, nil
-    # TODO: Test that the timestamp is correct
+    assert_equal 201, last_response.status
+    assert_equal last_response_body['checked_out'], false
+    assert_equal last_response_body['need_user_assignment'], true
+    assert_equal IdCard.find_by(card_number: 'non-existent-card').present?, true
   end
 
-  def test_check_in_fails_when_there_is_a_current_checkin_session
+  def test_check_in_records_correct_checkin_at_timestamp
     room = FactoryBot.create(:room)
-    id_card = FactoryBot.create(:id_card)
-    checkin = FactoryBot.create(:check_in, room: room, id_card: id_card)
+    card = FactoryBot.create(:id_card)
 
-    json_data = { card_id: id_card.card_number, room: room.room_number }
+    json_data = { card_id: card.card_number, room_number: room.room_number }
 
-    post_json '/api/iotrack/check-in', json_data
+    freeze_time
+    expected_time = Time.zone.now
 
-    assert_equal 403, last_response.status
-    assert_equal last_response_body.error, 'A current checkin session in this room for this person exists'
+    post_json '/api/iotrack/check-in-out', json_data
+
+    assert_equal 201, last_response.status
+    assert_equal last_response_body['checked_out'], false
+    assert_equal CheckIn.first.checkin_at, expected_time
   end
 
-  def test_check_out_student_sets_checkout_timestamp_correctly
-    # TODO
+  def test_check_out_returns_correct_response
+    room = FactoryBot.create(:room)
+    card = FactoryBot.create(:id_card)
+    checkin = FactoryBot.create(:check_in, room: room, id_card: card)
+
+    json_data = { card_id: card.card_number, room_number: room.room_number }
+
+    post_json '/api/iotrack/check-in-out', json_data
+
+    assert_equal 201, last_response.status
+    assert_equal last_response_body['checked_out'], true
   end
 
-  def test_check_out_fails_when_there_is_no_current_checkin_session
-    # TODO
+  def test_check_out_records_correct_checkout_timestamp
+    room = FactoryBot.create(:room)
+    card = FactoryBot.create(:id_card)
+    checkin = FactoryBot.create(:check_in, room: room, id_card: card)
+
+    json_data = { card_id: card.card_number, room_number: room.room_number }
+
+    freeze_time
+    expected_time = Time.zone.now
+
+    post_json '/api/iotrack/check-in-out', json_data
+
+    assert_equal 201, last_response.status
+    assert_equal last_response_body['checked_out'], true
+    assert_equal checkin.reload.checkout_at, expected_time
   end
 
-  def test_seat_assignment_records_seat_correctly_when_there_is_a_current_checkin_session
-    # TODO
-  end
+  # def test_seat_assignment_records_seat_correctly_when_there_is_a_current_checkin_session
+  #   # TODO
+  # end
 
-  def test_seat_assignment_fails_when_there_is_no_current_checkin_session
-    # TODO
-  end
+  # def test_seat_assignment_fails_when_there_is_no_current_checkin_session
+  #   # TODO
+  # end
 end
