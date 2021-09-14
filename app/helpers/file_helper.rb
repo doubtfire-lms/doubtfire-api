18280a6e1611d7da3dea7966565568be9db8a65d
vuthanh <vuthanh@deakin.edu.au>
2021-09-14 18:34:44 +1000

    config: file support for hack , jack ,asm and hdl

--- app/helpers/file_helper.rb
+++ app/helpers/file_helper.rb
@@ -18,8 +18,8 @@ module FileHelper
       accept = ['image/png', 'image/gif', 'image/bmp', 'image/tiff', 'image/jpeg', 'image/x-ms-bmp']
     when 'code'
       accept = ['text/x-pascal', 'text/x-c', 'text/x-c++', 'text/plain', 'text/', 'application/javascript, text/html',
-                'text/css', 'text/x-ruby', 'text/coffeescript', 'text/x-scss', 'application/json', 'text/xml', 'application/xml',
-                'text/x-yaml', 'application/xml', 'text/x-typescript']
+        'text/css', 'text/x-ruby', 'text/coffeescript', 'text/x-scss', 'application/json', 'text/xml', 'application/xml',
+        'text/x-yaml', 'application/xml', 'text/x-typescript','text/x-hdl','text/x-asm','text/x-jack','text/x-hack']
     when 'document'
       accept = [ # -- one day"application/vnd.openxmlformats-officedocument.wordprocessingml.document",
         # --"application/msword",
