73070489da0c4d8908f5716d778d1bb97df81219
Adhitya Ram <Adhityaram417@gmail.com>
2021-09-14 20:54:02 +1000

    fix: fix the mime types

--- app/helpers/file_helper.rb
+++ app/helpers/file_helper.rb
@@ -19,7 +19,7 @@ module FileHelper
     when 'code'
       accept = ['text/x-pascal', 'text/x-c', 'text/x-c++', 'text/plain', 'text/', 'application/javascript, text/html',
         'text/css', 'text/x-ruby', 'text/coffeescript', 'text/x-scss', 'application/json', 'text/xml', 'application/xml',
-        'text/x-yaml', 'application/xml', 'text/x-typescript','text/x-hdl','text/x-asm','text/x-jack','text/x-hack']
+        'text/x-yaml', 'application/xml', 'text/x-typescript','text/x-vhdl','text/x-asm','text/x-jack','application/x-httpd-php']
     when 'document'
       accept = [ # -- one day"application/vnd.openxmlformats-officedocument.wordprocessingml.document",
         # --"application/msword",
