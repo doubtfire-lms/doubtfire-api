a18eb8d1d653c00031323fce35f4a9be010d5c20
Jake Renzella <j@kerenzella.com>
2021-09-20 14:47:32 +1000

    new: add saml envioronment variables

--- app/helpers/authentication_helpers.rb
+++ app/helpers/authentication_helpers.rb
@@ -71,6 +71,9 @@ module AuthenticationHelpers
       settings.assertion_consumer_service_url = Doubtfire::Application.config.saml[:consumer_target_url]
       settings.sp_entity_id                   = Doubtfire::Application.config.saml[:entity_id]
       settings.idp_sso_target_url             = Doubtfire::Application.config.saml[:idp_sso_target_url]
+      settings.idp_slo_target_url             = Doubtfire::Application.config.saml[:idp_sso_target_url]
+      settings.idp_cert                       = Doubtfire::Application.config.saml[:idp_cert]
+
       settings.name_identifier_format         = "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress"
 
       # Optional for most SAML IdPs
