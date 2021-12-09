b7582eb72e54d979f7ac960f1512e2dcb4bdccb8
Andrew Cain <macite@gmail.com>
2021-12-09 16:37:09 +1100

    fix: simplify migration to replace root path

--- db/migrate/20211208231733_make_evidence_path_relative.rb
+++ db/migrate/20211208231733_make_evidence_path_relative.rb
@@ -2,10 +2,10 @@ class MakeEvidencePathRelative < ActiveRecord::Migration
   def up
     root = FileHelper.student_work_dir
 
-    connection.exec_update(<<-EOQ, "SQL", [[nil, root], [nil, "#{root}%"]])
+    connection.exec_update(<<-EOQ, "SQL", [])
       UPDATE  tasks
-      SET     portfolio_evidence = REPLACE(portfolio_evidence, $1, '')
-      WHERE   portfolio_evidence like $2
+      SET     portfolio_evidence = REPLACE(portfolio_evidence, '#{root}'', '')
+      WHERE   portfolio_evidence like '#{root}%'
     EOQ
   end
 
