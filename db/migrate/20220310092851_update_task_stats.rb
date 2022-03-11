eb5ef83ca899bfb872f93eb9cdae8d7844f7c2b7
Andrew Cain <macite@gmail.com>
2022-03-12 09:04:09 +1100

    perf: ensure that project updates happoen in batches

--- db/migrate/20220310092851_update_task_stats.rb
+++ db/migrate/20220310092851_update_task_stats.rb
@@ -1,8 +1,8 @@
 class UpdateTaskStats < ActiveRecord::Migration[7.0]
   def change
     # Migrate all projects data - update the task stats
-    Project.all.each do |project|
-      project.update_task_stats
+    Project.find_in_batches do |projects|
+      projects.each { |project| project.update_task_stats }
     end
   end
 end
