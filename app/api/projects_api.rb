9ba5a5069f5c8b8d30e27156dbc0b4914c9fc413
Andrew Cain <macite@gmail.com>
2022-03-16 23:03:03 +1100

    fix: post project uses default task stats

--- app/api/projects_api.rb
+++ app/api/projects_api.rb
@@ -192,7 +192,7 @@ class ProjectsApi < Grape::API
           grade_rationale: proj.grade_rationale,
           max_pct_copy: 0,
           has_portfolio: false,
-          stats: Project.DEFAULT_TASK_STATS
+          stats: Project::DEFAULT_TASK_STATS
         }
         present result, with: Grape::Presenters::Presenter
       end
