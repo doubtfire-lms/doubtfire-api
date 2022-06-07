17d5a9840366d7546d7e6e2a77cf835f2219d1c3
Andrew Cain <macite@gmail.com>
2022-06-07 10:05:58 +1000

    feat: only grant extensions on positive feedback responses

--- app/models/task.rb
+++ app/models/task.rb
@@ -520,7 +520,7 @@ class Task < ApplicationRecord
 
       # Grant an extension on fix if due date is within 1 week
       case task_status
-      when TaskStatus.redo, TaskStatus.fix_and_resubmit, TaskStatus.discuss, TaskStatus.demonstrate
+      when TaskStatus.fix_and_resubmit, TaskStatus.discuss, TaskStatus.demonstrate
         if to_same_day_anywhere_on_earth(due_date) < Time.zone.now + 7.days && can_apply_for_extension? && unit.extension_weeks_on_resubmit_request > 0
           grant_extension(assessor, unit.extension_weeks_on_resubmit_request)
         end
