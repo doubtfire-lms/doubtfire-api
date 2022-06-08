a65f3898b19678ecc906395938ae809c8785d20e
Andrew Cain <macite@gmail.com>
2022-06-08 10:06:10 +1000

    refactor: update user export data

--- app/api/entities/user_entity.rb
+++ app/api/entities/user_entity.rb
@@ -2,20 +2,19 @@
 module Entities
   class UserEntity < Grape::Entity
     expose :id
-    expose :student_id
+    expose :student_id, unless: :minimal
     expose :email
-    expose :name
     expose :first_name
     expose :last_name
     expose :username
     expose :nickname
-    expose :receive_task_notifications
-    expose :receive_portfolio_notifications
-    expose :receive_feedback_notifications
-    expose :opt_in_to_research
-    expose :has_run_first_time_setup
+    expose :receive_task_notifications, unless: :minimal
+    expose :receive_portfolio_notifications, unless: :minimal
+    expose :receive_feedback_notifications, unless: :minimal
+    expose :opt_in_to_research, unless: :minimal
+    expose :has_run_first_time_setup, unless: :minimal
 
-    expose :system_role do |user, options|
+    expose :system_role, unless: :minimal do |user, options|
       user.role.name if user.role.present?
     end
   end
