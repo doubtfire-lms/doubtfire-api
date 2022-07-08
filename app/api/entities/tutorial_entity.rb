3def8db24e6706d57da96365b8416fb022179b57
Andrew Cain <macite@gmail.com>
2022-07-08 22:40:19 +1000

    refactor: update tutorial entity to match new entity service requirements

--- app/api/entities/tutorial_entity.rb
+++ app/api/entities/tutorial_entity.rb
@@ -2,17 +2,17 @@ module Entities
   class TutorialEntity < Grape::Entity
     expose :id
     expose :meeting_day
-    expose :meeting_time # ?? should we use: tutorial.meeting_time.to_time
+    expose :meeting_time
     expose :meeting_location
     expose :abbreviation
-    expose :campus_id
+    expose :campus_id, expose_nil: false
     expose :capacity
-    expose :tutorial_stream do |tutorial, options|
+    expose :tutorial_stream_abbr, expose_nil: false do |tutorial, options|
       tutorial.tutorial_stream.abbreviation unless tutorial.tutorial_stream.nil?
     end
     expose :num_students #TODO: remove this and request it dynamically when needed
-    expose :tutor do |tutorial, options|
-      Entities::UserEntity.represent tutorial.tutor, only: [:id, :name]
+    expose :tutor_id, expose_nil: false do |tutorial, options|
+      tutorial.tutor.id unless tutorial.tutor.nil?
     end
   end
 end
