e0310810efa0a36e1f720617a47ee7a70d6d6214
Andrew Cain <macite@gmail.com>
2022-07-11 23:51:06 +1000

    refactor: change get students to get enrolled or withdrawn students
    Will enable front end to query for all students on unit load, and withdrawn students
    when accessing students in the editor

--- app/api/students_api.rb
+++ app/api/students_api.rb
@@ -11,13 +11,13 @@ class StudentsApi < Grape::API
   desc 'Get users'
   params do
     requires :unit_id, type: Integer, desc: 'The unit to get the students for'
-    optional :all, type: Boolean, desc: 'Show all students or just current students'
+    optional :withdrawn, type: Boolean, desc: 'Show all students or just current students'
   end
   get '/students' do
     unit = Unit.find(params[:unit_id])
 
     if (authorise? current_user, unit, :get_students) || (authorise? current_user, User, :admin_units)
-      result = if params[:all].nil? || (!params[:all].nil? && !params[:all])
+      result = if params[:withdrawn].nil? || (!params[:withdrawn].nil? && !params[:withdrawn])
                   unit.student_query(true)
                 else
                   unit.student_query(false)
