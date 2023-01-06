185eb5e1d68e06f30b6f837526d03df9eb57aa41
Andrew Cain <macite@gmail.com>
2023-01-06 15:22:18 +1100

    test: add tii test to create submission

--- test/factories/projects_factory.rb
+++ test/factories/projects_factory.rb
@@ -1,7 +1,8 @@
 FactoryBot.define do
   factory :project do
-    unit
-    campus
+    unit { create(:unit, with_students:false) }
+    campus { Campus.offset(rand(Campus.count)).first || create(:campus) }
+    task_stats { Project::DEFAULT_TASK_STATS }
     user
   end
 end
