f236c228e3391dbf19deed472dac5fd804e463a9
Andrew Cain <macite@gmail.com>
2023-03-20 22:16:29 +1100

    refactor: fix rubocop for overseer changes

--- app/models/overseer_image.rb
+++ app/models/overseer_image.rb
@@ -10,7 +10,7 @@ class OverseerImage < ApplicationRecord
   # Always add a unique index with uniqueness constraint
   # This is to prevent new records from passing the validations when checked at the same time before being written
   validates :name,  presence: true, uniqueness: true
-  validates :tag,   presence: true, uniqueness: true, format: { with: /\A([\w.\-_]+((?::\d+|)(?=\/[a-z0-9._-]+\/[a-z0-9._-]+))|)(?:\/|)([a-z0-9.\-_]+(?:\/[a-z0-9.\-_]+|))(:([\w.\-_]{1,127})|)\z/ }
+  validates :tag,   presence: true, uniqueness: true, format: { with: %r{\A([\w.\-_]+((?::\d+|)(?=/[a-z0-9._-]+/[a-z0-9._-]+))|)(?:/|)([a-z0-9.\-_]+(?:/[a-z0-9.\-_]+|))(:([\w.\-_]{1,127})|)\z} }
 
   enum pulled_image_status: { failed: 0, success: 1 }
 
@@ -20,17 +20,17 @@ class OverseerImage < ApplicationRecord
       return false
     end
 
-    #TODO: add global repository ... #{global_registry_setting}#{tag}... so you can add a global registry without it being added to the tag name
+    # TODO: add global repository ... #{global_registry_setting}#{tag}... so you can add a global registry without it being added to the tag name
     self.last_pulled_date = Time.zone.now
     cmd = "docker pull #{tag}"
     out_text, error_text, exit_status = Open3.capture3(cmd)
     self.pulled_image_text = "#{cmd}\n#{out_text}\n#{error_text}"
 
-    if (exit_status != 0)
-      self.pulled_image_status = :failed
-    else 
-      self.pulled_image_status = :success
-    end
+    self.pulled_image_status = if exit_status == 0
+                                 :success
+                               else
+                                 :failed
+                               end
 
     logger.debug self.pulled_image_text
 
