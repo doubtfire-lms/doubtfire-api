04429a3a5617c10d1d0e1ddaceb233ef0fc78646
Andrew Cain <macite@gmail.com>
2023-08-02 13:40:16 +1000

    fix: ensure tii initializer loads correctly

--- config/initializers/turn_it_in_initializer.rb
+++ config/initializers/turn_it_in_initializer.rb
@@ -1,6 +1,8 @@
-require_relative '../../app/models/application_record'
-require_relative '../../app/models/turn_it_in/tii_action'
-require_relative '../../app/models/turn_it_in/tii_action_fetch_features_enabled'
-require_relative '../../app/models/turn_it_in/tii_action_fetch_eula'
-
-TurnItIn.launch_tii(with_webhooks: Rails.env.production?) if Doubtfire::Application.config.tii_enabled && !Rails.env.test?
+# Initialise TurnItIn API
+# - get eula
+# - get details
+if Doubtfire::Application.config.tii_enabled && !Rails.env.test?
+  Doubtfire::Application.config.after_initialize do
+    TurnItIn.launch_tii(with_webhooks: Rails.env.production?)
+  end
+end
