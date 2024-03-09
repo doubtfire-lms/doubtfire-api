ae2208c344c0959382e833557ccc69227e471da3
Andrew Cain <macite@gmail.com>
2024-03-09 21:41:20 +1100

    feat: cache tii details in files

--- app/models/turn_it_in/tii_action_fetch_eula.rb
+++ app/models/turn_it_in/tii_action_fetch_eula.rb
@@ -7,7 +7,7 @@ class TiiActionFetchEula < TiiAction
   end
 
   def run
-    if fetch_eula_version
+    if fetch_eula_from_tii
       self.complete = true
     else
       retry_request
@@ -23,8 +23,27 @@ class TiiActionFetchEula < TiiAction
       last_eula_check < DateTime.now - 1.day
   end
 
-  # Connect to tii to get the latest eula details.
-  def fetch_eula_version
+  def eula?
+    Rails.cache.exist?('tii.eula_version')
+  end
+
+  def eula_yaml_path
+    "#{FileHelper.student_work_root}/tii_eula.yml"
+  end
+
+  def load_eula_yaml
+    require 'yaml' # Built in, no gem required
+    YAML::load_file(eula_yaml_path, permitted_classes: [Time, DateTime, TCAClient::EulaVersion]) if File.exist?(eula_yaml_path) # Load
+  rescue
+    nil
+  end
+
+  def save_eula_yaml(data)
+    require 'yaml' # Built in, no gem required
+    File.write eula_yaml_path, data.to_yaml
+  end
+
+  def fetch_eula_from_tii
     exec_tca_call 'fetch TII EULA version' do
       api_instance = TCAClient::EULAApi.new
       eula = api_instance.eula_version_id_get(
@@ -39,8 +58,32 @@ class TiiActionFetchEula < TiiAction
       # Also check the html
       html = fetch_eula_html(eula.version)
 
+      # Update the eula yaml
+      if eula.present? && html.present?
+        data = {}
+        data['expire'] = DateTime.now + 2.days
+        data['eula'] = eula
+        data["html-#{eula.version}"] = html
+
+        # Save the eula to a cache file
+        save_eula_yaml(data)
+      end
+
       # return the eula
       eula.present? && html.present?
+    end
+  end
+
+  # Connect to tii to get the latest eula details.
+  def fetch_eula_version
+    # Attempt to load the eula from file
+    data = load_eula_yaml
+    if data && data['eula'] && data['expire'] && data['expire'] > DateTime.now
+      # update cache
+      Rails.cache.write('tii.eula_version', data['eula'], expires_in: 48.hours)
+      true
+    else
+      fetch_eula_from_tii
     end
   end
 
@@ -48,14 +91,18 @@ class TiiActionFetchEula < TiiAction
   def fetch_eula_html(eula_version)
     # Only update if we do not have the html for this version...
     Rails.cache.fetch("tii.eula_html.#{eula_version}", expires_in: 365.days) do
-      exec_tca_call "fetch TII EULA html for #{eula_version}" do
-        # Get the eula html - and return it to store in cache
-        TCAClient::EULAApi.new.eula_version_id_view_get(
-          TurnItIn.x_turnitin_integration_name,
-          TurnItIn.x_turnitin_integration_version,
-          eula_version
-        )
-      end
+      require 'yaml' # Built in, no gem required
+      data = load_eula_yaml # Load
+
+      html =  (data && data["html-#{eula_version}"]) ||
+              exec_tca_call("fetch TII EULA html for #{eula_version}") do
+                # Get the eula html - and return it to store in cache
+                TCAClient::EULAApi.new.eula_version_id_view_get(
+                  TurnItIn.x_turnitin_integration_name,
+                  TurnItIn.x_turnitin_integration_version,
+                  eula_version
+                )
+              end
     end
   end
 end
