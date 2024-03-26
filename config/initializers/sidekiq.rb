e9628eb31398719d78508a610a251a785f56a14f
ublefo <90136978+ublefo@users.noreply.github.com>
2024-03-27 02:51:50 +1100

    fix: pick up redis url from env for sidekiq if present

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,9 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379/1' }
+  config.redis = { url: ENV.fetch('DF_REDIS_SIDEKIQ_URL', 'redis://localhost:6379/1') }
   config.logger = Rails.logger
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379/1' }
+  config.redis = { url: ENV.fetch('DF_REDIS_SIDEKIQ_URL', 'redis://localhost:6379/1') }
   config.logger = Rails.logger
 end
