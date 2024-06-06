f10b7ba25c5f1897933602e94c00735719451882
Andrew Cain <macite@gmail.com>
2024-06-06 14:23:50 +1000

    chore: minor fixes to tests and schema post merge

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_05_28_223908) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_28_223908) do
   create_table "activity_types", charset: "utf8", collation: "utf8_unicode_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "abbreviation", null: false
