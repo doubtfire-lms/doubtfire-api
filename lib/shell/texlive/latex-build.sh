1878cccf94263c9d2034781bf0fd765ff38e40db
b0ink <40929320+b0ink@users.noreply.github.com>
2024-08-17 00:42:49 +1000

    refactor: create additional work subfolder
    * solves current permission issues that prevented the working folder from being rm -rf'd from the api
    * this ensures that input.pdf and input.log still exist when the script successfully completes

--- lib/shell/texlive/latex-build.sh
+++ lib/shell/texlive/latex-build.sh
@@ -1,11 +1,29 @@
 #!/bin/sh
 
 # Name of the working directory
-WORK_DIR=$1 
+WORK_DIR=$1
 
-cd /workdir/texlive-latex/${WORK_DIR}
+mkdir -p /workdir/texlive-latex/${WORK_DIR}/work
 
-# TODO: debug test file submissions: '002-code.cs', 'java_with_invalid_unicode.java', 'long.ipynb' ... (are they supposed to fail lualatex compile?)
+# Copy input.tex and jupynotex.py into the /work folder
+cp -r /workdir/texlive-latex/${WORK_DIR}/*.tex /workdir/texlive-latex/${WORK_DIR}/work/
+cp -r /workdir/texlive-latex/${WORK_DIR}/*.py /workdir/texlive-latex/${WORK_DIR}/work/
+
+cd /workdir/texlive-latex/${WORK_DIR}/work
+
+# Compile PDFs
 /bin/bash -c "lualatex -shell-escape -interaction=batchmode -halt-on-error input.tex"
 echo "Running lualatex a second time to remove temporary last page..."
 /bin/bash -c "lualatex -shell-escape -interaction=batchmode -halt-on-error input.tex"
+
+# Copy input.pdf and input.log into #{WORK_DIR}
+if [ -f "input.pdf" ]; then
+  cp input.pdf ../
+fi
+if [ -f "input.log" ]; then
+  cp input.log ../
+fi
+
+# Remove /work directory - leaving input.pdf, input.log, and
+cd ..
+rm -rf work
