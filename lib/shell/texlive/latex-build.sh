99fc05aaaf30d8698bba004251d5486e4e106529
b0ink <40929320+b0ink@users.noreply.github.com>
2024-08-18 04:03:55 +1000

    refactor: reorganise latex script

--- lib/shell/texlive/latex-build.sh
+++ lib/shell/texlive/latex-build.sh
@@ -1,29 +1,20 @@
 #!/bin/sh
 
-# Name of the working directory
-WORK_DIR=$1
+OUTPUT_DIR=$1
 
-mkdir -p /workdir/texlive-latex/${WORK_DIR}/work
+cd /workdir/texlive-latex/${OUTPUT_DIR}
 
-# Copy input.tex and jupynotex.py into the /work folder
-cp -r /workdir/texlive-latex/${WORK_DIR}/*.tex /workdir/texlive-latex/${WORK_DIR}/work/
-cp -r /workdir/texlive-latex/${WORK_DIR}/*.py /workdir/texlive-latex/${WORK_DIR}/work/
+# Initialise work subfolder
+mkdir -p work
+cp *.tex *.py work/
+cd work
 
-cd /workdir/texlive-latex/${WORK_DIR}/work
-
-# Compile PDFs
-/bin/bash -c "lualatex -shell-escape -interaction=batchmode -halt-on-error input.tex"
+# Compile PDF
+lualatex -shell-escape -interaction=batchmode -halt-on-error input.tex
 echo "Running lualatex a second time to remove temporary last page..."
-/bin/bash -c "lualatex -shell-escape -interaction=batchmode -halt-on-error input.tex"
+lualatex -shell-escape -interaction=batchmode -halt-on-error input.tex
 
-# Copy input.pdf and input.log into #{WORK_DIR}
-if [ -f "input.pdf" ]; then
-  cp input.pdf ../
-fi
-if [ -f "input.log" ]; then
-  cp input.log ../
-fi
-
-# Remove /work directory, preserving input.pdf + input.log + original input files
+# Copy PDF to parent directory and cleanup
+cp *.pdf *.log ../
 cd ..
 rm -rf work
