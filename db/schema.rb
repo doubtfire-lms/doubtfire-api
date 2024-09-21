a9f746420350e939225b27ae88fd07c53c10536a
Andrew Cain <macite@gmail.com>
2024-09-22 08:19:42 +1000

    fix: update schema with latest commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_01_221318) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_20_052508) do
   create_table "activity_types", charset: "utf8", collation: "utf8_unicode_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "abbreviation", null: false
