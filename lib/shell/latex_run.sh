49324624371b75e347c00c31a527a7ca2ff9fa6b
b0ink <40929320+b0ink@users.noreply.github.com>
2024-09-22 00:57:23 +1000

    fix: allow docker exec to work in gh actions

--- lib/shell/latex_run.sh
+++ lib/shell/latex_run.sh
@@ -2,4 +2,4 @@
 # This script is copied into tmp/rails-latex/$WORK_DIR/ and executed by rails-latex
 
 WORK_DIR=$(basename "$PWD")
-sudo docker exec -it $LATEX_CONTAINER_NAME $LATEX_BUILD_PATH $WORK_DIR
+docker exec -t $LATEX_CONTAINER_NAME $LATEX_BUILD_PATH $WORK_DIR
