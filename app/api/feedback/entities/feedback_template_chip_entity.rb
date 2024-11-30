1920cbce2776ee374da128c6143e917e900139a7
Jake Mansfield <jmansfield0209@gmail.com>
2024-11-30 22:54:09 +1100

    just committing small changes to work on rebase

--- app/api/feedback/entities/feedback_template_chip_entity.rb
+++ app/api/feedback/entities/feedback_template_chip_entity.rb
@@ -7,9 +7,9 @@ module Feedback
       expose :description
       expose :task_status
       expose :parent_chip_id
-      expose :learning_outcome
-      expose :related_entity
-      expose :section
+      expose :learning_outcome # becomes id
+      expose :related_entity # can be gotten from learning outcome
+      expose :section # removed
 
       expose :comment_text
       expose :summary_text
