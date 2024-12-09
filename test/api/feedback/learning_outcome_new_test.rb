982d443f744ba0221715ba7f49aff80b5d96fdee
Jake Mansfield <jmansfield0209@gmail.com>
2024-12-10 00:33:58 +1100

    fixed task_definition learning_outcome implementation and added appropriate tests

--- test/api/feedback/learning_outcome_new_test.rb
+++ test/api/feedback/learning_outcome_new_test.rb
@@ -81,4 +81,72 @@ class LearningOutcomeNewTest < ActiveSupport::TestCase
     unit.destroy
   end
 
+  def test_create_global_outcome
+    data_to_post = {
+      context_type: nil,
+      context_id: nil,
+      abbreviation: 'GLO2',
+      short_description: 'global short_description',
+      full_outcome_description: 'global full_outcome_description'
+    }
+    add_auth_header_for user: User.first
+    post_json "api/global/outcomes", data_to_post
+    puts last_response.body
+    assert_equal 201, last_response.status
+  end
+
+  def test_update_global_outcome
+    learning_outcome = FactoryBot.create(:learning_outcome, context_id: nil, context_type: nil, abbreviation: 'GLO2', short_description: 'global short_description', full_outcome_description: 'global full_outcome_description')
+    data_to_put = {
+      abbreviation: 'changed abbreviation',
+      short_description: 'changed short_description',
+      full_outcome_description: 'changed full_outcome_description'
+    }
+    add_auth_header_for user: User.first
+    put_json "api/global/outcomes/#{learning_outcome.id}", data_to_put
+    assert_equal 200, last_response.status
+    learning_outcome.destroy
+  end
+
+  def test_create_task_definition_learning_outcome
+    task_definition = FactoryBot.create(:task_definition)
+    data_to_post = {
+      context_type: 'TaskDefinition',
+      context_id: task_definition.id,
+      abbreviation: 'abbr',
+      short_description: 'sd',
+      full_outcome_description: 'fod'
+    }
+    add_auth_header_for user: User.first
+    post_json "api/task_definitions/#{task_definition.id}/outcomes", data_to_post
+    puts last_response.body
+    assert_equal 201, last_response.status
+    task_definition.destroy
+  end
+
+  def test_update_task_definition_learning_outcome
+    task_definition = FactoryBot.create(:task_definition)
+    learning_outcome = FactoryBot.create(:learning_outcome, context_id: task_definition.id, context_type: 'TaskDefinition', abbreviation: 'abbr', short_description: 'sd', full_outcome_description: 'fod')
+    data_to_put = {
+      abbreviation: 'changed abbreviation',
+      short_description: 'changed short_description',
+      full_outcome_description: 'changed full_outcome_description'
+    }
+    add_auth_header_for user: User.first
+    put_json "api/task_definitions/#{task_definition.id}/outcomes/#{learning_outcome.id}", data_to_put
+    puts last_response.body
+    assert_equal 200, last_response.status
+    task_definition.destroy
+    learning_outcome.destroy
+  end
+
+  def test_delete_task_definition_learning_outcome
+    task_definition = FactoryBot.create(:task_definition)
+    learning_outcome = FactoryBot.create(:learning_outcome, context_id: task_definition.id, context_type: 'TaskDefinition', abbreviation: 'abbr', short_description: 'sd', full_outcome_description: 'fod')
+    add_auth_header_for user: User.first
+    delete "api/task_definitions/#{task_definition.id}/outcomes/#{learning_outcome.id}"
+    assert_equal 204, last_response.status
+    task_definition.destroy
+  end
+
 end
