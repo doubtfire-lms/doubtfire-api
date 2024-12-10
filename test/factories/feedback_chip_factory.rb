d2a981348961d33a2d6ad9e9b61de874a3bc1112
Jake Mansfield <jmansfield0209@gmail.com>
2024-12-10 19:03:30 +1100

    added dummy data command, added faker to chip factories

--- test/factories/feedback_chip_factory.rb
+++ test/factories/feedback_chip_factory.rb
@@ -1,19 +1,19 @@
 FactoryBot.define do
   factory :feedback_group_chip, class: 'Feedback::FeedbackGroupChip' do
-    chip_text { "Sample title" }
-    description { "Sample description" }
+    chip_text { "Group Chip Text #{Faker::Lorem.word}" }
+    description { Faker::Lorem.sentence }
     parent_chip_id { nil }
     learning_outcome_id { 1 }
   end
 
   factory :feedback_template_chip, class: 'Feedback::FeedbackTemplateChip' do
-    chip_text { "Sample chip text" }
-    description { "Sample description" }
+    chip_text { "Template Chip Text #{Faker::Lorem.word}" }
+    description { Faker::Lorem.sentence }
     task_status_id { TaskStatus.complete.id }
     parent_chip_id { nil }
     learning_outcome_id { 1 }
 
-    comment_text { "Sample comment text" }
-    summary_text { "Sample summary text" }
+    comment_text { Faker::Lorem.sentence }
+    summary_text { Faker::Lorem.word }
   end
 end
