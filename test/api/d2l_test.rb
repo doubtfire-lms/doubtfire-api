21d78079ea691a8375d7c910fb7e35a7c07cec86
Andrew Cain <macite@gmail.com>
2024-12-17 20:59:22 +1100

    feat: ensure only convenor or higher can use d2l details

--- test/api/d2l_test.rb
+++ test/api/d2l_test.rb
@@ -40,4 +40,25 @@ class D2lTest < ActiveSupport::TestCase
 
     assert_equal initial_count, D2lAssessmentMapping.count
   end
+
+  def test_convenor_needed_for_d2l_details
+    unit = FactoryBot.create(:unit)
+    user = FactoryBot.create(:user, :student)
+    add_auth_header_for(user: user)
+
+    post "/api/units/#{unit.id}/d2l", { org_unit_id: '12345' }
+    assert_equal 403, last_response.status, last_response.inspect
+
+    user = FactoryBot.create(:user, :tutor)
+    add_auth_header_for(user: user)
+
+    post "/api/units/#{unit.id}/d2l", { org_unit_id: '12345' }
+    assert_equal 403, last_response.status, last_response.inspect
+
+    user = FactoryBot.create(:user, :auditor)
+    add_auth_header_for(user: user)
+
+    post "/api/units/#{unit.id}/d2l", { org_unit_id: '12345' }
+    assert_equal 403, last_response.status, last_response.inspect
+  end
 end
