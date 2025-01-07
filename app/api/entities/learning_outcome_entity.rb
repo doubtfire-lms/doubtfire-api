ccda2e9048a227ae3cd9691ecc96966f4b13c190
Jake Mansfield <jmansfield0209@gmail.com>
2025-01-07 15:05:45 +1100

    created test for future migration testing, fixed linked learning outcome id when returned

--- app/api/entities/learning_outcome_entity.rb
+++ app/api/entities/learning_outcome_entity.rb
@@ -6,8 +6,8 @@ module Entities
     expose :abbreviation
     expose :short_description
     expose :full_outcome_description
-    expose :linked_outcomes, using: LearningOutcomeEntity do |learning_outcome, options|
-      learning_outcome.linked_outcomes
+    expose :linked_outcomes do |learning_outcome, options|
+      learning_outcome.linked_outcomes.pluck(:id)
     end
   end
 end
