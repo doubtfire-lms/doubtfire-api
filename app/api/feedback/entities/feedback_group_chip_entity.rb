d89e0cdcf07e48684b04bcb444d165b200372c79
Jake Mansfield <jmansfield0209@gmail.com>
2025-01-13 20:10:39 +1100

    fixed some issues with csv endpoints

--- app/api/feedback/entities/feedback_group_chip_entity.rb
+++ app/api/feedback/entities/feedback_group_chip_entity.rb
@@ -2,7 +2,16 @@ module Feedback
   module Entities
     class FeedbackGroupChipEntity < Grape::Entity
       expose :id
-      expose :type
+      expose :type do |chip|
+        case chip.type
+        when 'Feedback::FeedbackTemplateChip'
+          'template'
+        when 'Feedback::FeedbackGroupChip'
+          'group'
+        else
+          'unknown'
+        end
+      end
       expose :chip_text
       expose :description
       expose :parent_chip_id
