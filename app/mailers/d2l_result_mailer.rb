f2804476e1ace924f37081f3ae756fc3b6873327
Andrew Cain <macite@gmail.com>
2025-01-23 21:20:40 +1100

    feat: email users on D2L grade transfer fail

--- app/mailers/d2l_result_mailer.rb
+++ app/mailers/d2l_result_mailer.rb
@@ -1,17 +1,19 @@
 class D2lResultMailer < ApplicationMailer
-  def result_message(unit, user)
+  def result_message(unit, user, result_message = 'completed', success = true)
     email = user.email
     return nil if email.blank?
 
     path = D2lIntegration.result_file_path(unit)
 
-    if File.exist?(path)
-      attachments['result.csv'] = File.read(path)
-    end
-
     @doubtfire_product_name = Doubtfire::Application.config.institution[:product_name]
     @user = user
     @unit = unit
+    @result_message = result_message
+    @has_file = success && File.exist?(path)
+
+    if @has_file
+      attachments['result.csv'] = File.read(path)
+    end
 
     mail(to: email, from: email, subject: "#{@doubtfire_product_name} #{unit.code} - D2L Grade Transfer Result")
   end
