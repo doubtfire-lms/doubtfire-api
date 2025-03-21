790b5dace8d76700bbfe4e73b0589b0a772701df
Andrew Cain <macite@gmail.com>
2025-03-21 15:15:12 +1100

    docs: document tables for feedback and outcome feature

--- app/models/feedback/feedback_template_chip.rb
+++ app/models/feedback/feedback_template_chip.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: feedback_chips
+#
+#  id                  :bigint           not null, primary key
+#  type                :string(255)
+#  chip_text           :text(65535)
+#  description         :text(65535)
+#  comment_text        :text(65535)
+#  summary_text        :text(65535)
+#  learning_outcome_id :bigint           not null
+#  parent_chip_id      :bigint
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  task_status         :string(255)
+#
 module Feedback
   class FeedbackTemplateChip < FeedbackChip
     validates :comment_text, presence: true
--- app/models/learning_outcome_link.rb
+++ app/models/learning_outcome_link.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: learning_outcome_links
+#
+#  id         :bigint           not null, primary key
+#  source_id  :bigint           not null
+#  target_id  :bigint           not null
+#  link_type  :string(255)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class LearningOutcomeLink < ApplicationRecord
   belongs_to :source, class_name: 'LearningOutcome'
   belongs_to :target, class_name: 'LearningOutcome'
