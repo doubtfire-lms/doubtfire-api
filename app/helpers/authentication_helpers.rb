b1e17bf1563c66de19f3f8bd6fa8072f2cd8fe5a
Andrew Cain <macite@gmail.com>
2025-04-01 16:21:23 +1100

    feat: enable saml config per institution
    - add metadata by file
    - allow customisation of new user details

--- app/helpers/authentication_helpers.rb
+++ app/helpers/authentication_helpers.rb
@@ -87,19 +87,25 @@ module AuthenticationHelpers
     return unless saml_auth?
 
     metadata_url = Doubtfire::Application.config.saml[:SAML_metadata_url] || nil
+    metadata_file = Doubtfire::Application.config.saml[:SAML_metadata_file_path] || nil
+
+    idp_metadata_parser = OneLogin::RubySaml::IdpMetadataParser.new
 
     if metadata_url
-      idp_metadata_parser = OneLogin::RubySaml::IdpMetadataParser.new
       settings = idp_metadata_parser.parse_remote(metadata_url)
+    elsif metadata_file && File.exist?(metadata_file)
+      metadata = File.read(metadata_file)
+      settings = idp_metadata_parser.parse(metadata)
     else
       settings = OneLogin::RubySaml::Settings.new
       settings.idp_cert                     = Doubtfire::Application.config.saml[:idp_sso_cert]
       settings.name_identifier_format       = Doubtfire::Application.config.saml[:idp_name_identifier_format]
     end
+
     settings.assertion_consumer_service_url = Doubtfire::Application.config.saml[:assertion_consumer_service_url]
     settings.sp_entity_id                   = Doubtfire::Application.config.saml[:entity_id]
     settings.idp_sso_target_url             = Doubtfire::Application.config.saml[:idp_sso_target_url]
-    settings.idp_slo_target_url             = Doubtfire::Application.config.saml[:idp_sso_target_url]
+    settings.idp_slo_target_url             = Doubtfire::Application.config.saml[:idp_sso_signout_url]
 
     settings
   end
