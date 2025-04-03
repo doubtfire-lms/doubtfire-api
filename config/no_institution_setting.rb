a4e2b11edc12539799d25ba16be5842b33846453
Andrew Cain <macite@gmail.com>
2025-04-03 16:48:46 +1100

    docs: add comments on institution setting methods

--- config/no_institution_setting.rb
+++ config/no_institution_setting.rb
@@ -1,6 +1,36 @@
 class InstitutionSettings
+
+  # Used when importing students from a CSV into a unit. This is called to check if
+  # the headers in the CSV match an institution system. If this returns true, the import
+  # will be processed using the lambdas you specify in the import_settings.
   def are_headers_institution_users?(headers)
     false
+  end
+
+  # Returns user import settings for an institution CSV format. When
+  # you return true from `are_headers_institution_users?`, these lambdas are used
+  # to process the CSV data.
+  def user_import_settings_for(headers)
+    {
+      missing_headers_lambda: ->(row) { headers - row.to_hash.keys }, # lambda to check if row is missing key data. Return a list of missing header names (list of string)
+      fetch_row_data_lambda: lambda { |row, unit|
+        # Return a hash with the data to import
+        {
+          unit_code: unit.code,
+          username: row["username"],
+          student_id: row["student_code"],
+          first_name: row["first_name"],
+          last_name: row["last_name"],
+          nickname: row["preferred_name"],
+          email: row["email_address"],
+          enrolled: true,
+          tutorials: [],
+          campus: row["campus"]
+        }
+      }, # lambda to convert row from csv to required import data (see student list in the sync function). Return a hash of the data.
+      replace_existing_tutorial: false, # when true, old tutorials are replaced. When false, if the student already has a tutorial enrolment it is not updated even if different.
+      replace_existing_campus: false # as with tutorials, but for campus
+    }
   end
 
   def extract_user_from_row(row)
@@ -14,6 +44,36 @@ class InstitutionSettings
       tutorials: nil
     }
   end
+
+  # Main entry point for processing - passes in the unit to be synced
+  # this should query your institution system and then update enrolments
+  # in the unit.
+  #
+  # You can use the helper method in the unit:
+  #
+  # unit.sync_enrolment_with(student_list, import_settings, result)
+  #
+  # `student_list` contains the list of changes to make. Each item is the list is a hash representing a unique student.
+  # The has should contain the following keys:
+  #   - :row the string data associated with the change for error reporting
+  #   - :username
+  #   - :student_id
+  #   - :first_name
+  #   - :last_name
+  #   - :nickname
+  #   - :email
+  #   - :tutorial_codes - a list of tutorial codes. These should already exist. The student will be enrolled in all tutorials passed in.
+  #   - :enrolled (boolean) - true if the student should be enrolled, false to withdraw the student
+  #   - :campus
+  #
+  # `import_settings` is a hash with the following keys:
+  #   - :missing_headers_lambda - lambda to check if row is missing key data
+  #   - :fetch_row_data_lambda - lambda to convert row from csv to required import data
+  #   - :replace_existing_tutorial - boolean to indicate if tutorials in csv override ones in doubtfire
+  #   - :replace_existing_campus - boolean to indicate if campus in csv override ones in doubtfire
+  #
+  # For this sync, the main settings would be replace_existing_tutorial and replace_existing_campus. The other settings are used when importing from CSV.
+  #
 
   def sync_enrolments(unit)
     # rubocop:disable Rails/Output
