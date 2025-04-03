91ffa3afa9c422b79a298192a81b501d9b4ab6c5
Andrew Cain <macite@gmail.com>
2025-04-04 10:12:58 +1100

    docs: add details on latex env vars

--- lib/shell/latex_run.sh
+++ lib/shell/latex_run.sh
@@ -2,4 +2,4 @@
 # This script is copied into tmp/rails-latex/$WORK_DIR/ and executed by rails-latex
 
 WORK_DIR=$(basename "$PWD")
-docker exec -t $LATEX_CONTAINER_NAME $LATEX_BUILD_PATH $WORK_DIR
+docker exec -t $LATEX_CONTAINER_NAME ${LATEX_BUILD_PATH:-/texlive/shell/latex_build.sh} $WORK_DIR
