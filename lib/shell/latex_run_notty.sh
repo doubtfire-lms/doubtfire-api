c364bccbe77b8b55347b251d6e53610160cf293c
Andrew Cain <macite@gmail.com>
2025-04-11 14:36:22 +1000

    chore: avoid required tty for github action test
    The -it setting in docker run for the latex command fails. Replace teh latex run with the -i option instead.

---
+++ lib/shell/latex_run_notty.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# This script is copied into tmp/rails-latex/$WORK_DIR/ and executed by rails-latex
+
+WORK_DIR=$(basename "$PWD")
+docker exec -i $LATEX_CONTAINER_NAME ${LATEX_BUILD_PATH:-/texlive/shell/latex_build.sh} $WORK_DIR
