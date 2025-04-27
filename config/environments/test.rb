c454932bdcfd117b17de7441f4640630f2b97e5d
SahiruWithanage <s223047862@deakin.edu.au>
2025-04-27 15:51:11 +1000

    feat(notifications): send notifications when extensions are granted
    Implemented backend logic to send emails to tutor and student when extensions are granted. Also enable it so the front end can use the returned information from the api to display notifications.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -27,6 +27,9 @@ Doubtfire::Application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'test.host' }
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
