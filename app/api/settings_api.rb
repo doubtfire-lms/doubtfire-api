e5c20cc5d33c01987e0101eb744c1cf3cd18fad1
theiris6 <143162156+theiris6@users.noreply.github.com>
2025-05-21 00:08:38 +1000

    Added error handling for settings endpoint Added authentication check for /settings/privacy endpoint

--- app/api/settings_api.rb
+++ app/api/settings_api.rb
@@ -10,17 +10,23 @@ class SettingsApi < Grape::API
   get '/settings' do
     # Require authentication for the main settings endpoint
     authenticated?
-    response = {
-      externalName: Doubtfire::Application.config.institution[:product_name],
-      hasLogo: Doubtfire::Application.config.institution[:has_logo],
-      logoUrl: Doubtfire::Application.config.institution[:logo_url],
-      logoLinkUrl: Doubtfire::Application.config.institution[:logo_link_url],
-      overseerEnabled: Doubtfire::Application.config.overseer_enabled,
-      tiiEnabled: TurnItIn.enabled?,
-      d2lEnabled: D2lIntegration.enabled?
-    }
 
-    present response, with: Grape::Presenters::Presenter
+    begin
+      response = {
+        externalName: Doubtfire::Application.config.institution[:product_name],
+        hasLogo: Doubtfire::Application.config.institution[:has_logo],
+        logoUrl: Doubtfire::Application.config.institution[:logo_url],
+        logoLinkUrl: Doubtfire::Application.config.institution[:logo_link_url],
+        overseerEnabled: Doubtfire::Application.config.overseer_enabled,
+        tiiEnabled: TurnItIn.enabled?,
+        d2lEnabled: D2lIntegration.enabled?
+      }
+
+      present response, with: Grape::Presenters::Presenter
+    rescue StandardError => e
+      logger.error "Error fetching settings: #{e.message}"
+      error!({ error: "Could not retrieve settings due to an internal error" }, 500)
+    end
   end
 
   #
@@ -38,6 +44,8 @@ class SettingsApi < Grape::API
 
   desc 'Return privacy policy details'
   get '/settings/privacy' do
+    authenticated?
+
     response = {
       privacy: Doubtfire::Application.config.institution[:privacy],
       plagiarism: Doubtfire::Application.config.institution[:plagiarism]
