cef0d362ae6846c6815f44a3a8d7d92fe57ee6a2
b0ink <40929320+b0ink@users.noreply.github.com>
2025-06-30 11:21:21 +1000

    chore: use rails.root.join for jplag report dir path

--- config/application.rb
+++ config/application.rb
@@ -72,7 +72,7 @@ module Doubtfire
     # File server location for storing JPLAG reports. Defaults to `jplag/results`
     # directory under root but is overridden using DF_JPLAG_REPORT_DIR environment
     # variable.
-    config.jplag_report_dir = ENV['DF_JPLAG_REPORT_DIR'] || "#{Rails.root}/jplag/results"
+    config.jplag_report_dir = ENV['DF_JPLAG_REPORT_DIR'] || Rails.root.join('jplag/results').to_s
 
     # ==> Load credentials from env
     credentials.secret_key_base = ENV.fetch('DF_SECRET_KEY_BASE', Rails.env.production? ? nil : '9e010ee2f52af762916406fd2ac488c5694a6cc784777136e657511f8bbc7a73f96d59c0a9a778a0d7cf6406f8ecbf77efe4701dfbd63d8248fc7cc7f32dea97')
