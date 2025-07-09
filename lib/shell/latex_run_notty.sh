5891f9eef538d2c3170ca4dda7043adb30939845
Andrew Cain <macite@gmail.com>
2025-07-09 21:07:57 +1000

    fix: remove need for iteractive terminal for latex run
    -it attaches to terminal, which may not exist and is not needed.

--- lib/shell/latex_run_notty.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# This script is copied into tmp/rails-latex/$WORK_DIR/ and executed by rails-latex
-
-WORK_DIR=$(basename "$PWD")
-docker exec -i $LATEX_CONTAINER_NAME ${LATEX_BUILD_PATH:-/texlive/shell/latex_build.sh} $WORK_DIR
