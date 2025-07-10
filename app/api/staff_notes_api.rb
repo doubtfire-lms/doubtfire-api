a340d748b2f10fa74fa7d94a63037fefb77a0e89
b0ink <40929320+b0ink@users.noreply.github.com>
2025-07-10 11:50:52 +1000

    feat: endpoint to update staff notes

--- app/api/staff_notes_api.rb
+++ app/api/staff_notes_api.rb
@@ -70,4 +70,23 @@ class StaffNotesApi < Grape::API
     present staff_note.destroyed?, with: Grape::Presenters::Presenter
   end
 
+  desc "Update a staff note for a project"
+  params do
+    requires :id, type: Integer, desc: 'The staff note id to update'
+    requires :note, type: String, desc: 'The text to update the staff note with'
+  end
+  put '/projects/:project_id/staff_notes/:id' do
+    project = Project.find(params[:project_id])
+    staff_note = StaffNote.find(params[:id])
+
+    unless authorise?(current_user, project, :create_staff_note) && staff_note.user.id == current_user.id
+      error!({ error: 'You do not have permission to edit this note.' }, 403)
+    end
+
+    error!({ error: 'Note does not belong to this project' }, 404) if staff_note.project_id != project.id
+
+    staff_note.update!(note: params[:note])
+    present staff_note, with: Entities::StaffNoteEntity, user: current_user
+  end
+
 end
