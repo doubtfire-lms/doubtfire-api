fbb70fcdea8cdf183308e9639279a47f14da5798
b0ink <40929320+b0ink@users.noreply.github.com>
2025-08-09 00:58:05 +1000

    feat: lti user enrolment

--- app/api/lti_api.rb
+++ app/api/lti_api.rb
@@ -40,4 +40,35 @@ class LtiApi < Grape::API
 
     status 200
   end
+
+  desc 'Enrol a student into a linked Lti unit'
+  params do
+    optional :ltik, type: String, desc: 'LtiKey provided with user info and deeplink resources'
+  end
+  post '/lti/enrol' do
+    authenticated?
+
+    token = decode_lti_token(params[:ltik])
+
+    unit_id = token["unit_id"]
+    if unit_id.nil?
+      error!({ error: 'Invalid LTI token.' }, 401)
+    end
+
+    unit = Unit.find_by(id: unit_id)
+    if unit.nil?
+      error!({ error: 'Unit does not exist' }, 404)
+    end
+
+    role = unit.role_for(current_user)
+    if role != Role.student && !role.nil?
+      # error!({ error: 'Failed to enrol, user is already staff.' }, 400)
+      return nil
+    end
+
+    # TODO: which campus?
+    project = unit.enrol_student(current_user, Campus.first)
+
+    present project, with: Entities::ProjectEntity, user: current_user, for_student: true, in_project: true
+  end
 end
