bf7b61266956f741069c7e13d934c90f3f2b22a7
b0ink <40929320+b0ink@users.noreply.github.com>
2025-08-19 16:43:55 +1000

    chore: validate task prerequisite row is unique

--- app/models/task_prerequisite.rb
+++ app/models/task_prerequisite.rb
@@ -3,8 +3,8 @@ class TaskPrerequisite < ApplicationRecord
   belongs_to :prerequisite, class_name: "TaskDefinition"
 
   validate :cannot_be_its_own_prerequisite
-
   validate :no_reverse_prerequisite
+  validates :prerequisite_id, uniqueness: { scope: :task_definition_id, message: "already exists for this task" }
 
   def no_reverse_prerequisite
     if TaskPrerequisite.exists?(task_definition: prerequisite, prerequisite: task_definition)
