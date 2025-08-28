2334eeb7245835870e0209dee7e134d4d1a6064a
b0ink <40929320+b0ink@users.noreply.github.com>
2025-08-28 13:01:17 +1000

    test: ensure prerequisite due date is not later than task

--- app/models/task_prerequisite.rb
+++ app/models/task_prerequisite.rb
@@ -2,11 +2,19 @@ class TaskPrerequisite < ApplicationRecord
   belongs_to :task_definition
   belongs_to :prerequisite, class_name: "TaskDefinition"
 
+  validate :prerequisite_due_date_not_after_task
   validate :cannot_be_its_own_prerequisite
   validate :no_reverse_prerequisite
   validates :prerequisite_id, uniqueness: { scope: :task_definition_id, message: "already exists for this task" }
   validate :same_unit
   validate :prerequisite_grade_not_higher
+
+  def prerequisite_due_date_not_after_task
+    return unless task_definition && prerequisite
+    if prerequisite.due_date > task_definition.due_date
+      errors.add(:prerequisite, "due date cannot be later than the task definition's due date")
+    end
+  end
 
   def no_reverse_prerequisite
     if TaskPrerequisite.exists?(task_definition: prerequisite, prerequisite: task_definition)
