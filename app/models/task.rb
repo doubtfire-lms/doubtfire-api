b2ce06d067aed6580eb562abbbcfc811fe985aca
b0ink <40929320+b0ink@users.noreply.github.com>
2025-09-15 12:51:34 +1000

    chore: prevent duplicate status comments during tutor feedback

--- app/models/task.rb
+++ app/models/task.rb
@@ -540,10 +540,14 @@ class Task < ApplicationRecord
           end
         end
 
-        assess status, by_user
+        lc = comments.last
+        # Prevent duplicate status comments during feedback
+        unless lc && lc.user == by_user && lc.comment == status.name && lc.task_status == status
+          assess status, by_user
 
-        # Add a status comment for new assessments - only recorded on submitter's task in groups
-        add_status_comment(by_user, status)
+          # Add a status comment for new assessments - only recorded on submitter's task in groups
+          add_status_comment(by_user, status)
+        end
       else
         # Attempt to move to tutor state by non-tutor
         return nil
