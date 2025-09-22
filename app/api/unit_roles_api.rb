6d2041fe9141fc63a4bc9c844cd16efff37ac194
b0ink <40929320+b0ink@users.noreply.github.com>
2025-09-22 14:52:10 +1000

    feat: add observer updating

--- app/api/unit_roles_api.rb
+++ app/api/unit_roles_api.rb
@@ -70,19 +70,28 @@ class UnitRolesApi < Grape::API
   params do
     requires :unit_role, type: Hash do
       requires :role_id, type: Integer, desc: 'The role to create with'
+      optional :observer_only, type: Boolean, desc: 'If the staff has read-only permissions'
     end
   end
   put '/unit_roles/:id' do
     unit_role = UnitRole.find_by(id: params[:id])
 
     unless (authorise? current_user, unit_role.unit, :employ_staff) || (authorise? current_user, User, :admin_units)
-      error!({ error: "Couldn't find Unit with id=#{params[:id]}" }, 403)
+      error!({ error: "Not authorised to update unit role with id=#{params[:id]}" }, 403)
     end
 
-    unit_role_parameters = ActionController::Parameters.new(params)
+    unit_role_params = params.dup
+
+    # Prevent staff from setting themselves as read-only
+    if unit_role_params[:unit_role][:observer_only] && unit_role.user.id == current_user.id
+      error!({ error: "You cannot make yourself an observer" })
+    end
+
+    unit_role_parameters = ActionController::Parameters.new(unit_role_params)
                                                        .require(:unit_role)
                                                        .permit(
-                                                         :role_id
+                                                         :role_id,
+                                                         :observer_only
                                                        )
 
     if unit_role_parameters[:role_id] == Role.tutor.id && unit_role.role == Role.convenor && unit_role.unit.convenors.count == 1
