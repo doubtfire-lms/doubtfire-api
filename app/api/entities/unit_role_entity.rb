74f01a91fdde6c2712e6927154f2fde0b97f9220
b0ink <40929320+b0ink@users.noreply.github.com>
2025-09-22 14:52:10 +1000

    feat: add observer updating

--- app/api/entities/unit_role_entity.rb
+++ app/api/entities/unit_role_entity.rb
@@ -4,5 +4,6 @@ module Entities
     expose :role do |unit_role, options| unit_role.role.name end
     expose :user, using: Entities::Minimal::MinimalUserEntity
     expose :unit, using: Entities::Minimal::MinimalUnitEntity, unless: :in_unit
+    expose :observer_only
   end
 end
