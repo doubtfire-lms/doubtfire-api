d5938ebfe7805656d49ab527bba3cdd04da083b3
b0ink <40929320+b0ink@users.noreply.github.com>
2025-09-30 09:29:29 +1000

    fix: correct path to jplag files

--- lib/shell/jplag_run.sh
+++ lib/shell/jplag_run.sh
@@ -11,11 +11,12 @@ WORK_ID=$5
 IMAGE_NAME="$DF_JPLAG_IMAGE_NAME"
 CONTAINER_NAME="jplag-$WORK_ID"
 
+PATH_TO_JPLAG_FILES="$DF_JPLAG_PATH_TO_WORKDIRS/$WORK_DIR_NAME"
+
 docker run --rm \
   --network none \
   -e TERM=xterm \
-  -e DF_LATEX_PATH_TO_WORKDIRS="$DF_LATEX_PATH_TO_WORKDIRS" \
   --volumes-from $DF_API_CONTAINER_NAME \
   --name "$CONTAINER_NAME" \
   "$IMAGE_NAME" \
-  java -jar /jplag/jplag-jar-with-dependencies.jar /tmp/jplag/"$WORK_DIR_NAME" -l "$LANGUAGE" --similarity-threshold="$THRESHOLD" -M RUN -r /tmp/jplag/"$WORK_DIR_NAME"/result.jplag
+  java -jar /jplag/jplag-jar-with-dependencies.jar "$PATH_TO_JPLAG_FILES" -l "$LANGUAGE" --similarity-threshold="$THRESHOLD" -M RUN -r "$PATH_TO_JPLAG_FILES"/result.jplag --overwrite
