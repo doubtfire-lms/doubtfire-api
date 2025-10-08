5d6f7f0b5603de833ded74a3a490842e523bc4e6
b0ink <40929320+b0ink@users.noreply.github.com>
2025-10-08 16:56:46 +1100

    chore: aggregate activity actions count

--- app/api/entities/marking_session_entity.rb
+++ app/api/entities/marking_session_entity.rb
@@ -2,12 +2,24 @@ module Entities
   class MarkingSessionEntity < Grape::Entity
     expose :id
     expose :user_id
-    expose :unit_id
-    expose :ip_address
+    # expose :unit_id
+    # expose :ip_address
     expose :start_time
     expose :end_time
     expose :duration_minutes
-    expose :created_at
-    expose :updated_at
+    # expose :created_at
+    # expose :updated_at
+
+    expose :comments_added do |session, _options|
+      session.session_activities.count { |act| act.action == 'add-comment' }
+    end
+
+    expose :assessments do |session, _options|
+      session.session_activities.count { |act| act.action == 'assessing' }
+    end
+
+    expose :submissions_opened do |session, _options|
+      session.session_activities.count { |act| act.action == 'get-submission-details' }
+    end
   end
 end
