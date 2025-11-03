740460137ef2d31440251edf930a6a1a1a46cc85
b0ink <40929320+b0ink@users.noreply.github.com>
2025-11-03 12:41:42 +1100

    refactor: use absolute paths to allow concurrent latex jobs

--- lib/shell/latex_build.sh
+++ lib/shell/latex_build.sh
@@ -3,27 +3,26 @@
 # This script is copied into the TeX Live container and remotely executed by latex_run.sh
 
 OUTPUT_DIR=$1
+BASE_DIR="/workdir/texlive-latex/${OUTPUT_DIR}"
+WORK_DIR="${BASE_DIR}/work"
 
-cd /workdir/texlive-latex/${OUTPUT_DIR}
 
 # Initialise work subfolder
-mkdir -p work
-cp *.tex *.py work/
-cd work
+mkdir -p "$WORK_DIR"
+cp "$BASE_DIR"/*.tex "$BASE_DIR"/*.py "$WORK_DIR"/
 
 # Compile PDF
-lualatex -shell-escape -interaction=batchmode -halt-on-error input.tex
+lualatex -output-directory="$WORK_DIR" -shell-escape -interaction=batchmode -halt-on-error "$WORK_DIR/input.tex"
 RESULT=$?
 if [ $RESULT -eq 0 ]; then
   echo "Running lualatex a second time to remove temporary last page..."
-  lualatex -shell-escape -interaction=batchmode -halt-on-error input.tex
+  lualatex -output-directory="$WORK_DIR" -shell-escape -interaction=batchmode -halt-on-error "$WORK_DIR/input.tex"
   RESULT=$?
 fi
 
 # Copy PDF to parent directory and cleanup
-cp *.log ../
-cp *.pdf ../
-cd ..
-rm -rf work
+cp "$WORK_DIR"/*.log "$BASE_DIR"/
+cp "$WORK_DIR"/*.pdf "$BASE_DIR"/
+rm -rf "$WORK_DIR"
 
 exit $RESULT
