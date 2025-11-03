660de95d6699f257bc4c445f41655db137f13e35
b0ink <40929320+b0ink@users.noreply.github.com>
2025-11-03 13:52:13 +1100

    chore: revert latex_build.sh

--- lib/shell/latex_build.sh
+++ lib/shell/latex_build.sh
@@ -2,29 +2,28 @@
 
 # This script is copied into the TeX Live container and remotely executed by latex_run.sh
 
-cd "/workdir/texlive-latex"
-
 OUTPUT_DIR=$1
-BASE_DIR="./${OUTPUT_DIR}"
-WORK_DIR="${BASE_DIR}/work"
 
+cd /workdir/texlive-latex/${OUTPUT_DIR}
 
 # Initialise work subfolder
-mkdir -p "$WORK_DIR"
-cp "$BASE_DIR"/*.tex "$BASE_DIR"/*.py "$WORK_DIR"/
+mkdir -p work
+cp *.tex *.py work/
+cd work
 
 # Compile PDF
-lualatex -output-directory="$WORK_DIR" -shell-escape -interaction=batchmode -halt-on-error "$WORK_DIR/input.tex"
+lualatex -shell-escape -interaction=batchmode -halt-on-error input.tex
 RESULT=$?
 if [ $RESULT -eq 0 ]; then
   echo "Running lualatex a second time to remove temporary last page..."
-  lualatex -output-directory="$WORK_DIR" -shell-escape -interaction=batchmode -halt-on-error "$WORK_DIR/input.tex"
+  lualatex -shell-escape -interaction=batchmode -halt-on-error input.tex
   RESULT=$?
 fi
 
 # Copy PDF to parent directory and cleanup
-cp "$WORK_DIR"/*.log "$BASE_DIR"/
-cp "$WORK_DIR"/*.pdf "$BASE_DIR"/
-rm -rf "$WORK_DIR"
+cp *.log ../
+cp *.pdf ../
+cd ..
+rm -rf work
 
 exit $RESULT
