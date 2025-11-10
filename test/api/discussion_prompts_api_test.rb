f5e27b9a8ab4b13add86dbc1dc661f592f081155
b0ink <40929320+b0ink@users.noreply.github.com>
2025-11-11 10:12:34 +1100

    test: ensure discussion prompt permissions

--- test/api/discussion_prompts_api_test.rb
+++ test/api/discussion_prompts_api_test.rb
@@ -95,14 +95,87 @@ class DiscussionPromptsApiTest < ActiveSupport::TestCase
   end
 
   def test_student_dicussion_prompts_permissions
-    # Ensure student cant GET discussion prompts
-    # Ensure student cant POST discussion prompts
-    # Ensure student cant PUT discussion prompts
-    # Ensure student cant DELETE discussion prompts
-    # Ensure tutors & convenors can GET discussion prompts
-    # Ensure tutors & convenors can POST discussion prompts
-    # Ensure tutors & convenors can PUT discussion prompts
-    # Ensure tutors & convenors can DELETE discussion prompts
+    unit = FactoryBot.create(:unit)
+    convenor = FactoryBot.create(:user, :convenor)
+    tutor = FactoryBot.create(:user, :tutor)
+
+    unit.employ_staff(convenor, Role.convenor)
+    unit.employ_staff(tutor, Role.tutor)
+
+    td = FactoryBot.create(:task_definition, unit: unit)
+
+    student = FactoryBot.create(:user, :student)
+    project = unit.enrol_student(student, nil)
+
+    users_can = [convenor, tutor]
+
+    users_cant = [student]
+
+    users_can.each do |user|
+      add_auth_header_for(user: user)
+
+      get "/api/task_definitions/#{td.id}/discussion_prompts"
+      assert_equal 200, last_response.status, last_response_body
+
+      post "/api/task_definitions/#{td.id}/discussion_prompts", {
+        content: 'test content 123',
+        weight: 1
+      }
+      assert_equal 201, last_response.status, last_response_body
+
+      last_prompt = DiscussionPrompt.last
+
+      assert_not_nil last_prompt
+      assert_equal 'test content 123', last_prompt.content
+      assert_equal 1, last_prompt.weight
+
+      put "/api/task_definitions/#{td.id}/discussion_prompts/#{last_prompt.id}", {
+        content: 'test content 456',
+        weight: 2
+      }
+      assert_equal 200, last_response.status, last_response_body
+      last_prompt.reload
+      assert_equal 'test content 456', last_prompt.content
+      assert_equal 2, last_prompt.weight
+
+      delete "/api/task_definitions/#{td.id}/discussion_prompts/#{last_prompt.id}"
+      assert_equal 200, last_response.status, last_response_body
+      assert_nil DiscussionPrompt.find_by(id: last_prompt.id)
+
+      get "/api/projects/#{project.id}/task_definitions/#{td.id}/discussion_prompts"
+      assert_equal 200, last_response.status, last_response_body
+
+      get "/api/projects/#{project.id}/discussion_prompts"
+      assert_equal 200, last_response.status, last_response_body
+    end
+
+    users_cant.each do |user|
+      add_auth_header_for(user: user)
+
+      get "/api/task_definitions/#{td.id}/discussion_prompts"
+      assert_equal 403, last_response.status, last_response_body
+
+      post "/api/task_definitions/#{td.id}/discussion_prompts", {
+        content: 'test',
+        weight: 0
+      }
+      assert_equal 403, last_response.status, last_response_body
+
+      put "/api/task_definitions/#{td.id}/discussion_prompts/1", {
+        content: 'test',
+        weight: 0
+      }
+      assert_equal 403, last_response.status, last_response_body
+
+      delete "/api/task_definitions/#{td.id}/discussion_prompts/1"
+      assert_equal 403, last_response.status, last_response_body
+
+      get "/api/projects/#{project.id}/task_definitions/#{td.id}/discussion_prompts"
+      assert_equal 403, last_response.status, last_response_body
+
+      get "/api/projects/#{project.id}/discussion_prompts"
+      assert_equal 403, last_response.status, last_response_body
+    end
   end
 
 end
