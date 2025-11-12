8065841df09b4931d93cf19169c2e97c764d60f1
b0ink <40929320+b0ink@users.noreply.github.com>
2025-11-12 14:29:35 +1100

    chore: remove unneeded fields

--- app/api/entities/discussion_prompt_entity.rb
+++ app/api/entities/discussion_prompt_entity.rb
@@ -2,10 +2,7 @@ module Entities
   class DiscussionPromptEntity < Grape::Entity
     expose :id
     expose :task_definition_id
-    expose :project_id
-    expose :created_by_id
     expose :content
     expose :weight
-    expose :discussed_at
   end
 end
