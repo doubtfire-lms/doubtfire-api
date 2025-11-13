cf96eb1097609551ae64c1da1d7ee19bf030011f
b0ink <40929320+b0ink@users.noreply.github.com>
2025-11-13 14:22:52 +1100

    feat: import and export discussion prompts via task definition csv

--- app/models/task_definition.rb
+++ app/models/task_definition.rb
@@ -471,7 +471,7 @@ class TaskDefinition < ApplicationRecord
 
   def to_csv_row
     TaskDefinition.csv_columns
-                  .reject { |col| [:start_week, :start_day, :target_week, :target_day, :due_week, :due_day, :upload_requirements, :group_set, :tutorial_stream, :assess_in_portfolio_only, :task_prerequisites].include? col }
+                  .reject { |col| [:start_week, :start_day, :target_week, :target_day, :due_week, :due_day, :upload_requirements, :group_set, :tutorial_stream, :assess_in_portfolio_only, :task_prerequisites, :discussion_prompts].include? col}
                   .map { |column| attributes[column.to_s] } +
       [
         group_set.nil? ? "" : group_set.name,
@@ -490,6 +490,12 @@ class TaskDefinition < ApplicationRecord
             abbreviation: prereq.abbreviation,
             task_status_id: tp.task_status_id
           }
+        end.to_json,
+        discussion_prompts.map do |prompt|
+        {
+          content: prompt.content,
+          priority: prompt.priority
+        }
         end.to_json
       ]
     # [target_date.strftime('%d-%m-%Y')] +
@@ -500,7 +506,7 @@ class TaskDefinition < ApplicationRecord
     [:name, :abbreviation, :description, :weighting, :target_grade, :restrict_status_updates, :max_quality_pts,
      :is_graded, :plagiarism_warn_pct, :scorm_enabled, :scorm_allow_review, :scorm_bypass_test, :scorm_time_delay_enabled,
      :scorm_attempt_limit, :group_set, :upload_requirements, :start_week, :start_day, :target_week, :target_day,
-     :due_week, :due_day, :tutorial_stream, :assess_in_portfolio_only, :task_prerequisites]
+     :due_week, :due_day, :tutorial_stream, :assess_in_portfolio_only, :task_prerequisites, :discussion_prompts]
   end
 
   def self.task_def_for_csv_row(unit, row)
@@ -560,6 +566,20 @@ class TaskDefinition < ApplicationRecord
 
     if row[:tutorial_stream].present?
       result.tutorial_stream = unit.tutorial_streams.where(abbreviation: row[:tutorial_stream]).first
+    end
+
+    result.discussion_prompts.destroy_all
+
+    if row[:discussion_prompts].present?
+      prompts = JSON.parse(row[:discussion_prompts])
+      prompts.each do |prompt|
+        DiscussionPrompt.create!({
+                                   task_definition: result,
+                                   content: prompt['content'],
+                                   priority: prompt['priority']
+                                 })
+      end
+
     end
 
     result.assess_in_portfolio_only = %w(Yes y Y yes true TRUE 1).include? "#{row[:assess_in_portfolio_only]}".strip
