124f60f05e9b79dd215142a4228b4562038ded9f
b0ink <40929320+b0ink@users.noreply.github.com>
2025-11-25 16:29:39 +1100

    fix: allow pdf gen and overseer job to be queued at the same time

--- app/sidekiq/accept_overseer_job.rb
+++ app/sidekiq/accept_overseer_job.rb
@@ -9,7 +9,7 @@ class AcceptOverseerJob
 
   sidekiq_options lock: :until_executed,
                   # TODO: should students be allowed to submit a new task submission when the previous overseer job has not started/completed?
-                  lock_args_method: ->(args) { [args.first] },
+                  lock_args_method: ->(args) { [args.first, 'overseer-assessment'] },
                   on_conflict: :reject,
                   retry: 1
 
