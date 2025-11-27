197ea620edf40f30c4ac5c1c5e8261c940b13ec6
b0ink <40929320+b0ink@users.noreply.github.com>
2025-11-27 12:05:56 +1100

    chore: add base64 check method

--- app/models/overseer_assessment.rb
+++ app/models/overseer_assessment.rb
@@ -253,5 +253,9 @@ class OverseerAssessment < ApplicationRecord
   def delete_associated_files
     FileUtils.rm_rf output_path
   end
+
+  def base64?(value)
+    value.is_a?(String) && Base64.strict_encode64(Base64.decode64(value)) == value
+  end
 end
 # rubocop:enable Rails/Output
