de3d67743b0147284c946a1bf1559a7e11a1fdc4
b0ink <40929320+b0ink@users.noreply.github.com>
2025-12-03 12:40:33 +1100

    fix: set correct mount

--- app/sidekiq/accept_overseer_job.rb
+++ app/sidekiq/accept_overseer_job.rb
@@ -79,7 +79,7 @@ class AcceptOverseerJob
                      # used in production, as it breaks isolation between tasks.
                      "--volumes-from #{Doubtfire::Application.config.overseer_fallback_volume_container}"
                    else
-                     "-v #{mount}/#{task_id}:/overseer/work-dir/#{work_dir_name}"
+                     "-v #{mount}/#{work_dir_name}:/overseer/work-dir/#{work_dir_name}"
                    end
 
     container_name = "overseer-#{task_id}-#{timestamp}"
