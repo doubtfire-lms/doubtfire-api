39ef7aa863934ce089a64138a4862831de2b1204
b0ink <40929320+b0ink@users.noreply.github.com>
2025-12-15 16:27:23 +1100

    feat: endpoint to reset all target dates for a project

--- app/api/tasks_api.rb
+++ app/api/tasks_api.rb
@@ -383,4 +383,45 @@ class TasksApi < Grape::API
     end
   end
 
+  desc 'Update the target dates for a task - when date flexibility is allowed'
+  params do
+    requires :id, type: Integer, desc: 'The project id to locate'
+  end
+  put '/projects/:id/reset_target_dates' do
+    project = Project.find(params[:id])
+
+    # check the user can put this task
+    if authorise? current_user, project, :make_submission
+      # Check unit allows planned date changes
+      unless project.unit.allow_flexible_dates
+        error!({ error: 'This unit does not allow you to adjust due dates.' }, 403)
+      end
+
+      project.tasks.each do |task|
+        next if task.target_start_date.nil? && task.target_due_date.nil?
+
+        task.update!(
+          target_start_date: nil,
+          target_due_date: nil
+        )
+
+        comment_text = "Planned date reset: #{task.task_definition.start_date.strftime('%d %b')} - #{task.task_definition.target_date.strftime('%d %b')}."
+        comment = TaskComment.create(
+          task: task,
+          user: current_user,
+          comment: comment_text,
+          content_type: :plan,
+          recipient: project.student
+        )
+
+        comment.mark_as_read(project.tutor_for(task.task_definition))
+      end
+
+      present project, with: Entities::ProjectEntity, user: current_user, for_student: true, in_project: true
+
+    else
+      error!({ error: "You are not permitted to adjust the plan." }, 403)
+    end
+  end
+
 end
