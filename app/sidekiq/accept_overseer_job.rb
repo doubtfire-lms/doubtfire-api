5576029998298006178de86dcbeba1b63042c02b
b0ink <40929320+b0ink@users.noreply.github.com>
2025-12-18 17:08:37 +1100

    refactor: init refactor of new overseer step workflow

--- app/sidekiq/accept_overseer_job.rb
+++ app/sidekiq/accept_overseer_job.rb
@@ -1,4 +1,5 @@
 require 'yaml'
+require 'open3'
 
 class AcceptOverseerJob
   include Sidekiq::Job
@@ -20,6 +21,7 @@ class AcceptOverseerJob
     total(1)
 
     task = Task.find(task_id)
+    task_definition = task.task_definition
 
     work_dir_name = "#{task.id}-#{overseer_assessment_id}"
 
@@ -61,51 +63,127 @@ class AcceptOverseerJob
       end
     end
 
+    overseer_steps = task_definition.overseer_steps
+
     # Extract execution script
-    script_path = task.task_definition.task_assessment_script
-    raise "No execution script found" unless File.exist?(script_path)
+    # script_path = task.task_definition.task_assessment_script
+    # raise "No execution script found" unless File.exist?(script_path)
 
-    script_contents = File.read(script_path)
-    raise "Execution script is empty" if script_contents.blank?
+    # system(command)
+    #
 
-    run_sh_path = File.join(work_dir, 'run.sh')
-    File.write(run_sh_path, script_contents)
-    system("chmod +x #{work_dir}/run.sh")
+    success_status = nil
+    failure_status = nil
 
-    mount = Doubtfire::Application.config.overseer_workdir_volume_mount
-    volume_mount = if mount.nil?
-                     # Fallback for development only — mounts the entire overseer container volume,
-                     # allowing all task work directories to be accessible. This should never be
-                     # used in production, as it breaks isolation between tasks.
-                     "--volumes-from #{Doubtfire::Application.config.overseer_fallback_volume_container}"
-                   else
-                     "-v #{mount}/#{work_dir_name}:/overseer/work-dir/#{work_dir_name}"
-                   end
+    overseer_steps.each do |step|
+      # script_contents = File.read(script_path)
+      script_contents = step.run_command
+      raise "Execution script is empty" if script_contents.blank?
 
-    container_name = "overseer-#{task_id}-#{timestamp}"
+      run_sh_path = File.join(work_dir, 'run.sh')
+      File.write(run_sh_path, script_contents)
+      system("chmod +x #{work_dir}/run.sh")
 
-    command = %(
-      timeout 300 docker run --rm \
-      --cpus 1 \
-      --network none \
-      #{volume_mount} \
-      --name #{container_name} \
-      #{docker_image_name_tag} \
-      bash -c "cd /overseer/work-dir/#{work_dir_name} && ./run.sh"
-    )
+      mount = Doubtfire::Application.config.overseer_workdir_volume_mount
+      volume_mount = if mount.nil?
+                       # Fallback for development only — mounts the entire overseer container volume,
+                       # allowing all task work directories to be accessible. This should never be
+                       # used in production, as it breaks isolation between tasks.
+                       "--volumes-from #{Doubtfire::Application.config.overseer_fallback_volume_container}"
+                     else
+                       "-v #{mount}/#{work_dir_name}:/overseer/work-dir/#{work_dir_name}"
+                     end
 
-    system(command)
+      container_name = "overseer-#{task_id}-#{timestamp}"
 
-    yaml_path = File.join(work_dir, 'output.yaml')
+      command = %(
+        timeout 300 docker run --rm -i \
+        --cpus 1 \
+        --network none \
+        #{volume_mount} \
+        --name #{container_name} \
+        #{docker_image_name_tag} \
+        bash -c "cd /overseer/work-dir/#{work_dir_name} && ./run.sh"
+      )
 
-    oa = OverseerAssessment.find(overseer_assessment_id)
+      output = ""
+      status = nil
 
-    oa.update_from_output(work_dir)
-    if File.exist?(yaml_path)
-      path = FileHelper.task_submission_identifier_path_with_timestamp(:done, task, timestamp)
-      FileUtils.cp(yaml_path, path)
-      FileUtils.rm_rf(work_dir)
+      stdin_input_file = nil
+      expected_output_file = nil
+      if step.step_type == 'output_diff'
+        stdin_input_file = step.stdin_input_file.present? ? File.join(work_dir, step.stdin_input_file) : nil
+        expected_output_file = step.expected_output_file.present? ? File.join(work_dir, step.expected_output_file) : nil
+      end
+
+      stdin_contents = nil
+      expected_output_contents = nil
+
+      Open3.popen2e(command) do |stdin, stdout_err, wait_thr|
+        if stdin_input_file && File.exist?(stdin_input_file)
+          File.open(stdin_input_file, 'rb') { |f| IO.copy_stream(f, stdin) }
+          stdin_contents = File.read(stdin_input_file)
+          stdin.close
+        end
+
+        stdout_err.each { |line| output << line }
+        status = wait_thr.value
+      end
+
+      pass = true
+
+      if status.exitstatus != 0
+        pass = false
+      end
+
+      if step.step_type == 'output_diff'
+        expected_output = File.read(expected_output_file)
+        expected_output_contents = expected_output
+        if step.partial_output_diff
+          unless output.include?(expected_output)
+            pass = false
+          end
+        elsif output != expected_output
+          pass = false
+        end
+      end
+
+      puts "Test passed?: #{[pass]}"
+
+      stdout_sha256 = Digest::SHA256.hexdigest(output)
+      stdin_sha256 = stdin_contents && Digest::SHA256.hexdigest(stdin_contents)
+      expected_sha256 = expected_output_contents && Digest::SHA256.hexdigest(expected_output_contents)
+
+      OverseerStepResult.create!({
+                                   overseer_assessment_id: overseer_assessment_id,
+                                   overseer_step: step,
+                                   exit_status: status.exitstatus,
+                                   pass: pass,
+                                   stdout: output,
+                                   stdin: stdin_contents,
+                                   expected_output: expected_output_contents,
+                                   stdout_sha256: stdout_sha256,
+                                   stdin_sha256: stdin_sha256,
+                                   expected_output_sha256: expected_sha256
+                                 })
+
+      byebug
+
+      if !pass && step.halt_on_failure
+        break
+      end
     end
+
+    # yaml_path = File.join(work_dir, 'output.yaml')
+
+    # oa = OverseerAssessment.find(overseer_assessment_id)
+
+    # oa.update_from_output(work_dir)
+    # if File.exist?(yaml_path)
+    #   path = FileHelper.task_submission_identifier_path_with_timestamp(:done, task, timestamp)
+    #   FileUtils.cp(yaml_path, path)
+    #   FileUtils.rm_rf(work_dir)
+    # end
 
     logger.info "Completed overseer job"
   rescue StandardError => e
