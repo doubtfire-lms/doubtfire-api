9f60b661ea412ff92cd70a973ffcd8c5c2fb4cbd
b0ink <40929320+b0ink@users.noreply.github.com>
2026-01-05 14:17:35 +1100

    chore: remove todo

--- app/api/entities/overseer_assessment_entity.rb
+++ app/api/entities/overseer_assessment_entity.rb
@@ -8,9 +8,6 @@ module Entities
     expose :created_at
     expose :updated_at
 
-    # TODO: filter permissions, create a custom map of what should be exposed but on the original step
-    # expose :overseer_step_results, using: OverseerStepResultEntity
-
     expose :total_steps
     expose :passed_steps
   end
