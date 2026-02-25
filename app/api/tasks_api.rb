7d55b1a8555c2cc42ba7a67c1df93a2d7d3fbf0e
b0ink <40929320+b0ink@users.noreply.github.com>
2026-02-25 11:11:49 +1100

    chore: expose task claims to staff only

--- app/api/tasks_api.rb
+++ app/api/tasks_api.rb
@@ -261,6 +261,7 @@ class TasksApi < Grape::API
     needs_upload_docs = !task_definition.upload_requirements.empty?
 
     task = nil
+    unit_role = project.unit.unit_role_for(current_user)
 
     # check if we actually have this task... if not must be false.
     if needs_upload_docs && project.has_task_for_task_definition?(task_definition)
@@ -271,10 +272,14 @@ class TasksApi < Grape::API
         has_pdf: task.has_pdf,
         submission_date: task.submission_date,
         processing_pdf: task.processing_pdf?,
-        task_status: task.task_status.status_key,
-        # TODO: expose to staff only?
-        claimed_by_unit_role_id: task.active_overflow_task_claim&.claimed_by_unit_role_id
+        task_status: task.task_status.status_key
       }
+
+      # Expose task claim to staff only
+      if unit_role
+        result[:claimed_by_unit_role_id] =
+          task.active_overflow_task_claim&.claimed_by_unit_role_id
+      end
     else
       result = {
         has_pdf: false,
