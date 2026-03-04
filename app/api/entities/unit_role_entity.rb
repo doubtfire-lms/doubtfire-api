6e84982db724b535067b5b474a61dd78143d3581
b0ink <40929320+b0ink@users.noreply.github.com>
2026-03-04 13:20:44 +1100

    fix: ignore notes sent from self

--- app/api/entities/unit_role_entity.rb
+++ app/api/entities/unit_role_entity.rb
@@ -13,7 +13,10 @@ module Entities
 
     expose :mentor_id, if: ->(unit_role, options) { staff?(options[:my_role]) }
     expose :tutor_note_count, if: ->(unit_role, options) { unit_role.unit.unit_role_for(options[:user])&.id == unit_role.id } do |unit_role, options|
-      unit_role.tutor_notes.where(read_by_unit_role: false).count
+      unit_role.tutor_notes
+               .where(read_by_unit_role: false)
+               .where.not(user_id: options[:user].id)
+               .count
     end
 
     expose :can_mark_overflow_tasks, if: ->(unit_role, options) { staff?(options[:my_role]) }
