d892cb102235cc938183eabbe26e17629a413976
b0ink <40929320+b0ink@users.noreply.github.com>
2026-03-05 15:01:30 +1100

    refactor: ensure to notify original user if note is a reply

--- app/api/tutor_notes_api.rb
+++ app/api/tutor_notes_api.rb
@@ -112,10 +112,12 @@ class TutorNotesApi < Grape::API
     result = unit_role.add_tutor_note(current_user, text_note, task_id, reply_to_id)
 
     notify_unit_role =
-      if current_unit_role == unit_role
-        unit_role.mentor # tutor is writing on their own notes -> notify mentor
+      if reply_target && original_staff_note.user != current_user
+        reply_target      # tutor is responding to a reply -> notify original user that tutor is replying to
+      elsif current_unit_role == unit_role
+        unit_role.mentor  # tutor is writing on their own notes -> notify the mentor
       else
-        unit_role        # someone else wrote about this tutor -> notify the tutor
+        unit_role         # anyone else wrote about this tutor, whether its their mentor or another convenor -> notify tutor
       end
 
     if result.present? && notify_unit_role.present? && notify_unit_role.user_id != current_user.id
