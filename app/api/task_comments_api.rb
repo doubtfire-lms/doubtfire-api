2d8f95495a2624cba39d663b9c513e54a4d381fd
b0ink <40929320+b0ink@users.noreply.github.com>
2026-04-27 12:32:05 +1000

    feat: enable comment editing

--- app/api/task_comments_api.rb
+++ app/api/task_comments_api.rb
@@ -191,6 +191,54 @@ class TaskCommentsApi < Grape::API
     present false
   end
 
+  desc 'Edit a comment'
+  params do
+    requires :comment, type: String, desc: 'The updated comment text'
+  end
+  put '/projects/:project_id/task_def_id/:task_definition_id/comments/:id' do
+    project = Project.find(params[:project_id])
+    task_definition = project.unit.task_definitions.find(params[:task_definition_id])
+
+    unless authorise? current_user, project, :make_submission
+      error!({ error: 'Not authorised to edit this comment' }, 403)
+    end
+
+    task = project.task_for_task_definition(task_definition)
+    task_comment = task.all_comments.find(params[:id])
+
+    unless task_comment.user == current_user
+      error!({ error: 'You can only edit your own comments' }, 403)
+    end
+
+    unless task_comment.content_type.blank? || task_comment.content_type == 'text'
+      error!({ error: 'Only text comments can be edited' }, 403)
+    end
+
+    if task_comment.created_at < 10.minutes.ago
+      error!({ error: 'Comments can only be edited within 10 minutes of being created' }, 403)
+    end
+
+    if params[:comment].blank?
+      error!({ error: 'Comment text is empty, unable to update comment' }, 403)
+    end
+
+    task_comment.comment = params[:comment]
+
+    unless task_comment.save
+      error!({ error: task_comment.errors.full_messages.to_sentence.presence || 'Unable to update comment' }, 403)
+    end
+
+    SessionTracker.record_assessment_activity(
+      action: 'edit-comment',
+      user: current_user,
+      project: project,
+      ip_address: request.ip,
+      task: task
+    )
+
+    present task_comment.serialize(current_user), with: Grape::Presenters::Presenter
+  end
+
   desc 'Mark a comment as unread'
   post '/projects/:project_id/task_def_id/:task_definition_id/comments/:id' do
     project = Project.find(params[:project_id])
--- app/models/session_activity.rb
+++ app/models/session_activity.rb
@@ -4,6 +4,6 @@ class SessionActivity < ApplicationRecord
   belongs_to :task, optional: true
   belongs_to :task_definition, optional: true
 
-  VALID_ACTIONS = %w[inbox GET PUT assessing add-comment get-comments delete-comment get-comment-attachment mark-comment-unread get-submission-details get-submission-files].freeze
+  VALID_ACTIONS = %w[inbox GET PUT assessing add-comment edit-comment get-comments delete-comment get-comment-attachment mark-comment-unread get-submission-details get-submission-files].freeze
   validates :action, presence: true, inclusion: { in: VALID_ACTIONS }
 end
