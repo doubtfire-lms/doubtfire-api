b36f0c834258e907429837ed342f2377c279afa3
AjayPAnand <40824905+AjayPAnand@users.noreply.github.com>
2026-05-01 15:11:48 +1000

    Reverting schema.rb -removed enforce_feedback_before_discussed_in_class column
    Removed the 'enforce_feedback_before_discussed_in_class' column from the schema as per the fix that is required this change is not necessary for my PR.

--- db/schema.rb
+++ db/schema.rb
@@ -743,7 +743,6 @@ ActiveRecord::Schema[8.0].define(version: 2026_03_27_041457) do
     t.boolean "mark_late_submissions_as_assess_in_portfolio", default: false, null: false
     t.integer "feedback_warning_threshold_days", default: 5
     t.integer "feedback_overflow_threshold_days", default: 7
-    t.boolean "enforce_feedback_before_discussed_in_class", default: false, null: false
     t.index ["draft_task_definition_id"], name: "index_units_on_draft_task_definition_id"
     t.index ["main_convenor_id"], name: "index_units_on_main_convenor_id"
     t.index ["overseer_image_id"], name: "index_units_on_overseer_image_id"
