d3cf4f4cbd4c6ba941a1a7cad937c2112a3b4320
b0ink <40929320+b0ink@users.noreply.github.com>
2026-05-13 14:12:43 +1000

    chore: allow manual environment set

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -3,7 +3,7 @@ if ENV["SENTRY_DSN"].present?
     config.dsn = ENV.fetch("SENTRY_DSN", nil)
     # get breadcrumbs from logs
     config.breadcrumbs_logger = [:active_support_logger, :http_logger]
-    config.environment = Rails.env
+    config.environment = ENV.fetch("SENTRY_ENVIRONMENT", Rails.env)
     config.release = ENV["SENTRY_RELEASE"] if ENV["SENTRY_RELEASE"].present?
     # Add data like request headers and IP for users, if applicable;
     # see https://docs.sentry.io/platforms/ruby/data-management/data-collected/ for more info
