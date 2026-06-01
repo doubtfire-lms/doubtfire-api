ad85a5d6dd8146bf6f01cc8b6b5743b4e66dc321
b0ink <40929320+b0ink@users.noreply.github.com>
2026-06-01 11:34:01 +1000

    refactor: ensure schedule next run is recalculated when updating teaching periods

--- app/models/teaching_period.rb
+++ app/models/teaching_period.rb
@@ -18,6 +18,7 @@ class TeachingPeriod < ApplicationRecord
   validate :validate_end_date_after_start_date, :validate_active_until_after_end_date
 
   after_update :propogate_date_changes
+  after_update :refresh_communication_schedule_caches, if: :saved_change_to_teaching_dates?
 
   # Public methods
 
@@ -132,6 +133,13 @@ class TeachingPeriod < ApplicationRecord
     TeachingPeriod.where("start_date > :end_date", end_date: end_date)
   end
 
+  def refresh_communication_schedule_caches
+    CommunicationSetSchedule
+      .joins(communication_set: :unit)
+      .where(units: { teaching_period_id: id })
+      .find_each(&:refresh_next_run_at!)
+  end
+
   private
 
   def can_destroy?
@@ -159,5 +167,9 @@ class TeachingPeriod < ApplicationRecord
     units.each do |u|
       u.update(start_date: self.start_date, end_date: self.end_date)
     end
+  end
+
+  def saved_change_to_teaching_dates?
+    saved_change_to_start_date? || saved_change_to_end_date?
   end
 end
