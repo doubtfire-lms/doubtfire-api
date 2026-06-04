f5b383fdeda016dd7d391325f04700356cc58da5
b0ink <40929320+b0ink@users.noreply.github.com>
2026-06-04 10:41:32 +1000

    fix: allow error log emails with no backtrace

--- app/mailers/error_log_mailer.rb
+++ app/mailers/error_log_mailer.rb
@@ -8,7 +8,8 @@ class ErrorLogMailer < ApplicationMailer
     end
 
     @doubtfire_product_name = Doubtfire::Application.config.institution[:product_name]
-    @error_log = "#{message}\n\n#{exception.message}\n\n#{exception.backtrace.join("\n")}"
+    backtrace = exception.backtrace&.join("\n") || 'No backtrace available'
+    @error_log = "#{message}\n\n#{exception.message}\n\n#{backtrace}"
 
     mail(to: email, from: email, subject: "#{@doubtfire_product_name} Error Log - #{subject}")
   end
